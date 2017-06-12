1c3438849f4085ba6802653e1b26f42ad84c1a9a
Kunal Shroff <shroffk@bnl.gov>
2017-06-12 14:16:23 -0400

    Creating a packaging module

---
+++ phoebus-product/src/main/java/org/phoebus/product/Launcher.java
@@ -0,0 +1,13 @@
+package org.phoebus.product;
+
+import org.phoebus.framework.workbench.PhoebusApplication;
+
+import javafx.application.Application;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        Application.launch(PhoebusApplication.class, args);
+    }
+
+}
