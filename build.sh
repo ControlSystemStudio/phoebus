edd73ebae33f079798fd6f8d3d294a949179f00f
kasemir <kasemirk@ornl.gov>
2017-06-23 15:08:49 -0400

    build: Adding ant file

---
+++ build.sh
@@ -0,0 +1,6 @@
+export JAVA_HOME=/home/ky9/Eclipse/jdk-9
+export ANT_HOME=/home/ky9/Eclipse/apache-ant-1.9.4
+export PATH="$ANT_HOME/bin:$JAVA_HOME/bin:$PATH"
+
+ant
+ 
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp phoebus-product/target/classes:core/phoebus-framework/target/framework-0.0.1.jar org.phoebus.product.Launcher
