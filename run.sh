f877194130762f65e17eac8bf6b095282f597a82
kasemir <kasemirk@ornl.gov>
2017-07-03 11:32:45 -0400

    Update build scripts

--- run.sh
+++ run.sh
@@ -1,1 +1,16 @@
-java -cp phoebus-product/target/classes:core/phoebus-framework/target/framework-0.0.1.jar org.phoebus.product.Launcher
+
+
+CP="phoebus-product/target/product-0.0.1.jar"
+CP="$CP:core/framework/target/framework-0.0.1.jar"
+CP="$CP:core/core-types/target/core-types-0.0.1.jar"
+CP="$CP:core/logging/target/logging-0.0.1.jar"
+CP="$CP:applications/greetings/target/greetings-0.0.1.jar"
+CP="$CP:applications/probe/target/probe-0.0.1.jar"
+
+for lib in ../dependencies/*.jar
+do
+  CP="$CP:$lib"
+done
+
+java -cp $CP org.phoebus.product.Launcher
+
