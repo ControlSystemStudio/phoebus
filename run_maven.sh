ab87871be7fe7e77f0a0705db115af32b2d38c32
Kay Kasemir <kay.kasemir@gmail.com>
2017-08-02 17:44:32 -0400

    Don't install the product

--- run_maven.sh
+++ run_maven.sh
@@ -3,6 +3,6 @@ mvn -o clean install -f core/pom.xml
 mvn -o clean install -f applications/probe/pom.xml
 mvn -o clean install -f applications/logbook/pom.xml
 mvn -o clean install -f applications/greetings/pom.xml
-mvn -o clean install -f phoebus-product/pom.xml
+mvn -o clean verify  -f phoebus-product/pom.xml
 
 (cd phoebus-product/target; java -jar product-0.0.1-SNAPSHOT.jar )
