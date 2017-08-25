a6a4d25b31e4e4368e38304d9f20c64f36c8ec2d
Kay Kasemir <kay.kasemir@gmail.com>
2017-08-25 10:07:49 -0400

    Add 'loc://' PV

---
+++ core/pv/src/main/java/org/phoebus/pv/loc/LocalPV.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ * Copyright (c) 2014-2016 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.phoebus.pv.loc;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.logging.Level;
+
+import org.diirt.vtype.AlarmSeverity;
+import org.diirt.vtype.VDouble;
+import org.diirt.vtype.VDoubleArray;
+import org.diirt.vtype.VString;
+import org.diirt.vtype.VStringArray;
+import org.diirt.vtype.VType;
+import org.phoebus.pv.PV;
+
+/** Local Process Variable
+ *
+ *  <p>Syntax:
+ *  <ul>
+ *  <li>loc://name(3.14), same as loc://name&lt;VDouble>(3.14)
+ *  <li>loc://name("Fred"), same as loc://name&lt;VString>("Fred")
+ *  <li>loc://name(1, 2, 3), same as loc://name&lt;VDoubleArray>(1, 2, 3)
+ *  <li>loc://name&lt;VDoubleArray>(1), forces array type
+ *  <li>loc://name("a", "b", "c"), same as loc://name&lt;VStringArray>("a", "b", "c")
+ *  <li>loc://name&lt;VLong>(1e10), forces long integer data type
+ *  <li>loc://name&lt;VEnum>(0, "a", "b", "c"), declares enumerated type with initial value and labels
+ *  <li>loc://name&lt;VTable>, declares PV as table (initially empty)
+ *  <li>loc://name&lt;VTable>("X", "Y"), declares PV as table with given column names (initially empty)
+ *  </ul>
+ *  @author Kay Kasemir, based on similar code in org.csstudio.utility.pv and diirt
+ */
+@SuppressWarnings("nls")
+public class LocalPV extends PV
+{
+    private volatile Class<? extends VType> type;
+    private final List<String> initial_value;
+
+    protected LocalPV(final String actual_name, final Class<? extends VType> type, final List<String> initial_value) throws Exception
+    {
+        super(actual_name);
+        this.type = type;
+        this.initial_value = initial_value;
+
+        // Set initial value
+        notifyListenersOfValue(ValueHelper.getInitialValue(initial_value, type));
+    }
+
+    protected void checkInitializer(final Class<? extends VType> type, final List<String> initial_value)
+    {
+        if (type != this.type  ||  ! Objects.equals(initial_value, this.initial_value))
+            logger.log(Level.WARNING, "PV " + getName() + " was initialized as " + formatInit(this.type, this.initial_value) +
+                    " and is now requested as " +  formatInit(type, initial_value));
+    }
+
+    private String formatInit(final Class<? extends VType> type, final List<String> value)
+    {
+        final StringBuilder buf = new StringBuilder();
+        buf.append('<').append(type.getSimpleName()).append('>');
+        if (value != null)
+        {
+            buf.append('(');
+            for (int i=0; i<value.size(); ++i)
+            {
+                if (i > 0)
+                    buf.append(",");
+                buf.append(value.get(i));
+            }
+            buf.append(')');
+        }
+        return buf.toString();
+    }
+
+    @Override
+    public void write(final Object new_value) throws Exception
+    {
+        if (new_value == null)
+            throw new Exception(getName() + " got null");
+
+        try
+        {
+            final VType last_value = read();
+            final boolean change_from_double = initial_value == null  &&
+                                               last_value instanceof VDouble  &&
+                                               ((VDouble)last_value).getAlarmSeverity() == AlarmSeverity.UNDEFINED;
+            final VType value = ValueHelper.adapt(new_value, type, last_value, change_from_double);
+            if (change_from_double  &&  ! type.isInstance(value))
+            {
+                final Class<? extends VType> new_type;
+                if (value instanceof VDoubleArray)
+                    new_type = VDoubleArray.class;
+                else if (value instanceof VStringArray)
+                    new_type = VStringArray.class;
+                else
+                    new_type = VString.class;
+                logger.log(Level.WARNING, "PV " + getName() + " changed from " + type.getSimpleName() + " to " + new_type.getSimpleName());
+                type = new_type;
+            }
+            notifyListenersOfValue(value);
+        }
+        catch (Exception ex)
+        {
+            if (new_value != null  &&  new_value.getClass().isArray())
+                throw new Exception("Failed to write " + new_value.getClass().getSimpleName() + " to " + getName(), ex);
+            throw new Exception("Failed to write '" + new_value + "' to " + this, ex);
+        }
+     }
+
+    @Override
+    protected void close()
+    {
+        super.close();
+        LocalPVFactory.releasePV(this);
+    }
+}
---
+++ core/pv/src/main/java/org/phoebus/pv/loc/LocalPVFactory.java
@@ -0,0 +1,135 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.phoebus.pv.loc;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.diirt.vtype.VDouble;
+import org.diirt.vtype.VDoubleArray;
+import org.diirt.vtype.VEnum;
+import org.diirt.vtype.VLong;
+import org.diirt.vtype.VString;
+import org.diirt.vtype.VStringArray;
+import org.diirt.vtype.VTable;
+import org.diirt.vtype.VType;
+import org.phoebus.pv.PV;
+import org.phoebus.pv.PVFactory;
+import org.phoebus.pv.PVPool;
+
+/** Factory for creating {@link LocalPV}s
+ *  @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class LocalPVFactory implements PVFactory
+{
+    final public static String TYPE = "loc";
+
+    /** Map of local PVs */
+    private static final Map<String, LocalPV> local_pvs = new HashMap<>();
+
+    @Override
+    public String getType()
+    {
+        return TYPE;
+    }
+
+    @Override
+    public PV createPV(final String name, final String base_name) throws Exception
+    {
+        final String[] ntv = ValueHelper.parseName(base_name);
+
+        // Actual name: loc://the_pv  without <type> or (initial value)
+        final String actual_name = LocalPVFactory.TYPE + PVPool.SEPARATOR + ntv[0];
+
+        // Info for initial value, null if nothing provided
+        final List<String> initial_value = ValueHelper.splitInitialItems(ntv[2]);
+
+        // Determine type from initial value or use given type
+        final Class<? extends VType> type = ntv[1] == null
+                                          ? determineValueType(initial_value)
+                                          : parseType(ntv[1]);
+        LocalPV pv;
+        synchronized (local_pvs)
+        {
+            pv = local_pvs.get(actual_name);
+            if (pv == null)
+            {
+                pv = new LocalPV(actual_name, type, initial_value);
+                local_pvs.put(actual_name, pv);
+            }
+            else
+                pv.checkInitializer(type, initial_value);
+        }
+        return pv;
+    }
+
+    private static Class<? extends VType> determineValueType(final List<String> items) throws Exception
+    {
+        if (items == null)
+            return VDouble.class;
+
+        if (ValueHelper.haveInitialStrings(items))
+        {
+            if (items.size() == 1)
+                return VString.class;
+            else
+                return VStringArray.class;
+        }
+        else
+        {
+            if (items.size() == 1)
+                return VDouble.class;
+            else
+                return VDoubleArray.class;
+        }
+    }
+
+    private static Class<? extends VType> parseType(final String type) throws Exception
+    {   // Lenient check, ignore case and allow partial match
+        final String lower = type.toLowerCase();
+        if (lower.contains("doublearray"))
+            return VDoubleArray.class;
+        if (lower.contains("double")) // 'VDouble', 'vdouble', 'double'
+            return VDouble.class;
+        if (lower.contains("stringarray"))
+            return VStringArray.class;
+        if (lower.contains("string"))
+            return VString.class;
+        if (lower.contains("enum"))
+            return VEnum.class;
+        if (lower.contains("long"))
+            return VLong.class;
+        if (lower.contains("table"))
+            return VTable.class;
+        throw new Exception("Local PV cannot handle type '" + type + "'");
+    }
+
+    /** Remove local PV from pool
+     *  To be called by LocalPV when closed
+     *  @param pv {@link LocalPV}
+     */
+    static void releasePV(final LocalPV pv)
+    {
+        synchronized (local_pvs)
+        {
+            local_pvs.remove(pv.getName());
+        }
+    }
+
+    // For unit test
+    public static Collection<LocalPV> getLocalPVs()
+    {
+        synchronized (local_pvs)
+        {
+            return local_pvs.values();
+        }
+    }
+}
