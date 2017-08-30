6aca8c0a6ae45ede0f2509820afd34599dd0085a
Kay Kasemir <kay.kasemir@gmail.com>
2017-08-30 12:24:06 -0400

    Update pvtable pom

--- run_maven.sh
+++ run_maven.sh
@@ -1,10 +1,5 @@
 mvn clean verify  -f dependencies/pom.xml
-mvn -o clean install -f core/pom.xml
-mvn -o clean install -f applications/probe/pom.xml
-mvn -o clean install -f applications/logbook/pom.xml
-mvn -o clean install -f applications/greetings/pom.xml
-mvn -o clean install -f applications/pvtable/pom.xml
-mvn -o clean install -f applications/pvtree/pom.xml
-mvn -o clean verify  -f phoebus-product/pom.xml
-
+# All but first run can use -o for offline
+#
+mvn -o -DskipTests clean install 
 (cd phoebus-product/target; java -jar product-0.0.1-SNAPSHOT.jar  --add-modules=ALL-SYSTEM )
