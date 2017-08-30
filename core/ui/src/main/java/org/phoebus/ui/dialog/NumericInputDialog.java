13d89ad924265a0ed64783059e7df0ef1f9ae7d6
Kay Kasemir <kay.kasemir@gmail.com>
2017-08-30 17:10:50 -0400

    comments

--- core/ui/src/main/java/org/phoebus/ui/dialog/NumericInputDialog.java
+++ core/ui/src/main/java/org/phoebus/ui/dialog/NumericInputDialog.java
@@ -9,13 +9,6 @@ package org.phoebus.ui.dialog;
 
 import java.util.Optional;
 import java.util.function.Consumer;
-/*******************************************************************************
- * Copyright (c) 2017 Oak Ridge National Laboratory.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
 import java.util.function.Function;
 
 import javafx.event.ActionEvent;
