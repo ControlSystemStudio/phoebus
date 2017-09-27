8fa7e5a1ccd70079cfcaf0e9c5e617c9a2779ca7
Kay Kasemir <kay.kasemir@gmail.com>
2017-09-27 13:46:02 -0400

    phoebus.bat for Windows

---
+++ phoebus-product/phoebus.bat
@@ -0,0 +1,15 @@
+REM Phoebus launcher for Windows
+REM When deploying, you might want to change "TOP"
+REM to the absolute installation path
+
+set V=0.0.1
+
+IF EXIST product-%V%.jar (
+  SET JAR=product-%V%.jar
+) ELSE (
+  SET JAR=product-%V%-SNAPSHOT.jar
+
+)
+
+java -jar %JAR% %*
+
