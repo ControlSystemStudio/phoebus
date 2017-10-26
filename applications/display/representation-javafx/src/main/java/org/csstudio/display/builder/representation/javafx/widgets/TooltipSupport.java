6325b1b6186e809e1594e5985e8b7cdf48b60054
Kay Kasemir <kay.kasemir@gmail.com>
2017-10-26 08:55:44 -0400

    display tooltip: Use Java 9 API, no need to hack

--- applications/display/representation-javafx/src/main/java/org/csstudio/display/builder/representation/javafx/widgets/TooltipSupport.java
+++ applications/display/representation-javafx/src/main/java/org/csstudio/display/builder/representation/javafx/widgets/TooltipSupport.java
@@ -9,7 +9,6 @@ package org.csstudio.display.builder.representation.javafx.widgets;
 
 import static org.csstudio.display.builder.representation.ToolkitRepresentation.logger;
 
-import java.lang.reflect.Field;
 import java.util.logging.Level;
 import java.util.regex.Pattern;
 
@@ -20,8 +19,6 @@ import org.csstudio.display.builder.model.macros.MacroHandler;
 import org.csstudio.display.builder.model.macros.MacroValueProvider;
 import org.csstudio.display.builder.model.properties.StringWidgetProperty;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.scene.Node;
 import javafx.scene.control.Tooltip;
 import javafx.util.Duration;
@@ -34,8 +31,6 @@ public class TooltipSupport
 {
     /** Legacy tool tip: "$(pv_name)\n$(pv_value)" where number of '\n' can vary */
     private final static Pattern legacy_tooltip = Pattern.compile("\\$\\(pv_name\\)\\s*\\$\\(pv_value\\)");
-
-    private static boolean initialized_behavior = false;
 
     /** Attach tool tip
      *  @param node Node that should have the tool tip
@@ -80,46 +75,12 @@ public class TooltipSupport
             }
         });
 
+        // Show after 250 instead of 1000 ms
+        tooltip.setShowDelay(Duration.millis(250));
+
+        // Hide after 30 instead of 5 secs
+        tooltip.setShowDuration(Duration.seconds(30));
+
         Tooltip.install(node, tooltip);
-
-        if (! initialized_behavior)
-        {
-            // Unfortunately, no API to control when tooltop shows, and for how long.
-            // http://stackoverflow.com/questions/26854301/control-javafx-tooltip-delay
-            // has the hack used in here, which only needs to be applied once
-            // because it changes a static BEHAVIOR inside the Tooltip.
-            // Java 9 will offer API, https://bugs.openjdk.java.net/browse/JDK-8090477
-            hack_behavior(tooltip);
-            initialized_behavior = true;
-        }
-    }
-
-    private static void hack_behavior(final Tooltip tooltip)
-    {
-        try
-        {
-            final Field behavior_field = tooltip.getClass().getDeclaredField("BEHAVIOR");
-            behavior_field.setAccessible(true);
-            final Object behavior = behavior_field.get(tooltip);
-
-            // Show after 250 instead of 1000 ms
-            hack_behavior(behavior, "activationTimer", 250);
-            // Show after 30 instead of 5 secs
-            hack_behavior(behavior, "hideTimer", 30000);
-        }
-        catch (Exception ex)
-        {
-            logger.log(Level.WARNING, "Cannot set Tooltip behaviour", ex);
-        }
-    }
-
-    private static void hack_behavior(final Object behavior, final String aspect, final int ms) throws Exception
-    {
-        final Field field = behavior.getClass().getDeclaredField(aspect);
-        field.setAccessible(true);
-        final Timeline timer = (Timeline) field.get(behavior);
-        timer.getKeyFrames().clear();
-        timer.getKeyFrames().add(new KeyFrame(new Duration(ms)));
-        logger.log(Level.FINE, "Set Tooltip " + aspect + " to " + ms + " ms");
     }
 }
