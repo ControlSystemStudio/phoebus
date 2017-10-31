6fa41624d6255b9af8954b9f78896ec7230a87df
Kay Kasemir <kay.kasemir@gmail.com>
2017-10-31 13:22:33 -0400

    display: Add DisplayInfo w/ URL conversion and test

---
+++ applications/display/runtime/src/main/java/org/csstudio/display/builder/runtime/app/DisplayInfo.java
@@ -0,0 +1,235 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2017 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.csstudio.display.builder.runtime.app;
+
+import static org.csstudio.display.builder.runtime.WidgetRuntime.logger;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Objects;
+import java.util.logging.Level;
+
+import org.csstudio.display.builder.model.DisplayModel;
+import org.csstudio.display.builder.model.macros.Macros;
+
+/** Information about a display
+ *  @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class DisplayInfo
+{
+    private static final String UTF_8 = "UTF-8";
+    private final String path, name;
+    private final Macros macros;
+    private final boolean resolve;
+
+    /** Parse URL for a display
+     *
+     *  <p>Minimal example:
+     *  <code>
+     *  file://path/to/file.bob
+     *  </code>
+     *
+     *  <p>Full example:
+     *  <code>
+     *  file://path/to/file.bob;MACRO1=value1;MACRO2=another%20value#Display%20Name
+     *  </code>
+
+     *  <p>Path of the URL is the file or http link to
+     *  the display.
+     *
+     *  <p>Optional path parameters, added via ';' to the path,
+     *  provide macros.
+     *
+     *  <p>An optional '#..' reference is used as the display name,
+     *  which is otherwise derived from the file name,
+     *  i.e. the last segment of the path.
+     *
+     *  @param url
+     *  @return {@link DisplayInfo}
+     */
+    public static DisplayInfo forURL(final URL url)
+    {
+        if (url.getQuery() != null)
+            logger.log(Level.WARNING, "Ignoring query component '" + url.getQuery() + "' in " + url);
+
+        // Get basic file or http 'path' and 'macros' from the URL pathname and path parameters
+        final String path_and_macros = url.getPath();
+        final String path;
+        final Macros macros = new Macros();
+        int sep = path_and_macros.indexOf(';');
+        if (sep > 0)
+        {
+            path = path_and_macros.substring(0, sep);
+            for (String macro_spec : path_and_macros.substring(sep+1).split(";"))
+            {
+                final int mvsep = macro_spec.indexOf('=');
+                if (mvsep > 0)
+                {
+                    final String name = macro_spec.substring(0, mvsep);
+                    final String value = decode(macro_spec.substring(mvsep+1));
+                    macros.add(name, value);
+                }
+            }
+        }
+        else
+            path = path_and_macros;
+
+        // Use either provided display name, or basename of path
+        String name = url.getRef();
+        if (name != null)
+            name = decode(name);
+        if (name == null)
+            name = basename(path);
+
+        return new DisplayInfo(path, name, macros, true);
+    }
+
+    /** Decode URL text
+     *  @param text Text that may contain '+' or '%20' etc.
+     *  @return Decoded text
+     */
+    private static String decode(final String text)
+    {
+        try
+        {
+            return URLDecoder.decode(text, UTF_8);
+        }
+        catch (UnsupportedEncodingException ex)
+        {
+            logger.log(Level.WARNING, "Cannot decode " + text);
+        }
+        return text;
+    }
+
+    /** Encode URL text
+     *  @param text Text that may contain spaces etc. in UTF-8
+     *  @return URL-encoded text
+     */
+    private static String encode(final String text)
+    {
+        try
+        {
+            return URLEncoder.encode(text, UTF_8);
+        }
+        catch (UnsupportedEncodingException ex)
+        {
+            logger.log(Level.WARNING, "Cannot encode " + text);
+        }
+        return text;
+    }
+
+    private static String basename(final String path)
+    {
+        int sep = path.lastIndexOf('/');
+        if (sep < 0)
+            sep = path.lastIndexOf('\\');
+        if (sep < 0)
+            return path;
+        return path.substring(sep+1);
+    }
+
+    /** @param model DisplayModel with macros and USER_DATA_INPUT_FILE
+     *  @return DisplayInfo
+     */
+    public static DisplayInfo forModel(final DisplayModel model)
+    {
+        return new DisplayInfo(model.getUserData(DisplayModel.USER_DATA_INPUT_FILE),
+                model.getDisplayName(),
+                model.propMacros().getValue(),
+                false);
+    }
+
+    /** @param path Path to the display
+     *  @param name Name to show
+     *  @param macros Macros
+     *  @param resolve Resolve the display, potentially using *.bob for a *.opi path?
+     */
+    public DisplayInfo(final String path, final String name, final Macros macros, final boolean resolve)
+    {
+        this.path = path;
+        this.name = name;
+        this.macros = Objects.requireNonNull(macros);
+        this.resolve = resolve;
+    }
+
+    /** @return Path to the display file or http link */
+    public String getPath()
+    {
+        return path;
+    }
+
+    /** @return Display name */
+    public String getName()
+    {
+        return name;
+    }
+
+    /** @return Macros */
+    public Macros getMacros()
+    {
+        return macros;
+    }
+
+    /** @return Resolve *.opi into *.bob? */
+    public boolean shouldResolve()
+    {
+        return resolve;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + name.hashCode();
+        result = prime * result + path.hashCode();
+        result = prime * result + macros.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj)
+    {
+        if (! (obj instanceof DisplayInfo))
+            return false;
+        final DisplayInfo other = (DisplayInfo) obj;
+        // Ignore the name
+        // Displays match if they refer to the same path and macros
+        return path.equals(other.path) &&
+               macros.equals(other.macros);
+    }
+
+    /** @return URL representation of display info */
+    public URL toURL() throws Exception
+    {
+        final StringBuilder buf = new StringBuilder();
+
+        buf.append(path);
+
+        for (String name : macros.getNames())
+            buf.append(';')
+               .append(name)
+               .append('=')
+               .append(encode(macros.getValue(name)));
+
+        if (name != null  &&  !basename(path).equals(name))
+            buf.append('#')
+               .append(encode(name));
+
+        return new URL(buf.toString());
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Display '" + name + "', file " + path + ", macros " + macros + (resolve ? " (resolve)" : "");
+    }
+}
---
+++ applications/display/runtime/src/test/java/org/csstudio/display/builder/runtime/test/DisplayInfoTest.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.csstudio.display.builder.runtime.test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.net.URL;
+
+import org.csstudio.display.builder.model.macros.Macros;
+import org.csstudio.display.builder.runtime.TextPatch;
+import org.csstudio.display.builder.runtime.app.DisplayInfo;
+import org.junit.Test;
+
+/** JUnit demo of the {@link TextPatch}
+ *  @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class DisplayInfoTest
+{
+    @Test
+    public void testURL2DisplayInfo() throws Exception
+    {
+        // Simple example
+        URL url = new URL("file:/some/path/xx.bob");
+        DisplayInfo info = DisplayInfo.forURL(url);
+        System.out.println(info);
+
+        assertThat(info.getPath(), equalTo("/some/path/xx.bob"));
+        assertThat(info.getName(), equalTo("xx.bob"));
+
+        // Complete example with display name and macros
+        url = new URL("file:/some/path/xx.bob;X=Fred+Harvey%20Newman;Y=2?query=ignored#X%20Overview");
+        info = DisplayInfo.forURL(url);
+        System.out.println(info);
+
+        assertThat(info.getPath(), equalTo("/some/path/xx.bob"));
+        assertThat(info.getName(), equalTo("X Overview"));
+        assertThat(info.getMacros().getValue("X"), equalTo("Fred Harvey Newman"));
+        assertThat(info.getMacros().getValue("Y"), equalTo("2"));
+    }
+
+    @Test
+    public void testDisplayInfo2URL() throws Exception
+    {
+        // Plain path
+        final Macros macros = new Macros();
+        DisplayInfo info = new DisplayInfo("file:/some/path/xx.bob", null, macros, false);
+        URL url = info.toURL();
+        System.out.println(url);
+        assertThat(url.toString(), equalTo("file:/some/path/xx.bob"));
+
+        // .. with macros
+        macros.add("X", "Fred Harvey Newman");
+        macros.add("Y", "2");
+        info = new DisplayInfo("file:/some/path/xx.bob", null, macros, false);
+        url = info.toURL();
+        System.out.println(url);
+
+        assertThat(url.toString(), equalTo("file:/some/path/xx.bob;X=Fred+Harvey+Newman;Y=2"));
+
+        // .. and display name
+        info = new DisplayInfo("file:/some/path/xx.bob", "X Overview", macros, false);
+        url = info.toURL();
+        System.out.println(url);
+
+        assertThat(url.toString(), equalTo("file:/some/path/xx.bob;X=Fred+Harvey+Newman;Y=2#X+Overview"));
+    }
+}
