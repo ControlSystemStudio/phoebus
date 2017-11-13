83f5c344b04419b22aa2b6913a3a7fb30286670e
Kay Kasemir <kay.kasemir@gmail.com>
2017-11-13 16:10:41 -0500

    ResourceParser: Add URI support for resource URIs ..
    .. which generally remain unchanged, except "pv:" is supported for PVs
    and "?... app=app_name" is used for optional application name.

--- core/framework/src/main/java/org/phoebus/framework/util/ResourceParser.java
+++ core/framework/src/main/java/org/phoebus/framework/util/ResourceParser.java
@@ -15,16 +15,143 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * A utility class for parsing user defined resources
  *
  * @author Kunal Shroff
- *
+ * @author Kay Kasemir
  */
 @SuppressWarnings("nls")
 public class ResourceParser {
+    /** URI schema for PV names */
+    public static final String PV_SCHEMA = "pv";
+
+    /** URI query tag used to specify the application name */
+    private static final String APP_QUERY_TAG = "app=";
+
+
+    /** Create URI for a resource
+     *
+     *  <p>Takes the resource passed by the user,
+     *  typically on the command line,
+     *  and turns it into a URI.
+     *
+     *  <p>Supported are URI strings
+     *  like "http:/..." or "file:...".
+     *  Plain strings that do not start with a 'schema'
+     *  are assumed to be files,
+     *  and are resolved as a path in the local file system.
+     *  Relative paths are resolved relative to the
+     *  "current working directory",
+     *  which may not be known when the application is running,
+     *  so that it generally a bad idea.
+     *
+     *  <p>The file must NOT exist, the "file://.." URI
+     *  will be created even if the file does not exist,
+     *  because the caller may use the URI to then create
+     *  the resource.
+     *
+     *  @param resource Resource text from user
+     *  @return URI for the resource
+     *  @throws Exception on error
+     */
+    public static URI createResourceURI(final String resource) throws Exception
+    {
+        try
+        {
+            final URI uri = URI.create(resource);
+            // Received complete URL, starting with scheme?
+            if (uri.getScheme() != null)
+                return uri;
+            else
+            {
+                // Treat it like a file resource
+                //
+                // By default the following is used by the default Paths.get(...) which should
+                // be the directory from which the program was executed
+                // FileSystem fs = FileSystems.getDefault();
+                // System.out.println(fs.toString());
+                final Path fileResource = Paths.get(resource);
+                return fileResource.toUri();
+            }
+        }
+        catch (Throwable ex)
+        {
+            throw new Exception("Cannot create URI for '" + resource + "'", ex);
+        }
+    }
+
+
+    /** Get file for URI
+     *  @return URI for the resource
+     *  @return {@link File} if URL represents a file, otherwise <code>null</code>
+     */
+    public static File getFile(final URI resource) throws Exception
+    {
+        if (resource == null  ||  !resource.getScheme().equals("file"))
+            return null;
+        return new File(resource);
+    }
+
+    /** Get URI for file
+     *  @param file {@link File}
+     *  @return {@link URI} for that file
+     */
+    public static URI getURI(final File file)
+    {
+        return file.toURI();
+    }
+
+    /** Get list of PVs from a "pv://?PV1&PV2" type URL
+     *
+     *  <p>If the URI is simply no "pv:" URI, an empty list
+     *  if returned.
+     *  If it is a "pv:" URI, at least one PV is expected,
+     *  otherwise an exception is thrown.
+     *
+     *  @param resource "pv://?PV1&PV2" type URL
+     *  @return List of PVs parsed from the resource
+     *  @throws Exception on error, including no PVs
+     */
+    public static List<String> parsePVs(final URI resource) throws Exception
+    {
+        if (! resource.getScheme().equals(PV_SCHEMA))
+            return List.of();
+        final List<String> pvs = getQueryStream(resource).filter(pv -> !pv.startsWith(APP_QUERY_TAG))
+                                                         .collect(Collectors.toList());
+        if (pvs.isEmpty())
+            throw new Exception("No PVs found in '" + resource + "'");
+        return pvs;
+    }
+
+    /** Get application name hint from resource
+     *  @param resource URI that might contain "?...app=the_app_name"
+     *  @return "the_app_name" or empty result.
+     */
+    public static Optional<String> getAppName(final URI resource)
+    {
+        return getQueryStream(resource).filter(q -> q.startsWith(APP_QUERY_TAG))
+                                       .map(app_name -> app_name.substring(APP_QUERY_TAG.length()))
+                                       .findFirst();
+    }
+
+    /** @param resource URI for resource
+     *  @return Stream for all query items
+     */
+    private static Stream<String> getQueryStream(final URI resource)
+    {
+        final String query = resource.getQuery();
+        if (query == null)
+            return Stream.empty();
+        return Arrays.stream(query.split("&"));
+    }
+
+    // ------------------------------------
+
 
     /** Resource query key for PV names */
     public static final String PV_ARG = "pv";
@@ -71,7 +198,7 @@ public class ResourceParser {
     }
 
     /**
-     * Get file for URL
+     * Get file for URLSN=220416
      *
      * @param url {@link URL}
      * @return {@link File} if URL represents a file, otherwise <code>null</code>
@@ -119,20 +246,6 @@ public class ResourceParser {
             return Collections.emptyMap();
         }
         return Arrays.stream(uri.getQuery().split("&")).map(ResourceParser::splitQueryParameter).collect(Collectors
-                .groupingBy(SimpleImmutableEntry::getKey, LinkedHashMap::new, mapping(Map.Entry::getValue, toList())));
-    }
-
-    /**
-     * Parse the query segment of a URL and return a {@link Map}
-     *
-     * @param url resource path URL
-     * @return {@link Map} a map of all the query parameters
-     */
-    public static Map<String, List<String>> parseQueryArgs(URL url) {
-        if (url.getQuery() == null || url.getQuery().isEmpty()) {
-            return Collections.emptyMap();
-        }
-        return Arrays.stream(url.getQuery().split("&")).map(ResourceParser::splitQueryParameter).collect(Collectors
                 .groupingBy(SimpleImmutableEntry::getKey, LinkedHashMap::new, mapping(Map.Entry::getValue, toList())));
     }
 
--- core/framework/src/test/java/org/phoebus/framework/util/ResourceParserTest.java
+++ core/framework/src/test/java/org/phoebus/framework/util/ResourceParserTest.java
@@ -1,27 +1,165 @@
 package org.phoebus.framework.util;
 
-import static org.phoebus.framework.util.ResourceParser.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.phoebus.framework.util.ResourceParser.PV_SCHEMA;
+import static org.phoebus.framework.util.ResourceParser.createAppURI;
+import static org.phoebus.framework.util.ResourceParser.createResourceURI;
+import static org.phoebus.framework.util.ResourceParser.createResourceURL;
+import static org.phoebus.framework.util.ResourceParser.getAppName;
+import static org.phoebus.framework.util.ResourceParser.getFile;
+import static org.phoebus.framework.util.ResourceParser.getURI;
+import static org.phoebus.framework.util.ResourceParser.parseAppName;
+import static org.phoebus.framework.util.ResourceParser.parsePVs;
+import static org.phoebus.framework.util.ResourceParser.parseQueryArgs;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.junit.Test;
 
 /**
- * A utility class for parsing user defined resources
- * 
- * @author Kunal Shroff
+ * JUnit test of {@link ResourceParser}
  *
+ * @author Kunal Shroff
+ * @author Kay Kasemir
  */
+@SuppressWarnings("nls")
 public class ResourceParserTest {
+    @Test
+    public void checkFileToURI() throws Exception
+    {
+        // File URL
+        URI uri = createResourceURI("file:/some/file/path");
+        System.out.println(uri);
+        assertThat(uri.getScheme(), equalTo("file"));
+        assertThat(uri.getPath(), equalTo("/some/file/path"));
+
+        // File URL with application hint
+        uri = createResourceURI("file:/some/file/path?app=probe");
+        System.out.println(uri);
+        assertThat(uri.getScheme(), equalTo("file"));
+        // File stays the same, the app hint in the query doesn't change that
+        assertThat(uri.getPath(), equalTo("/some/file/path"));
+
+        // A test resource relative to where this test is running
+        final String relative = "src/test/resources/test.plt";
+
+        // Absolute file path
+        final File file = new File(".", relative).getCanonicalFile();
+        final String absolute = file.getCanonicalPath();
+
+        // Test if absolute path is turned into URI
+        uri = createResourceURI(absolute);
+        System.out.println(uri);
+        assertThat(uri.getScheme(), equalTo("file"));
+        assertThat(uri.getPath(), equalTo(absolute));
+
+        // Turn relative file path into URI, which should then be absolute
+        uri = createResourceURI(relative);
+        System.out.println(uri);
+        assertThat(uri.getScheme(), equalTo("file"));
+        assertThat(uri.getPath(), equalTo(absolute));
+
+        // Convert between File and URI
+        assertThat(getFile(uri), equalTo(file));
+        assertThat(getURI(file), equalTo(uri));
+
+        // Nonexisting file
+        uri = createResourceURI("some/bogus/file");
+        assertThat(uri, not(nullValue()));
+        final File bogus = getFile(uri);
+        System.out.println(bogus);
+        assertThat(bogus.exists(), equalTo(false));
+    }
+
+    @Test
+    public void checkWebToURI() throws Exception
+    {
+        // Web URL
+        URI uri = createResourceURI("http://some.site/file/path");
+        System.out.println(uri);
+        assertThat(uri.getScheme(), equalTo("http"));
+        assertThat(uri.getHost(), equalTo("some.site"));
+        assertThat(uri.getPath(), equalTo("/file/path"));
+    }
+
+    @Test
+    public void checkPVs() throws Exception
+    {
+        // PV URL with one PV
+        URI uri = createResourceURI("pv://?Fred");
+        System.out.println(uri);
+        assertThat(uri.getScheme(), equalTo(PV_SCHEMA));
+
+        List<String> pvs = parsePVs(uri);
+        System.out.println(pvs);
+        assertThat(pvs.size(), equalTo(1));
+        assertThat(pvs, hasItems("Fred"));
+
+        // Several PVs (but don't include the application hint)
+        uri = createResourceURI("pv://?Fred&Jane&AnotherPV&app=probe");
+        System.out.println(uri);
+        assertThat(uri.getScheme(), equalTo(PV_SCHEMA));
+
+        pvs = parsePVs(uri);
+        System.out.println(pvs);
+        assertThat(pvs.size(), equalTo(3));
+        assertThat(pvs, hasItems("Fred", "Jane" , "AnotherPV"));
+
+        // Not a PV URL -> empty result
+        pvs = parsePVs(createResourceURI("http://some.site"));
+        assertThat(pvs.size(), equalTo(0));
+
+        // Check errors
+        try
+        {   // "pvs://" without actual PVs
+            parsePVs(createResourceURI("pv://some.site"));
+            fail("Failed to detect missing PVs");
+        }
+        catch (Exception ex)
+        {
+            // Good, got exception
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void checkApplicationHint() throws Exception
+    {
+        // Plain URL, no application hint
+        URI uri = createResourceURI("pv://?Fred");
+        Optional<String> app = getAppName(uri);
+        assertThat(app.isPresent(), equalTo(false));
+
+        // PVs with application hint
+        uri = createResourceURI("pv://?Fred&app=probe");
+        app = getAppName(uri);
+        assertThat(app.isPresent(), equalTo(true));
+        assertThat(app.get(), equalTo("probe"));
+
+        // File URL with application hint
+        uri = createResourceURI("file:/path/to/file?app=probe");
+        app = getAppName(uri);
+        assertThat(app.isPresent(), equalTo(true));
+        assertThat(app.get(), equalTo("probe"));
+    }
+
+
+    // ------------------------------------
 
     @Test
     public void checkResourcePathUtil() {
@@ -61,7 +199,7 @@ public class ResourceParserTest {
                 "Failed to parse app launcher string expected: " + queryArgs + " returned: "
                         + parseQueryArgs(createAppURI(resourceURI)),
                 queryArgs, parseQueryArgs(createAppURI(resourceURI)));
-        
+
         // the app name and arguments are provided
         resourceURI = "probe?pv=sim://noise&pv=sim://sine";
         queryArgs = new HashMap<>();
@@ -70,9 +208,9 @@ public class ResourceParserTest {
                 "Failed to parse app launcher string expected: " + queryArgs + " returned: "
                         + parseQueryArgs(createAppURI(resourceURI)),
                 queryArgs, parseQueryArgs(createAppURI(resourceURI)));
-        
+
     }
-    
+
     @Test
     public void checkFileParsing() {
 //        File file = new File("test");
