51cc93161ddd145a7916ed7bc26c4ff63c21f736
Kay Kasemir <kay.kasemir@gmail.com>
2017-12-21 12:06:04 -0500

    Move RTPlot from applications-display to applications
    .. for sharing with data browser

---
+++ applications/databrowser/src/main/java/org/csstudio/trends/databrowser3/Activator.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.csstudio.trends.databrowser3;
+
+import java.util.logging.Logger;
+
+/** Global Data Browser helper
+ *  @author Kay Kasemir
+ */
+// TODO Rename
+public class Activator
+{
+    /** Logger for all Data Browser code */
+    public static final Logger logger = Logger.getLogger(Activator.class.getPackageName());
+}
