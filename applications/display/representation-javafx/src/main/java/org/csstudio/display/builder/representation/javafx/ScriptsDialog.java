19e7891026337d2ece45d860d3806e502c74f659
Kay Kasemir <kay.kasemir@gmail.com>
2018-02-06 09:38:57 -0500

    Import RulesDlg and ModalityHack update
    https://github.com/kasemir/org.csstudio.display.builder/pull/338

--- applications/display/representation-javafx/src/main/java/org/csstudio/display/builder/representation/javafx/ScriptsDialog.java
+++ applications/display/representation-javafx/src/main/java/org/csstudio/display/builder/representation/javafx/ScriptsDialog.java
@@ -21,6 +21,7 @@ import org.csstudio.display.builder.model.Widget;
 import org.csstudio.display.builder.model.properties.ScriptInfo;
 import org.csstudio.display.builder.model.properties.ScriptPV;
 import org.csstudio.display.builder.model.util.ModelThreadPool;
+import org.csstudio.display.builder.representation.javafx.widgets.JFXBaseRepresentation;
 import org.phoebus.ui.autocomplete.AutocompleteMenu;
 import org.phoebus.ui.autocomplete.PVAutocompleteMenu;
 import org.phoebus.ui.dialog.DialogHelper;
@@ -41,6 +42,7 @@ import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
@@ -217,6 +219,9 @@ public class ScriptsDialog extends Dialog<List<ScriptInfo>>
 
         setTitle(Messages.ScriptsDialog_Title);
         setHeaderText(Messages.ScriptsDialog_Info);
+
+        final Node node = JFXBaseRepresentation.getJFXNode(widget);
+        initOwner(node.getScene().getWindow());
 
         scripts.forEach(script -> script_items.add(ScriptItem.forInfo(script)));
         fixupScripts(0);
