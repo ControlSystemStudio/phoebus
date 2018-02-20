eb5b13eeb7564469fe1943d0430745ba62fe6289
Kay Kasemir <kay.kasemir@gmail.com>
2018-02-20 15:06:28 -0500

    Scan: Import model..

---
+++ applications/scan/model/src/main/java/org/csstudio/scan/command/ScanCommandFactory.java
@@ -0,0 +1,16 @@
+package org.csstudio.scan.command;
+
+import java.util.List;
+
+import org.w3c.dom.Node;
+
+public class ScanCommandFactory
+{
+
+    public List<ScanCommand> readCommands(Node firstChild)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    // TODO Implement with SimpleScanCommandFactory, then add SPI
+}
