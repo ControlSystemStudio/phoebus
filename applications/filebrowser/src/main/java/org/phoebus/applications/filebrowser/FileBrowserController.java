7c6a3e2d4d4ede1017db832ee0c2efd24ac00e23
Kunal Shroff <shroffk@gmail.com>
2018-03-15 16:19:28 -0400

    add a context menu with support for open and openWith actions

--- applications/filebrowser/src/main/java/org/phoebus/applications/filebrowser/FileBrowserController.java
+++ applications/filebrowser/src/main/java/org/phoebus/applications/filebrowser/FileBrowserController.java
@@ -10,8 +10,12 @@ import org.phoebus.ui.javafx.ImageCache;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
@@ -24,6 +28,7 @@ import javafx.stage.Stage;
 
 /**
  * Controller for the file browser app
+ * 
  * @author Kunal Shroff
  *
  */
@@ -35,14 +40,54 @@ public class FileBrowserController {
     Button browse;
     @FXML
     TreeView<File> treeView;
+    
+    private ContextMenu contextMenu;
 
     @FXML
     public void initialize() {
-        File file =  Paths.get(System.getProperty("user.home")).toFile();
+        File file = Paths.get(System.getProperty("user.home")).toFile();
         TreeItem<File> root = new FileTreeItem(file);
         path.setText(file.getAbsolutePath());
         treeView.setCellFactory(f -> new FileTreeCell());
         treeView.setRoot(root);
+
+        // Create ContextMenu
+        contextMenu = new ContextMenu();
+
+        MenuItem open = new MenuItem("Open");
+        open.setGraphic(ImageCache.getImageView(PhoebusApplication.class, "/icons/fldr_obj.png"));
+        open.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                ObservableList<TreeItem<File>> selectedItems = treeView.selectionModelProperty().getValue()
+                        .getSelectedItems();
+                selectedItems.forEach(s -> {
+                    File selection = s.getValue();
+                    if (selection.isFile()) {
+                        ApplicationLauncherService.openFile(selection, false, null);
+                    }
+                });
+            }
+        });
+        MenuItem openWith = new MenuItem("Open With");
+        openWith.setGraphic(ImageCache.getImageView(PhoebusApplication.class, "/icons/fldr_obj.png"));
+        openWith.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                ObservableList<TreeItem<File>> selectedItems = treeView.selectionModelProperty().getValue()
+                        .getSelectedItems();
+                selectedItems.forEach(s -> {
+                    File selection = s.getValue();
+                    if (selection.isFile()) {
+                        ApplicationLauncherService.openFile(selection, true,
+                                (Stage) treeView.getParent().getScene().getWindow());
+                    }
+                });
+            }
+        });
+        contextMenu.getItems().addAll(open, openWith);
     }
 
     private final class FileTreeItem extends TreeItem<File> {
@@ -94,11 +139,13 @@ public class FileBrowserController {
         }
     }
 
+
     private final class FileTreeCell extends TreeCell<File> {
         public FileTreeCell() {
             super();
         }
 
+        ImageView image = ImageCache.getImageView(PhoebusApplication.class, "/icons/fldr_obj.png");
         @Override
         protected void updateItem(File file, boolean empty) {
             super.updateItem(file, empty);
@@ -111,7 +158,6 @@ public class FileBrowserController {
                     setText(file.getAbsolutePath());
                 } else {
                     if (file.isDirectory()) {
-                        ImageView image = ImageCache.getImageView(PhoebusApplication.class, "/icons/fldr_obj.png");
                         setGraphic(image);
                     }
                     setText(file.getName());
@@ -120,20 +166,28 @@ public class FileBrowserController {
 
         }
     }
-    
+
     @FXML
     public void createContextMenu(ContextMenuEvent e) {
-        
+        treeView.setContextMenu(null);
+        ObservableList<TreeItem<File>> selectedItems = treeView.selectionModelProperty().getValue().getSelectedItems();
+        if (selectedItems.stream().allMatch(item -> {
+            return item.getValue().isFile();
+        })) {
+            treeView.setContextMenu(contextMenu);
+        }
     }
-    
+
     @FXML
     public void handleMouseClickEvents(MouseEvent e) {
-        if(e.getClickCount() == 2) {
-            ObservableList<TreeItem<File>> selectedItems = treeView.selectionModelProperty().getValue().getSelectedItems();
-            selectedItems.forEach(s ->{
+        if (e.getClickCount() == 2) {
+            ObservableList<TreeItem<File>> selectedItems = treeView.selectionModelProperty().getValue()
+                    .getSelectedItems();
+            selectedItems.forEach(s -> {
                 File selection = s.getValue();
-                if(selection.isFile()) {
-                    ApplicationLauncherService.openFile(selection, true, (Stage) treeView.getParent().getScene().getWindow());
+                if (selection.isFile()) {
+                    ApplicationLauncherService.openFile(selection, true,
+                            (Stage) treeView.getParent().getScene().getWindow());
                 }
             });
         }
@@ -145,15 +199,15 @@ public class FileBrowserController {
         File newRootFile = p.toFile();
         treeView.setRoot(new FileTreeItem(newRootFile));
     }
-    
+
     @FXML
     public void browseNewRoot() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Select Browser Root");
-        if(Paths.get(path.getText()).toFile().isDirectory()) {
+        if (Paths.get(path.getText()).toFile().isDirectory()) {
             directoryChooser.setInitialDirectory(Paths.get(path.getText()).toFile());
         }
-        File newRootFile = directoryChooser.showDialog(treeView.getParent().getScene().getWindow());        
+        File newRootFile = directoryChooser.showDialog(treeView.getParent().getScene().getWindow());
         path.setText(newRootFile.getAbsolutePath());
         treeView.setRoot(new FileTreeItem(newRootFile));
     }
