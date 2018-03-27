4e591fe44585462667fa03ac10802705bd43a0e6
Kay Kasemir <kay.kasemir@gmail.com>
2018-03-27 18:09:04 -0400

    Add scan-server-product

--- run_maven.sh
+++ run_maven.sh
@@ -3,5 +3,8 @@
 mvn clean verify  -f dependencies/pom.xml
 # All but first run can use -o for offline
 #
-mvn -o -DskipTests clean install 
+mvn -DskipTests clean install 
 (cd phoebus-product/target; java -jar product-0.0.1-SNAPSHOT.jar  --add-modules=ALL-SYSTEM )
+
+# Or:
+# (cd scanserver-product/target; java -jar scan-server-product-0.0.1-SNAPSHOT.jar )
