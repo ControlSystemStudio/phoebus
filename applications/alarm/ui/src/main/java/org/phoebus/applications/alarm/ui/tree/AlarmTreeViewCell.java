aeee8b8828137bd46fee5ffc13e589308f1b1649
Kay Kasemir <kay.kasemir@gmail.com>
2018-05-03 16:04:42 -0400

    Alarm Tree: 'disabled' icon

--- applications/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/tree/AlarmTreeViewCell.java
+++ applications/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/tree/AlarmTreeViewCell.java
@@ -15,6 +15,7 @@ import org.phoebus.applications.alarm.model.SeverityLevel;
 import org.phoebus.applications.alarm.ui.AlarmUI;
 
 import javafx.scene.control.TreeCell;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 
@@ -43,6 +44,7 @@ class AlarmTreeViewCell extends TreeCell<AlarmTreeItem<?>>
                 final ClientState state = leaf.getState();
 
                 final StringBuilder text = new StringBuilder();
+                final Image icon;
                 text.append("PV: ").append(leaf.getName());
 
                 if (leaf.isEnabled()  &&  !state.isDynamicallyDisabled())
@@ -58,14 +60,16 @@ class AlarmTreeViewCell extends TreeCell<AlarmTreeItem<?>>
                                 .append(")");
                     }
                     setTextFill(AlarmUI.getColor(state.severity));
+                    icon = AlarmUI.getIcon(state.severity);
                 }
                 else
                 {
                     text.append(" (disabled)");
                     setTextFill(Color.GRAY);
+                    icon = AlarmUI.disabled_icon;
                 }
                 setText(text.toString());
-                setGraphic(new ImageView(AlarmUI.getIcon(state.severity)));
+                setGraphic(icon == null ? null : new ImageView(icon));
             }
             else
             {
