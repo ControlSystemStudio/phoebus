edde1c21d653c2c6bbabc67e36c8e716d1e48380
training.Evan <smithej@ornl.gov>
2018-05-22 16:10:45 -0400

    Added basic testing functionality. Now parses xml.

--- applications/alarm/model/src/main/java/org/phoebus/applications/alarm/model/xml/XmlModelReader.java
+++ applications/alarm/model/src/main/java/org/phoebus/applications/alarm/model/xml/XmlModelReader.java
@@ -1,6 +1,35 @@
 package org.phoebus.applications.alarm.model.xml;
 
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class XmlModelReader
 {
+	public static final String TAG_CONFIG = "config";
 
+	// Parse the xml stream and load the stream into a document.
+	public void load(InputStream stream) throws Exception
+	{
+        final DocumentBuilder docBuilder =
+                DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        final Document doc = docBuilder.parse(stream);
+
+        buildModel(doc);
+	}
+
+	private void buildModel(final Document doc) throws Exception
+	{
+		// Check if it's a <config/>.
+        doc.getDocumentElement().normalize();
+        final Element root_node = doc.getDocumentElement();
+        if (!root_node.getNodeName().equals(TAG_CONFIG))
+            throw new Exception("Expected " + TAG_CONFIG + " but got " + root_node.getNodeName());
+        else
+        	System.out.println("config");
+	}
 }
--- applications/alarm/model/src/test/java/org/phoebus/applications/alarm/AlarmModelReaderTest.java
+++ applications/alarm/model/src/test/java/org/phoebus/applications/alarm/AlarmModelReaderTest.java
@@ -1,6 +1,64 @@
 package org.phoebus.applications.alarm;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.phoebus.applications.alarm.model.xml.XmlModelReader;
+
 public class AlarmModelReaderTest
 {
+	// XML Alarm Test Model
+	public static final String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+	+ "<config name=\"Test\">\n"
+	+ "  <component name=\"Area1\">\n"
+	+ "    <command>\n"
+	+ "      <title>Area1 Command Title 1</title>\n"
+	+ "      <details>Area1 Command Detail 1</details>\n"
+	+ "    </command>\n"
+	+ "    <command>\n"
+	+ "      <title>Area1 Command Title 2</title>\n"
+	+ "      <details>Area1 Command Detail 2</details>\n"
+	+ "    </command>\n"
+	+ "    <pv name=\"a1pv1\">\n"
+	+ "       <description>a1pv1 description</description>\n"
+	+ "       <enabled>true</enabled>\n"
+	+ "       <latching>true</latching>\n"
+	+ "       <annunciating>true</annunciating>\n"
+	+ "       <delay>4</delay>\n"
+	+ "       <count>5</count>\n"
+	+ "       <filter>a1pv1 filter</filter>\n"
+	+ "    </pv>"
+	+ "    <pv name=\"a1pv2\">\n"
+	+ "      <description>a1pv2 description</description>\n"
+	+ "      <enabled>true</enabled>\n"
+	+ "      <latching>true</latching>\n"
+	+ "      <annunciating>true</annunciating>\n"
+	+ "    </pv>\n"
+	+ "  </component>\n"
+	+ "  <component name=\"Area2\">\n"
+	+ "    <component name=\"Area3\">\n"
+	+ "      <pv name=\"a3pv1\">\n"
+	+ "        <description>a3pv1 description</description>\n"
+	+ "        <enabled>true</enabled>\n"
+	+ "        <latching>true</latching>\n"
+	+ "        <annunciating>true</annunciating>\n"
+	+ "      </pv>\n"
+	+ "    </component>\n"
+	+ "    <pv name=\"a2pv1\">\n"
+	+ "      <description>a2pv1 description</description>\n"
+	+ "      <enabled>true</enabled>\n"
+	+ "      <latching>true</latching>\n"
+	+ "      <annunciating>true</annunciating>\n"
+	+ "    </pv>\n"
+	+ "  </component>\n"
+	+ "</config>\n";
 
+	@Test
+	public void testAlarmModelReader() throws Exception
+	{
+		final XmlModelReader reader = new XmlModelReader();
+		final InputStream input = new ByteArrayInputStream(xml.getBytes("UTF-8"));
+		reader.load(input);
+	}
 }
