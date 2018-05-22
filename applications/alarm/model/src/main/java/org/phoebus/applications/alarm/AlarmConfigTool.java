ccb730c5c351ecda268ed38357013ba95b75adf9
training.Evan <smithej@ornl.gov>
2018-05-22 13:27:46 -0400

    Refactored command line parsing and command line error messages.
    The command line arguments are now parsed straight from the args[] array
    instead of copying them into a list and removing them as they are each
    handled.

--- applications/alarm/model/src/main/java/org/phoebus/applications/alarm/AlarmConfigTool.java
+++ applications/alarm/model/src/main/java/org/phoebus/applications/alarm/AlarmConfigTool.java
@@ -2,9 +2,6 @@ package org.phoebus.applications.alarm;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
 
 import org.phoebus.applications.alarm.client.AlarmClient;
 import org.phoebus.applications.alarm.client.AlarmClientListener;
@@ -30,19 +27,13 @@ public class AlarmConfigTool
 		System.out.println("AlarmToolConfig help menu. Usage defined below.\n");
 		System.out.println("\tTo print this menu: java AlarmToolConfig --help");
 		System.out.println("\n\tWhen using --export the 'wait time' argument refers to the amount of time the model must have been stable before it will be written to file.\n");
-		System.out.println("\tTo export model to a file: java AlarmToolConfig --export [output filename] [wait time]");
-		System.out.println("\tTo export model to output: java AlarmToolConfig --export stdout [wait time]");
+		System.out.println("\tTo export model to a file: java AlarmToolConfig --export output_filename wait_time");
+		System.out.println("\tTo export model to output: java AlarmToolConfig --export stdout wait_time");
 
 		// TODO: Uncomment when import is implemented.
 		//System.out.print("\tTo import model from a file: java AlarmToolConfig --import [import filename]");
 
 		System.exit(0);
-	}
-
-	private void argError()
-	{
-		System.out.println("Argument order error. Use --help for program usage info.");
-		System.exit(1);
 	}
 
 	private void setTimeout(final long time)
@@ -56,7 +47,6 @@ public class AlarmConfigTool
 
 		final AlarmClient client = new AlarmClient(AlarmDemoSettings.SERVERS, AlarmDemoSettings.ROOT);
         client.start();
-        TimeUnit.SECONDS.sleep(4);
 
         System.out.printf("Writing file after model is stable for %d seconds:\n", timeout/1000);
 
@@ -169,54 +159,62 @@ public class AlarmConfigTool
 	// Constructor. Handles parsing of command lines and execution of command line options.
 	private AlarmConfigTool(String[] args)
 	{
-		// TODO: Parse command line arguments
-		// TODO: Add command line argument for timeout
-		final ArrayList<String> argList = new ArrayList<>(Arrays.asList(args));
-		int index = -1;
-		if (argList.contains(new String("--help")))
+
+		int wait_time = 0;
+		for (int i = 0; i < args.length; i++)
 		{
-			help();
+			if (0 == args[i].compareTo("--help"))
+			{
+				help();
+			}
+			if (0 == args[i].compareTo("--export"))
+			{
+				i++;
+				if (i >= args.length)
+				{
+					System.out.println("ERROR: '--export' must be accompanied by an output file name and a wait time. Use --help for program usage info.");
+					System.exit(1);
+				}
+
+				final String filename = args[i];
+				i++;
+				if (i >= args.length)
+				{
+					System.out.println("'ERROR: --export' must be accompanied by an output file name and a wait time. Use --help for program usage info.");
+					System.exit(1);
+				}
+
+				try
+				{
+					wait_time = Integer.parseInt(args[i]);
+				}
+				catch (final NumberFormatException e)
+				{
+					System.out.println("ERROR: Wait time must be an integer value. Use --help for program usage info.");
+					System.exit(1);
+				}
+
+				setTimeout(wait_time);
+
+				try
+				{
+					exportModel(filename);
+				}
+				catch (final Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+			else if (0 == args[i].compareTo("--import"))
+			{
+				System.out.println("import not yet implemented.");
+			}
+			else
+			{
+				System.out.printf("Unrecognized command line option: %s\n", args[i]);
+				System.exit(1);
+			}
 		}
-
-		if (-1 != (index = argList.lastIndexOf(new String("--export"))))
-		{
-			argList.remove(index);
-			String filename = "";
-			try
-			{
-				filename = argList.get(index);
-			}
-			catch (final IndexOutOfBoundsException e)
-			{
-				argError();
-			}
-
-			argList.remove(index);
-
-			long wait_time = 0;
-			try
-			{
-				wait_time = Integer.parseInt(argList.get(index));
-			}
-			catch (final IndexOutOfBoundsException e)
-			{
-				argError();
-			}
-
-			setTimeout(wait_time);
-
-			argList.remove(index);
-
-			try
-			{
-				exportModel(filename);
-			} catch (final Exception e)
-			{
-				e.printStackTrace();
-			}
-
-		}
-
 	}
 
 	public static void main(String[] args)
