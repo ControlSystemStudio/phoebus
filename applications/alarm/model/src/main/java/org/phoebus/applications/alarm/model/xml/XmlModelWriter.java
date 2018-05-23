34950014ec16f7aab2dfa675e5e3fedf217f3843
training.Evan <smithej@ornl.gov>
2018-05-23 15:44:08 -0400

    Added name and copyright info.

--- applications/alarm/model/src/main/java/org/phoebus/applications/alarm/model/xml/XmlModelWriter.java
+++ applications/alarm/model/src/main/java/org/phoebus/applications/alarm/model/xml/XmlModelWriter.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.phoebus.applications.alarm.model.xml;
 
 import java.io.IOException;
@@ -13,6 +20,11 @@ import org.phoebus.applications.alarm.model.TitleDetail;
 import org.phoebus.framework.persistence.IndentingXMLStreamWriter;
 import org.phoebus.framework.persistence.XMLUtil;
 
+/**
+ *
+ * @author Evan Smith
+ *
+ */
 public class XmlModelWriter
 {
 
