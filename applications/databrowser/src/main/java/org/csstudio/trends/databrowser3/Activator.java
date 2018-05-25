cd3e0daa5cbba3482923ce3c1f6503a91c272993
Kay Kasemir <kay.kasemir@gmail.com>
2018-05-25 12:28:04 -0400

    Data Browser: Dedicated 'scroll' timer ..
    .. to prevent long-running archive lookup threads from blocking the
    scrolling.

--- applications/databrowser/src/main/java/org/csstudio/trends/databrowser3/Activator.java
+++ applications/databrowser/src/main/java/org/csstudio/trends/databrowser3/Activator.java
@@ -22,12 +22,20 @@ import javafx.scene.image.ImageView;
 /** Global Data Browser helper
  *  @author Kay Kasemir
  */
-// TODO Rename
 @SuppressWarnings("nls")
 public class Activator
 {
     /** Logger for all Data Browser code */
     public static final Logger logger = Logger.getLogger(Activator.class.getPackageName());
+
+    /** Dedicated timer for scrolling/updating plots.
+     *
+     *  <p>The {@link #thread_pool} can be used to <code>schedule</code> tasks,
+     *  but long running tasks may be scheduled on the same thread and thus delay each other.
+     *
+     *  <p>This timer is to be used for short-running tasks.
+     */
+    public static final ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor(new NamedThreadFactory("DataBrowserScroll"));
 
     /** Thread pool, mostly for fetching archived data
      *
@@ -39,7 +47,7 @@ public class Activator
     static
     {
         // After 10 seconds, delete all idle threads
-        thread_pool = Executors.newScheduledThreadPool(0, new NamedThreadFactory("DataBrowser"));
+        thread_pool = Executors.newScheduledThreadPool(5, new NamedThreadFactory("DataBrowser"));
        ((ThreadPoolExecutor)thread_pool).setKeepAliveTime(10, TimeUnit.SECONDS);
     }
 
@@ -61,4 +69,65 @@ public class Activator
         return new ImageView(getImage(base_name));
     }
 
+    /*
+    public static void main(String[] args) throws Exception
+    {
+        // Thread pool grows to allow 'unlimited' number of submitted tasks..
+        for (int i=0; i<10; ++i)
+        {
+            final int id = i;
+            thread_pool.submit(() ->
+            {
+                for (int j=0; j<5; ++j)
+                {
+                    System.out.println(id + " running on " + Thread.currentThread().getName());
+                    try
+                    {
+                        Thread.sleep(100);
+                    }
+                    catch (InterruptedException e)
+                    {
+                    }
+                }
+                System.out.println(Thread.currentThread().getName() + " running...");
+            });
+        }
+
+        // timer uses one thread for 'scheduled' items
+        // If Task A takes a long time, scheduled task B doesn't
+        // get to run until task A completes.
+        timer.schedule(() ->
+        {
+            System.out.println("Long task A ...");
+            try
+            {
+                TimeUnit.SECONDS.sleep(4);
+            }
+            catch (InterruptedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("Long task A Done.");
+        }, 1, TimeUnit.SECONDS);
+
+        timer.schedule(() ->
+        {
+            System.out.println("Long task B ...");
+            try
+            {
+                TimeUnit.SECONDS.sleep(2);
+            }
+            catch (InterruptedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("Long task B Done.");
+        }, 2, TimeUnit.SECONDS);
+
+
+        TimeUnit.SECONDS.sleep(10);
+    }
+    */
 }
