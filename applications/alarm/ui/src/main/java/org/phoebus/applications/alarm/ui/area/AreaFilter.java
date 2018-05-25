eaae8d33ff51f746a20657452ec22ac619b36735
training.Evan <smithej@ornl.gov>
2018-05-25 15:51:09 -0400

    Removed hash map. There doesn't seem to be a need for it.

--- applications/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/area/AreaFilter.java
+++ applications/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/area/AreaFilter.java
@@ -1,26 +1,18 @@
 package org.phoebus.applications.alarm.ui.area;
 
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-
-import org.phoebus.applications.alarm.model.AlarmTreeItem;
+import org.phoebus.applications.alarm.model.AlarmTreePath;
 
 public class AreaFilter
 {
-	// Map associating components to their paths.
-	private final ConcurrentHashMap<String, AlarmTreeItem<?>> path2comp = new ConcurrentHashMap<>();
 	private int level = 2;
-	//private final Set<<AlarmTreeItem<?>> items_to_update = new LinkedHashSet<>();
 
-	public AreaFilter(List<AlarmTreeItem<?>> components, int level)
+	public AreaFilter(int level)
 	{
 		this.level = level;
-		for (final AlarmTreeItem<?> component : components)
-			path2comp.put(component.getPathName(), component);
 	}
 
-	private AlarmTreeItem<?> messageFilter(final String path)
+	public boolean filter(final String path)
 	{
-		return path2comp.get(path);
+		return (AlarmTreePath.splitPath(path).length == level);
 	}
 }
