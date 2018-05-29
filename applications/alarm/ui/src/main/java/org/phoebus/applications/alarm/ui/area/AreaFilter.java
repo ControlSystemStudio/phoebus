cf5d623090ac345bcd7d3bed4e676579b51f1ef9
training.Evan <smithej@ornl.gov>
2018-05-29 10:56:04 -0400

    Changed filter and levelCheck. Changed tests to match.
    filter now takes an alarmTreeItem as a message. It then uses a private
    method, levelCheck, to check the level of the passed message. Changed
    the tests to match this.

--- applications/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/area/AreaFilter.java
+++ applications/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/area/AreaFilter.java
@@ -1,20 +1,29 @@
 package org.phoebus.applications.alarm.ui.area;
 
+import org.phoebus.applications.alarm.model.AlarmTreeItem;
 import org.phoebus.applications.alarm.model.AlarmTreePath;
 
 public class AreaFilter
 {
 	private int level = 2;
 
-
 	public AreaFilter(int level)
 	{
-		// Set level to filter.
+		// Set level for levelCheck.
 		this.level = level;
 	}
 
-	public boolean filter(final String path)
+	// Check the level of the message by examining the number of levels in its path.
+	private boolean levelCheck(final String path)
 	{
 		return (AlarmTreePath.splitPath(path).length == level);
+	}
+
+	// Filter out messages not pertaining to the set level.
+	public boolean filter(AlarmTreeItem<?> message)
+	{
+		if (! levelCheck(message.getPathName()))
+			return false;
+		return true;
 	}
 }
--- applications/alarm/ui/src/test/java/org/phoebus/applications/alarm/AreaFilterTest.java
+++ applications/alarm/ui/src/test/java/org/phoebus/applications/alarm/AreaFilterTest.java
@@ -13,26 +13,29 @@ public class AreaFilterTest
 	public void testAreaFilter()
 	{
 		final int level = 2;
-		final AlarmClientNode root = new AlarmClientNode(null, "root");
 
+		// Build the test tree.
+		final AlarmClientNode root = new AlarmClientNode(null, "root");
 		final AlarmClientNode a1l2 = new AlarmClientNode(root, "Area 1 Level 2");
 		final AlarmClientNode a2l2 = new AlarmClientNode(root, "Area 2 Level 2");
 		final AlarmClientNode a3l2 = new AlarmClientNode(root, "Area 3 Level 2");
-
 		final AlarmClientNode a1l3 = new AlarmClientNode(a3l2, "Area 1 Level 3");
 
+		// Initialize an area filter to the desired level.
 		AreaFilter areaFilter = new AreaFilter(level);
 
-		assertTrue(areaFilter.filter(a1l2.getPathName()));
-		assertTrue(areaFilter.filter(a2l2.getPathName()));
-		assertTrue(areaFilter.filter(a3l2.getPathName()));
-		assertFalse(areaFilter.filter(a1l3.getPathName()));
+		// Test the message filtering.
+		assertTrue(areaFilter.filter(a1l2));
+		assertTrue(areaFilter.filter(a2l2));
+		assertTrue(areaFilter.filter(a3l2));
+		assertFalse(areaFilter.filter(a1l3));
 
+		// Try a different level.
 		areaFilter = new AreaFilter(level + 1);
 
-		assertFalse(areaFilter.filter(a1l2.getPathName()));
-		assertFalse(areaFilter.filter(a2l2.getPathName()));
-		assertFalse(areaFilter.filter(a3l2.getPathName()));
-		assertTrue(areaFilter.filter(a1l3.getPathName()));
+		assertFalse(areaFilter.filter(a1l2));
+		assertFalse(areaFilter.filter(a2l2));
+		assertFalse(areaFilter.filter(a3l2));
+		assertTrue(areaFilter.filter(a1l3));
 	}
 }
