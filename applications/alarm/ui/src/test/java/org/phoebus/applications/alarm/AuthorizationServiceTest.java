fd52328019644484e392a9d06da79389bf36390f
1es <1es@pc113080.ornl.gov>
2018-06-07 11:27:20 -0400

    Added tests for the user identity.

--- applications/alarm/ui/src/test/java/org/phoebus/applications/alarm/AuthorizationServiceTest.java
+++ applications/alarm/ui/src/test/java/org/phoebus/applications/alarm/AuthorizationServiceTest.java
@@ -44,87 +44,102 @@ public class AuthorizationServiceTest
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("8w4");
-
+        assertEquals("8w4", as.getUser());
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("9pj");
-
+        assertEquals("9pj", as.getUser());
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("ac7");
+        assertEquals("ac7", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("amp");
+        assertEquals("amp", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("c2y");
+        assertEquals("c2y", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("cmp");
+        assertEquals("cmp", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("fd4");
+        assertEquals("fd4", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("fg2");
+        assertEquals("fg2", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("gmc");
+        assertEquals("gmc", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("ky9");
+        assertEquals("ky9", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("kasemir");
+        assertEquals("kasemir", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("mkp");
+        assertEquals("mkp", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("nnl");
+        assertEquals("nnl", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("txg");
+        assertEquals("txg", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("udn");
+        assertEquals("udn", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         as.setUser("xgc");        
-        
+        assertEquals("xgc", as.getUser());
+
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
         
         // FULL
         as.setUser("1es");
+        assertEquals("1es", as.getUser());
 
         assertTrue(as.hasAuthorization("alarm_ack"));
         assertTrue(as.hasAuthorization("alarm_config"));
