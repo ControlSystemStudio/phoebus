b152bbd4d95d5636c757771e720cf7071b800d30
1es <smithej@ornl.gov>
2018-06-12 09:01:39 -0400

    Comments, doc, out of bounds excpetion added to remove(index).

--- applications/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/annunciator/NotifyingConcurrentArrayList.java
+++ applications/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/annunciator/NotifyingConcurrentArrayList.java
@@ -21,16 +21,30 @@ public class NotifyingConcurrentArrayList<T>
     /** Runnable to be executed on removal from array. */
     private Runnable onRemoval = null;
     
+    /**
+     * Set the onAddition runnable.
+     * <p> This runnable will be executed on successful addition to the list.
+     * @param r
+     */
     public void setOnAddition(Runnable r)
     {
         onAddition = r;
     }
     
+    /**
+     * Set the onRemoval runnable.
+     * <p> This runnable will be executed on successful removal from the list.
+     * @param r
+     */
     public void setOnRemoval(Runnable r)
     {
         onRemoval = r;
     }
     
+    /**
+     * Add an element to the end of the list.
+     * @param toAdd
+     */
     public void add(T toAdd)
     {
         array.add(toAdd);
@@ -39,22 +53,44 @@ public class NotifyingConcurrentArrayList<T>
             onAddition.run();
     }
     
+    /**
+     * Removes a specified item from the list. 
+     * <p>Safe to call if the list does not contain the specified element.
+     * @param toRemove
+     * @return <code>True</code> if the list contained the element specified to be removed, <code>false</code> otherwise.
+     */
     public boolean remove(T toRemove)
     {
         boolean removed = false;
         removed = array.remove(toRemove);
+        if (removed)
+            size--;
         if (removed && onRemoval != null)
             onRemoval.run();
         return removed;
     }
     
+    /**
+     * Removes an item from the list at the specified index.
+     * @param index
+     * @return <code>null</code> if the list is empty, the specified element otherwise.
+     * @throws IndexOutOfBoundsException
+     */
     public T remove(int index) throws IndexOutOfBoundsException
     {
-        T t = array.remove(index);
-        size--;
-        if (onRemoval != null)
-            onRemoval.run();
-        return t;
+        if (size > 0)
+        {
+            if (index > size-1)
+                throw new IndexOutOfBoundsException("Index out of range.");
+            
+            T t = array.remove(index);    
+            size--;
+            if (onRemoval != null)
+                onRemoval.run();
+            return t;
+        }
+        
+        return null;
     }
     
     /**
@@ -69,7 +105,7 @@ public class NotifyingConcurrentArrayList<T>
     }
     
     /**
-     * Gets the first element in the list/
+     * Gets the first element in the list.
      * @return <code>null</code> if empty, the first element otherwise.
      */
     public T front()
@@ -86,5 +122,14 @@ public class NotifyingConcurrentArrayList<T>
     public T popBack()
     {
         return remove(size-1);
+    }
+    
+    /**
+     * Pops the first element off the front of the list.
+     * @return <code>null</code> if the list is empty, the first element otherwise.
+     */
+    public T popFront()
+    {
+        return remove(0);
     }
 }
