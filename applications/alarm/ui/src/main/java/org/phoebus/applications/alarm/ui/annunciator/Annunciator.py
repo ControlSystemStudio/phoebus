a276669f986369a1697daac08c273d4d8c226728
1es <smithej@ornl.gov>
2018-06-14 08:35:22 -0400

    queue is now a priority queue. threshold is now a command line argument.

--- applications/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/annunciator/Annunciator.py
+++ applications/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/annunciator/Annunciator.py
@@ -11,17 +11,20 @@ config = 'Acclerator'
 
 argParser = argparse.ArgumentParser()
 
+argParser.add_argument("threshold", type=int, help="The number of messages that the message queue can accumulate before a generic message, 'There are N new messages', is annunciated.")
 argParser.add_argument("-server", "--server", help="The server name that the annunciator will listen to.")
 argParser.add_argument("-config", "--config", help="The config name that the annunciator will listen to.")
 
 args = argParser.parse_args()
+# Threshold of message count that causes a generic notification
+threshold = args.threshold
 if args.server:
     server = args.server
 if args.config:
     config = args.config
 
-print("Started with server value: {}".format(server))
-print("Started with config value: {}".format(config))
+print("Annunciator started with server value: {}".format(server))
+print("Annunciator started with config value: {}".format(config))
 
 # Kafka spreads messages across groups so group.id should be unique so that every 
 # alarm listener gets all of the messages.
@@ -36,16 +39,13 @@ print("Connected to topic {}Talk".format(config))
 
 # Message queue and accompanying lock.
 queueLock = threading.Lock()
-messageQueue = Queue.Queue()
+messageQueue = Queue.PriorityQueue()
 
 # Condition variable signifying that there are messages to annunciate.
 annunciateCondition = threading.Condition()
 
 # Boolean value, should the annunciator run?
 run = True
-
-# Threshold of message count that causes a generic notification
-threshold = 3
 
 # Annunciator, runs in its own thread, acts as consumer to messageQueue.
 class annunciatorThread(threading.Thread):
@@ -59,21 +59,35 @@ class annunciatorThread(threading.Thread):
         while (run):
             queueLock.acquire()
             size = messageQueue.qsize()
+            
             if size > threshold:
                 messageQueue.clear()
-                ex = "echo \"There are {} new messages.\" | festival --tts".format(size)
-                subprocess.call(ex, shell=True)
+                handleNMessages(size)
+            
             else:
                 while not messageQueue.empty():
-                    message = messageQueue.get()
-                    Str = message.key().decode('utf-8') + ' Alarm: ' + message.value().decode('utf-8')
-                    ex = "echo \"{}\" | festival --tts".format(Str)
-                    subprocess.call(ex, shell=True)
+                    messageWithPriority = messageQueue.get()
+                    handleMessage(messageWithPriority[1])
             
             queueLock.release()
             
             annunciateCondition.acquire()
             annunciateCondition.wait()
+
+    # Annunciates "There are N new messages"
+    def handleNMessages(N):
+        ex = "echo \"There are {} new messages.\" | festival --tts".format(size)
+        subprocess.call(ex, shell=True)
+    
+    # Annunciates message
+    def handleMessage(message):
+        toSay = message.value().decode('utf-8')
+        
+        if not toSay.startswith("*"):
+            toSay = message.key().decode('utf-8') + 'Alarm: ' + toSay
+        
+        ex = "echo \"{}\" | festival --tts".format(Str)
+        subprocess.call(ex, shell=True)
 
 # Add a message to the queue.                         
 def enqueueMessage(message):
@@ -81,16 +95,28 @@ def enqueueMessage(message):
 
 # Add the message to the queue in another thread. Notify all waiting consumer threads.
 def messageProducer(message):
+    messagePriority = getMessagePriority(message)
     queueLock.acquire()
-    messageQueue.put(message)
+    messageQueue.put((priority, message))
     annunciateCondition.acquire()
     annunciateCondition.notify_all()
     annunciateCondition.release()
     queueLock.release()
 
+severites = ["OK",  "MINOR_ACK",  "MAJOR_ACK", "INVALID_ACK", "UNDEFINED_ACK", "MINOR", "MAJOR", "INVALID", "UNDEFINED"]
+# Determine the priority of the message based on the alarm severity.
+def getMessagePriority(message):
+    severity = message.key().decode('utf-8')
+    try:
+        priority = severities.index(severity)
+    except ValueError:
+        return 0
+    return priority
+
 annunciator = annunciatorThread()
 annunciator.start()
 print("Started the annunciator.")
+print("Beginning to poll for messages.")
 while True:
     msg = c.poll(sys.float_info.max)
     
