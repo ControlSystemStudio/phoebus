59b74f72aebef797172121090f61737a8077c6ec
Kay Kasemir <kay.kasemir@gmail.com>
2018-06-21 15:33:05 -0400

    Fix default image for Picture and Symbol widgets
    #158

--- applications/display/model/src/main/java/org/csstudio/display/builder/model/widgets/PictureWidget.java
+++ applications/display/model/src/main/java/org/csstudio/display/builder/model/widgets/PictureWidget.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015-2016 Oak Ridge National Laboratory.
+ * Copyright (c) 2015-2018 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,7 +35,7 @@ import org.w3c.dom.Text;
 @SuppressWarnings("nls")
 public class PictureWidget extends VisibleWidget
 {
-    public final static String default_pic = "/icons/default_picture.png"; //$NON-NLS-1$
+    public final static String default_pic = "examples:/icons/default_picture.png";
 
     /** Widget descriptor */
     public static final WidgetDescriptor WIDGET_DESCRIPTOR =
