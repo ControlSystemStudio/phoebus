13ab449369eedd7cf48e1a395952273e81b5b2a9
Kay Kasemir <kay.kasemir@gmail.com>
2018-06-29 15:20:35 -0400

    Filebrowser: Add menu entry, icon, ant build

---
+++ applications/filebrowser/src/main/java/org/phoebus/applications/filebrowser/FileBrowserMenuEntry.java
@@ -0,0 +1,35 @@
+package org.phoebus.applications.filebrowser;
+
+import org.phoebus.framework.spi.MenuEntry;
+import org.phoebus.framework.workbench.ApplicationService;
+import org.phoebus.ui.javafx.ImageCache;
+
+import javafx.scene.image.Image;
+
+@SuppressWarnings("nls")
+public class FileBrowserMenuEntry implements MenuEntry
+{
+    @Override
+    public String getName() {
+        return FileBrowserApp.Name;
+    }
+
+    @Override
+    public String getMenuPath()
+    {
+        return "Utility";
+    }
+
+    @Override
+    public Image getIcon()
+    {
+        return ImageCache.getImage(FileBrowserApp.class, "/icons/filebrowser.png");
+    }
+
+    @Override
+    public Void call() throws Exception
+    {
+        ApplicationService.createInstance(FileBrowserApp.Name);
+        return null;
+    }
+}
