8edc5eedd46f3d7bb980cc1299ae6de7fd7d294f
Kunal Shroff <kunalshroff9@gmail.com>
2018-07-16 15:56:05 -0400

    #218 adda helper for the elastic operations

---
+++ services/alarm-logger/src/main/java/org/phoebus/alarm/logging/ElasticClientHelper.java
@@ -0,0 +1,103 @@
+/**
+ * 
+ */
+package org.phoebus.alarm.logging;
+
+import static org.phoebus.alarm.logging.AlarmLoggingService.logger;
+import static org.phoebus.alarm.logging.PropertiesHelper.getProperties;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.Level;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author Kunal Shroff {@literal <kunalshroff9@gmail.gov>}
+ *
+ */
+public class ElasticClientHelper {
+
+    private static RestHighLevelClient client;
+    private static ElasticClientHelper instance;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private ElasticClientHelper() {
+        try {
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                logger.info("Shutting down the ElasticClientHelper.");
+                if (client != null) {
+                    try {
+                        client.close();
+                    } catch (IOException e) {
+                        logger.log(Level.WARNING, "Failed to close the elastic rest client", e);
+                        e.printStackTrace();
+                    }
+                }
+            }));
+            client = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(getProperties().getProperty("es_host", "localhost"),
+                            Integer.valueOf(getProperties().getProperty("es_port", "9200")))));
+        } catch (Exception e) {
+            try {
+                client.close();
+            } catch (IOException ex) {
+                logger.log(Level.WARNING, "Failed to close the elastic rest client", ex);
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static ElasticClientHelper getInstance() {
+        if (instance == null) {
+            instance = new ElasticClientHelper();
+        }
+        return instance;
+    }
+
+    public RestHighLevelClient getClient() {
+        return client;
+    }
+
+    /**
+     * Check if an index exists with the given name Note: this is an synchronous
+     * call
+     * 
+     * @param indexName
+     * @return true if index exists
+     * @throws IOException
+     */
+    public boolean indexExists(String indexName) {
+        GetIndexRequest request = new GetIndexRequest();
+        request.indices(indexName);
+        try {
+            return client.indices().exists(request);
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "Failed to query elastic", e);
+            return false;
+        }
+    }
+
+    public boolean indexAlarmStateDocument(String indexName, AlarmStateMessage alarmStateMessage) {
+        IndexRequest indexRequest = new IndexRequest(indexName.toLowerCase(), "alarm");
+        try {
+            System.out.println(Arrays.toString(alarmStateMessage.sourceMap().entrySet().toArray()));
+            indexRequest.source(alarmStateMessage.sourceMap());
+            IndexResponse indexResponse = client.index(indexRequest);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
