9e69a574d63578507ddce1131e601de7b1a54eb6
Kay Kasemir <kay.kasemir@gmail.com>
2018-07-18 13:32:50 -0400

    Databrowser: Remove split section when 'search', .. are hidden
    Used to keep the empty split item, which left the split devider as an
    odd border

--- app/databrowser/src/main/java/org/csstudio/trends/databrowser3/ui/Perspective.java
+++ app/databrowser/src/main/java/org/csstudio/trends/databrowser3/ui/Perspective.java
@@ -293,7 +293,7 @@ public class Perspective extends SplitPane
     private void autoMinimize(final TabPane tabs, final SplitPane pane, final double pos)
     {
         if (tabs.getTabs().isEmpty())
-            pane.setDividerPositions(pos);
+            pane.getItems().remove(tabs);
     }
 
     /** Show search tab:
@@ -303,6 +303,12 @@ public class Perspective extends SplitPane
     private void showSearchTab()
     {
         createSearchTab();
+
+        if (! getItems().contains(left_tabs))
+        {
+            getItems().add(0, left_tabs);
+            setDividerPositions(0.2);
+        }
 
         // If tab not on screen, add it
         if (! left_tabs.getTabs().contains(search_tab))
@@ -337,6 +343,11 @@ public class Perspective extends SplitPane
         bottom_tabs.getSelectionModel().select(tab);
 
         // Assert that the tabs section is visible
+        if (! plot_and_tabs.getItems().contains(bottom_tabs))
+        {
+            plot_and_tabs.getItems().add(bottom_tabs);
+            plot_and_tabs.setDividerPositions(0.8);
+        }
         if (plot_and_tabs.getDividers().get(0).getPosition() > 0.9)
             plot_and_tabs.setDividerPositions(0.8);
 
