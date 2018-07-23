a1bcd7a5fe58b6da3d5b855804b575532cd7a650
Kay Kasemir <kay.kasemir@gmail.com>
2018-07-23 13:05:20 -0400

    core-email: Use 'email' in package name
    .. used to be 'mail' because of module name problems

--- core/email/src/main/java/org/phoebus/email/EmailPreferences.java
+++ core/email/src/main/java/org/phoebus/email/EmailPreferences.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
-package org.phoebus.mail;
+package org.phoebus.email;
 
 import org.phoebus.framework.preferences.PreferencesReader;
 
