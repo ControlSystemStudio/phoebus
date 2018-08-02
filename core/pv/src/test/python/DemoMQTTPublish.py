1ae06d4c667a32b21f03acc51c1754f6fde37973
1es <smithej@ornl.gov>
2018-08-02 12:05:50 -0400

    Publish 50 random numbers on 10 separate threads.

--- core/pv/src/test/python/DemoMQTTPublish.py
+++ core/pv/src/test/python/DemoMQTTPublish.py
@@ -1,12 +1,13 @@
 #
 # Author(s): Evan Smith, smithej@ornl.gov
 #
-import psutil
 import threading
+import time
+import random
 import paho.mqtt.client as mqtt
 
-topic = "cpuPercent"
-will_message = "cpuPercent has disconnected"
+topic = "random"
+will_message = "random has disconnected"
 
 client = mqtt.Client()
 
@@ -14,12 +15,20 @@ client.will_set(topic, will_message, 0, True)
 
 client.connect("localhost", 1883, 30)
 
-def publishCpuPercent():
-    while(True):
-        client.publish(topic, psutil.cpu_percent(interval=1), 0, False)
+topic_numbers = range(50)
 
-thread = threading.Thread(target=publishCpuPercent())
-thread.setName("CPU PERCENT THREAD")
-thread.setDaemon(True)
-thread.start()
+# Publish 50 random numbers.
+def publishRandomNumbers():
+    while(True):
+        thread_name = threading.current_thread().name
+        for i in topic_numbers:
+            client.publish("{}/{}/number{}".format(topic, thread_name, i), random.random(), 0, False)
+        time.sleep(1)
+
+# Publish 50 numbers from 10 threads each.
+for i in range(10):
+    thread = threading.Thread(target=publishRandomNumbers)
+    thread.setName("thread" + str(i))
+    thread.start()
+    
 
