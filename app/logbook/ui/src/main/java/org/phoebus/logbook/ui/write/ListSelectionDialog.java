5df6c692540d6c879f5ab32d382c305152e8a3a1
1es <smithej@ornl.gov>
2018-08-09 10:35:16 -0400

    Added search bar to search available items. Prepared for NLS.

--- app/logbook/ui/src/main/java/org/phoebus/logbook/ui/write/ListSelectionDialog.java
+++ app/logbook/ui/src/main/java/org/phoebus/logbook/ui/write/ListSelectionDialog.java
@@ -15,11 +15,13 @@ import java.util.function.Supplier;
 
 import org.phoebus.framework.preferences.PhoebusPreferenceService;
 import org.phoebus.ui.dialog.DialogHelper;
+import org.phoebus.ui.javafx.ClearingTextField;
 import org.phoebus.ui.javafx.ImageCache;
 
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -45,7 +47,30 @@ public class ListSelectionDialog extends Dialog<Boolean>
 {
     private static final int buttonWidth = 110, spacing = 10;
     private static final Font labelFont = new Font(16);
-
+    
+    /* Translations would apply to these strings. Can be moved to NLS */
+    private static final String ADD = "Add",                                
+                                ADD_TOOLTIP = "Add the selected items.",
+                                APPLY = "Apply",
+                                AVAILABLE = "Available", 
+                                CLEAR = "Clear",                                
+                                CLEAR_TOOLTIP = "Clear the selected items list.",
+                                NUM_SELECTED = "selected",
+                                REMOVE = "Remove",
+                                REMOVE_TOOLTIP = "Remove the selected items.",
+                                SEARCH = "Search available:",
+                                SELECTED = "Selected";
+    
+    /* Non NLS Strings */
+    private static final String ADD_ID = "addButton",
+                                ADD_ICON = "/icons/add.png",
+                                AVAILABLE_ID = "availableItems",
+                                CLEAR_ICON = "/icons/remove_multiple.png",
+                                CLEAR_ID = "clearButton",
+                                REMOVE_ICON = "/icons/delete.png",
+                                REMOVE_ID = "removeButton",
+                                SELECTED_ID = "selectedItems";
+    
     private final Function<String, Boolean> addSelected, removeSelected;
 
     private final ListView<String> availableItems, selectedItems;
@@ -71,8 +96,8 @@ public class ListSelectionDialog extends Dialog<Boolean>
             availableItems.getItems().remove(item);
 
         setTitle(title);
-
-        final ButtonType apply = new ButtonType("Apply", ButtonBar.ButtonData.OK_DONE);
+        
+        final ButtonType apply = new ButtonType(APPLY, ButtonBar.ButtonData.OK_DONE);
 
         getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL, apply);
         getDialogPane().setContent(formatContent());
@@ -86,25 +111,25 @@ public class ListSelectionDialog extends Dialog<Boolean>
         setResultConverter(button ->  button == apply);
     }
 
-    private HBox formatContent()
+    private VBox formatContent()
     {
         selectedItems.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         availableItems.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         selectedItems.setStyle("-fx-control-inner-background-alt: white");
         availableItems.setStyle("-fx-control-inner-background-alt: white");
-        selectedItems.setId("selectedItems");
-        availableItems.setId("availableItems");
+        selectedItems.setId(SELECTED_ID);
+        availableItems.setId(AVAILABLE_ID);
         
-        final Button add = new Button("Add", ImageCache.getImageView(ImageCache.class, "/icons/add.png"));
-        add.setTooltip(new Tooltip("Add the selected items."));
+        final Button add = new Button(ADD, ImageCache.getImageView(ImageCache.class, ADD_ICON));
+        add.setTooltip(new Tooltip(ADD_TOOLTIP));
         add.setOnAction(event -> addSelectedItems());
 
-        final Button remove = new Button("Remove", ImageCache.getImageView(ImageCache.class, "/icons/delete.png"));
-        remove.setTooltip(new Tooltip("Remove the selected items."));
+        final Button remove = new Button(REMOVE, ImageCache.getImageView(ImageCache.class, REMOVE_ICON));
+        remove.setTooltip(new Tooltip(REMOVE_TOOLTIP));
         remove.setOnAction(event -> removeItems(selectedItems.getSelectionModel().getSelectedItems()));
 
-        final Button clear  = new Button("Clear", ImageCache.getImageView(ImageCache.class, "/icons/remove_multiple.png"));
-        clear.setTooltip(new Tooltip("Clear the selected items list."));
+        final Button clear  = new Button(CLEAR, ImageCache.getImageView(ImageCache.class, CLEAR_ICON));
+        clear.setTooltip(new Tooltip(CLEAR_TOOLTIP));
         clear.setOnAction(event ->  removeItems(selectedItems.getItems()));
 
         add.setPrefWidth(buttonWidth);
@@ -115,9 +140,9 @@ public class ListSelectionDialog extends Dialog<Boolean>
         remove.setMinWidth(buttonWidth);
         clear.setMinWidth(buttonWidth);
         
-        add.setId("addButton");
-        remove.setId("removeButton");
-        clear.setId("clearButton");
+        add.setId(ADD_ID);
+        remove.setId(REMOVE_ID);
+        clear.setId(CLEAR_ID);
         
         // Note: For the followings, trying to initialize right away resulted in buttons that remained
         // disabled or would not re-enable.
@@ -151,15 +176,33 @@ public class ListSelectionDialog extends Dialog<Boolean>
         buttonsBox.setSpacing(10);
         buttonsBox.setAlignment(Pos.CENTER);
 
-        final Label itemsLabel = new Label("Available");
-        itemsLabel.setFont(labelFont);
+        final Label availableLabel = new Label(AVAILABLE);
+        availableLabel.setFont(labelFont);
         VBox.setVgrow(availableItems, Priority.ALWAYS);
-        final VBox availableBox = new VBox(spacing, itemsLabel, availableItems);
+        final VBox availableBox = new VBox(spacing, availableLabel, availableItems);
 
-        final Label selectedLabel = new Label("Selected");
+        availableItems.getSelectionModel().getSelectedItems().addListener((ListChangeListener<? super String>) c -> 
+        {
+            int selectedNum = availableItems.getSelectionModel().getSelectedItems().size();
+            if (selectedNum > 0)
+                Platform.runLater(() -> availableLabel.setText(AVAILABLE + " (" + selectedNum + " " + NUM_SELECTED + ")"));
+            else
+                Platform.runLater(() -> availableLabel.setText(AVAILABLE));
+        });
+        
+        final Label selectedLabel = new Label(SELECTED);
         selectedLabel.setFont(labelFont);
         VBox.setVgrow(selectedItems, Priority.ALWAYS);
         final VBox selectedBox = new VBox(spacing, selectedLabel, selectedItems);
+
+        selectedItems.getSelectionModel().getSelectedItems().addListener((ListChangeListener<? super String>) c -> 
+        {
+            int selectedNum = selectedItems.getSelectionModel().getSelectedItems().size();
+            if (selectedNum > 0)
+                Platform.runLater(() -> selectedLabel.setText(SELECTED + " (" + selectedNum + " " + NUM_SELECTED + ")"));
+            else
+                Platform.runLater(() -> selectedLabel.setText(SELECTED));
+        });
 
         HBox.setHgrow(availableBox, Priority.ALWAYS);
         HBox.setHgrow(selectedBox, Priority.ALWAYS);
@@ -167,12 +210,47 @@ public class ListSelectionDialog extends Dialog<Boolean>
         HBox.setMargin(availableBox, new Insets(5,  0, 10, 10));
         HBox.setMargin(buttonsBox,   new Insets(5,  0, 10,  0));
         HBox.setMargin(selectedBox,  new Insets(5, 10, 10,  0));
-
-        final HBox content = new HBox(spacing, availableBox, buttonsBox, selectedBox);
-        content.setAlignment(Pos.CENTER);
+        
+        final HBox selectionBox = new HBox(spacing, availableBox, buttonsBox, selectedBox);
+        selectionBox.setAlignment(Pos.CENTER);
+        
+        final ClearingTextField searchField = new ClearingTextField(); 
+        searchField.textProperty().addListener((changeListener, oldVal, newVal) -> 
+        {
+            searchAvailableItemsForSubstring(newVal);
+        });
+        
+        final Label searchLabel = new Label(SEARCH);
+        searchLabel.setFont(labelFont);
+        final HBox searchBox = new HBox(spacing, searchLabel, searchField);
+        searchBox.setAlignment(Pos.CENTER_LEFT);
+        HBox.setHgrow(searchField, Priority.ALWAYS);
+        
+        final VBox content = new VBox(spacing, searchBox, selectionBox);
+        VBox.setVgrow(selectionBox, Priority.ALWAYS);
+        VBox.setMargin(searchBox, new Insets(10, 10, 0, 10));
+        
         return content;
     }
 
+    private void searchAvailableItemsForSubstring(final String substring)
+    {
+        if (substring.trim().isEmpty())
+            availableItems.getSelectionModel().clearSelection();
+        else
+        {                
+            int size = availableItems.getItems().size();
+            for (int i = 0; i < size; i++)
+            {
+                final String item = availableItems.getItems().get(i);
+                if (item.contains(substring))
+                    availableItems.getSelectionModel().select(i);
+                else
+                    availableItems.getSelectionModel().clearSelection(i);
+            }
+        }
+    }
+    
     private void addSelectedItems()
     {
         // Can't modify list we're iterating over, so make a copy to iterate over.
