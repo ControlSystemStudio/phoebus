facbc6f497d1888660395528a706d4ec971b1d72
1es <smithej@ornl.gov>
2018-08-10 09:21:22 -0400

    AlarmTableUI: Adding a SeparatorMenuItem and PrintAction to context menu

--- app/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/table/AlarmTableUI.java
+++ app/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/table/AlarmTableUI.java
@@ -23,8 +23,10 @@ import org.phoebus.applications.email.actions.SendEmailAction;
 import org.phoebus.framework.persistence.Memento;
 import org.phoebus.logbook.ui.menu.SendLogbookAction;
 import org.phoebus.ui.application.SaveSnapshotAction;
+import org.phoebus.ui.docking.DockPane;
 import org.phoebus.ui.javafx.ClearingTextField;
 import org.phoebus.ui.javafx.ImageCache;
+import org.phoebus.ui.javafx.PrintAction;
 import org.phoebus.ui.javafx.Screenshot;
 import org.phoebus.ui.javafx.ToolbarHelper;
 import org.phoebus.ui.text.RegExHelper;
@@ -343,8 +345,11 @@ public class AlarmTableUI extends BorderPane
                 menu_items.add(new SeparatorMenuItem());
 
             if (AlarmUI.mayConfigure()  &&   selection.size() == 1)
+            {
                 menu_items.add(new ConfigureComponentAction(table, client, selection.get(0)));
-
+                menu_items.add(new SeparatorMenuItem());
+            }
+            menu_items.add(new PrintAction(DockPane.getActiveDockPane()));
             menu_items.add(new SaveSnapshotAction(table));
             menu_items.add(new SendEmailAction(table, "Alarm Snapshot", this::list_alarms, () -> Screenshot.imageFromNode(this)));
             menu_items.add(new SendLogbookAction(table, "Alarm Snapshot", this::list_alarms, () -> Screenshot.imageFromNode(this)));
