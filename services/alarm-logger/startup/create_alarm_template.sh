f8ae6eee6bbbae769ca3dea6d3c2c27c6808f534
1es <smithej@ornl.gov>
2018-08-24 14:56:15 -0400

    Adding scripts for creating/deleting elastic index templates

---
+++ services/alarm-logger/startup/create_alarm_template.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+if [ $# -ne 1 ]
+then
+    echo "Usage: create_alarm_index.sh accelerator"
+    exit 1
+fi
+
+es_host=localhost
+es_port=9200
+
+# Create the elastic index with the correct mapping for alarm state messages.
+# Create the Index
+# Set the mapping
+curl -XPUT http://${es_host}:${es_port}/_template/${1}_alarms_template -H 'Content-Type: application/json' -d'
+{
+  "index_patterns":["*"],
+  "mappings" : {  
+    "alarm" : {
+        "properties" : {
+          "APPLICATION-ID" : {
+            "type" : "text"
+          },
+          "config" : {
+            "type" : "keyword"
+          },
+          "pv" : {
+            "type" : "keyword"
+          },
+          "severity" : {
+            "type" : "keyword"
+          },
+          "message" : {
+            "type" : "text"
+          },
+          "value" : {
+            "type" : "text"
+          },
+          "time" : {
+            "type" : "date",
+            "format" : "yyyy-MM-dd HH:mm:ss.SSS"
+          },
+          "message_time" : {
+            "type" : "date",
+            "format" : "yyyy-MM-dd HH:mm:ss.SSS"
+          },
+          "current_severity" : {
+            "type" : "keyword"
+          },
+          "current_message" : {
+            "type" : "text"
+          },
+          "mode" : {
+            "type" : "text"
+          }
+        }
+      }
+  }
+}
+'
