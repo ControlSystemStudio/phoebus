e8cb78d8d70a7edb805c07d8f2bec001d9147bbe
Kay Kasemir <kay.kasemir@gmail.com>
2018-09-20 12:25:14 -0400

    Add AlertWithToggle

---
+++ core/ui/src/test/java/org/phoebus/ui/dialog/AlertWithToggleDemo.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.phoebus.ui.dialog;
+
+import javafx.application.Application;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
+/** Demo of {@link AlertWithToggle}
+ *  @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class AlertWithToggleDemo  extends Application
+{
+    @Override
+    public void start(final Stage stage)
+    {
+        AlertWithToggle dialog = new AlertWithToggle(AlertType.CONFIRMATION, "Test",
+                "This is a test\nAnother line");
+        System.out.println("Selected: " + dialog.showAndWait());
+        System.out.println("Hide from now on: " + dialog.isHideSelected());
+
+        dialog = new AlertWithToggle(AlertType.WARNING,
+                "Just using\nheader text",
+                "");
+        System.out.println("Selected: " + dialog.showAndWait());
+        System.out.println("Hide from now on: " + dialog.isHideSelected());
+
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
