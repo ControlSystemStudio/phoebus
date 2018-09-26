315ac4a1f9b223b95e46369f3cc35b3471c07137
1es <smithej@ornl.gov>
2018-09-26 15:24:34 -0400

    Demo3dViewer: Now using Viewer3dPane

--- app/3dViewer/src/test/java/org/phoebus/app/viewer3d/Demo3dViewer.java
+++ app/3dViewer/src/test/java/org/phoebus/app/viewer3d/Demo3dViewer.java
@@ -7,21 +7,8 @@
  *******************************************************************************/
 package org.phoebus.app.viewer3d;
 
-import java.io.File;
-import java.net.URL;
-
-import org.phoebus.ui.javafx.ImageCache;
-
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class Demo3dViewer extends Application
@@ -29,54 +16,9 @@ public class Demo3dViewer extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        TextField textField = new TextField();
-        Button fileButton = new Button(null, ImageCache.getImageView(ImageCache.class, "/icons/fldr_obj.png"));
-        FileChooser chooser = new FileChooser();
-        HBox toolbar = new HBox();
-        toolbar.getChildren().addAll(textField, fileButton);
-        Viewer3d viewer = new Viewer3d();
-        VBox root = new VBox();
-        Insets insets = new Insets(10);
+        Viewer3dPane viewerPane = new Viewer3dPane();
         
-        VBox.setMargin(viewer, insets);
-        
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Shape files (.shp)", "*.shp");
-        chooser.getExtensionFilters().add(extFilter);
-
-        fileButton.setOnAction(event -> 
-        {
-            File file = chooser.showOpenDialog(primaryStage);
-            if (null != file)
-            {
-                textField.setText(file.getPath());
-                viewer.buildStructure(file);
-            }
-        });
-        
-        HBox.setHgrow(textField, Priority.ALWAYS);
-        toolbar.setSpacing(10);
-        toolbar.setPadding(insets);
-        root.getChildren().addAll(toolbar, viewer);
-        
-        textField.setOnKeyPressed(event ->
-        {
-            if (event.getCode() == KeyCode.ENTER)
-            {
-                String pathway = textField.getText();
-                if (pathway.startsWith("examples:"))
-                {
-                    pathway = pathway.replaceFirst("examples:", "");
-                    URL resource = Viewer3d.class.getResource(pathway);
-                    if (null != resource)
-                        pathway = resource.getFile();
-                }
-                File file = new File(pathway);
-                if (file.exists() && !file.isDirectory())
-                    viewer.buildStructure(file);
-            }
-        });
-        
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(viewerPane);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
