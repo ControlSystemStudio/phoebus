3dabcc72cae58ffda245862a128764f16a4326c3
1es <smithej@ornl.gov>
2018-09-28 09:01:46 -0400

    Preferences: Creating preferences class for Viewer3dPane

---
+++ app/3dViewer/src/main/java/org/phoebus/app/viewer3d/Preferences.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.phoebus.app.viewer3d;
+
+import org.phoebus.framework.preferences.PreferencesReader;
+
+public class Preferences
+{
+    public static String READ_TIMEOUT = "read_timeout";
+    
+    public final static int read_timeout;
+    
+    static
+    {
+        final PreferencesReader prefs = new PreferencesReader(Preferences.class, "/viewer_3d_util_preferences.properties");
+        
+        read_timeout = prefs.getInt(READ_TIMEOUT);
+    }
+}
