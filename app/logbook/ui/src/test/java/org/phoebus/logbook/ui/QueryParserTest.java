f1c5e7b6b33dfd74b05e9f29f3ce539a27d28aa8
Kunal Shroff <kunalshroff9@gmail.com>
2018-10-17 10:23:55 -0400

    fixing the unit test for the parsing of time queries

--- app/logbook/ui/src/test/java/org/phoebus/logbook/ui/QueryParserTest.java
+++ app/logbook/ui/src/test/java/org/phoebus/logbook/ui/QueryParserTest.java
@@ -1,5 +1,6 @@
 package org.phoebus.logbook.ui;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
 import java.net.URI;
@@ -7,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
+import org.phoebus.logbook.ui.LogbookQueryUtil.Keys;
 
 /**
  * Test the parsing of the logbook query URI syntax
@@ -29,12 +31,13 @@ public class QueryParserTest {
 
     @Test
     public void timeParsing() {
+        long now = System.currentTimeMillis();
         URI uri = URI.create("logbook://?search=*Fault*Motor*&tag=operation&start=8hours&end=now");
         Map<String, String> queryParameters = LogbookQueryUtil.parseQueryURI(uri);
-        Map<String, String> expectedMap = new HashMap<String, String>();
-        expectedMap.put("search", "*Fault*Motor*");
-        expectedMap.put("tag", "operation");
-        assertEquals(expectedMap, queryParameters);
+        assertEquals("*Fault*Motor*", queryParameters.get(Keys.SEARCH.getName()));
+        assertEquals("operation", queryParameters.get(Keys.TAGS.getName()));
+        assertEquals(now, Double.valueOf(queryParameters.get(Keys.ENDTIME.getName())), 60*1000);
+        assertEquals(now-(8*60*60*1000), Double.valueOf(queryParameters.get(Keys.STARTTIME.getName())), 60*1000);
     }
 
     /**
