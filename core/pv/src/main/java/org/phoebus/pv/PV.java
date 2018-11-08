8739df1004e9928635fb1b029ba87b86a2235198
Kay Kasemir <kay.kasemir@gmail.com>
2018-11-08 15:42:19 -0500

    PVA PV: Log settings at CONFIG level

--- core/pv/src/main/java/org/phoebus/pv/PV.java
+++ core/pv/src/main/java/org/phoebus/pv/PV.java
@@ -40,7 +40,7 @@ import io.reactivex.Flowable;
 abstract public class PV
 {
     /** Suggested logger for all vtype.pv packages */
-    public static final Logger logger = Logger.getLogger(PV.class.getName());
+    public static final Logger logger = Logger.getLogger(PV.class.getPackageName());
 
     final private String name;
 
