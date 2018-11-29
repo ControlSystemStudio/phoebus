fb262d9e1031a7bf75808136c601aae98170a6e5
Kunal Shroff <kunalshroff9@gmail.com>
2018-11-29 11:05:10 -0500

    Record alarm commands in addition to state

--- services/alarm-logger/startup/create_alarm_template.sh
+++ services/alarm-logger/startup/create_alarm_template.sh
@@ -55,3 +55,32 @@ curl -XPUT http://${es_host}:${es_port}/_template/${1}_alarms_template -H 'Conte
   }
 }
 '
+
+# Create the elastic template with the correct mapping for alarm state messages.
+curl -XPUT http://${es_host}:${es_port}/_template/${1}_alarms_cmd_template -H 'Content-Type: application/json' -d'
+{
+  "index_patterns":["*_alarms*"],
+  "mappings" : {  
+    "alarm_cmd" : {
+        "properties" : {
+          "APPLICATION-ID" : {
+            "type" : "text"
+          },
+          "user" : {
+            "type" : "keyword"
+          },
+          "host" : {
+            "type" : "keyword"
+          },
+          "command" : {
+            "type" : "keyword"
+          },
+          "message_time" : {
+            "type" : "date",
+            "format" : "yyyy-MM-dd HH:mm:ss.SSS"
+          }
+        }
+      }
+  }
+}
+'
