45f2ef2107bb7002c59a4d2eff78bd4d91810d39
Kay Kasemir <kay.kasemir@gmail.com>
2018-12-11 11:42:32 -0500

    Autocomplete: Use popup with list instead of context menu

---
+++ core/ui/src/main/java/org/phoebus/ui/autocomplete/AutocompletePopup.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.phoebus.ui.autocomplete;
+
+import java.util.Collections;
+import java.util.List;
+
+import javafx.geometry.Bounds;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PopupControl;
+import javafx.scene.control.TextInputControl;
+import javafx.stage.Window;
+
+/** Popup for {@link AutocompleteItem}s
+ *  @author Kay Kasemir
+ */
+class AutocompletePopup extends PopupControl
+{
+    private final AutocompletePopupSkin skin;
+
+    public AutocompletePopup()
+    {
+        skin = new AutocompletePopupSkin(this);
+        setSkin(skin);
+
+        // Speed GC by releasing items, since menu stays in memory forever
+        setOnHidden(event -> clear());
+    }
+
+    public void clear()
+    {
+        setItems(Collections.emptyList());
+    }
+
+    public void setItems(final List<AutocompleteItem> items)
+    {
+        skin.setItems(items);
+    }
+
+    public void show(final TextInputControl field)
+    {
+        // Back when using a ContextMenu,
+        //  menu.show(field, Side.BOTTOM, 0, 0);
+        // held an `ownerNode` reference,
+        // so not passing the field sped up GC.
+        final Bounds bounds = field.localToScreen(field.getLayoutBounds());
+
+        // Min. width of 620 is useful for long sim PVs
+        ((ListView<?>) skin.getNode()).setPrefWidth(Math.max(620, bounds.getWidth()));
+
+        final Window window = field.getScene().getWindow();
+        show(window, bounds.getMinX(), bounds.getMaxY());
+    }
+}
