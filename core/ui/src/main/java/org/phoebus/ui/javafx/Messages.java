8ef3c988502e586759335a23dc1a2e93a2a276eb
pavel-ch <charvat.pavel@email.cz>
2018-12-12 11:26:11 +0100

    More text externalizations on core-ui.javafx

--- core/ui/src/main/java/org/phoebus/ui/javafx/Messages.java
+++ core/ui/src/main/java/org/phoebus/ui/javafx/Messages.java
@@ -8,22 +8,28 @@ import org.phoebus.framework.nls.NLS;
  */
 public class Messages
 {
-    public static String AddFileAttachments,
-                         AddImage,
-                         AddImages,
-                         AddImageTooltip,
-                         Clipboard,
-                         ClipboardTooltip,
-                         CSSWindow,
-                         CSSWindowTooltip,
-                         Files,
-                         Images,
-                         ImagesTitle,
-                         ImageFiles,
-                         NoImages,
-                         Print,
-                         Remove,
-                         RemoveImage;
+    public static String AddFileAttachments;
+    public static String AddImage;
+    public static String AddImageLog;
+    public static String AddImages;
+    public static String AddImageTooltip;
+    public static String AttachedFiles;
+    public static String AttachFile;
+    public static String Clipboard;
+    public static String ClipboardTooltip;
+    public static String CSSWindow;
+    public static String CSSWindowTooltip;
+    public static String Files;
+    public static String Images;
+    public static String ImagesTitle;
+    public static String ImageFiles;
+    public static String NoImages;
+    public static String Print;
+    public static String PrintErr;
+    public static String Remove;
+    public static String RemoveImage;
+    public static String RemoveSelected;
+    public static String RemoveSelectedFiles;
 
     static
     {
