60e0ed982518fe09cee03e01b84017c6875f709b
Kay Kasemir <kay.kasemir@gmail.com>
2018-12-12 10:29:35 -0500

    Display Editor: Log with package name. Log call trace

--- app/display/model/src/main/java/org/csstudio/display/builder/model/properties/PropertyChangeHandler.java
+++ app/display/model/src/main/java/org/csstudio/display/builder/model/properties/PropertyChangeHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015-2016 Oak Ridge National Laboratory.
+ * Copyright (c) 2015-2018 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -114,7 +114,7 @@ public abstract class PropertyChangeHandler<T extends Object>
     {
         final boolean removed = getListeners().remove(listener);
         if (! removed)
-            logger.log(Level.SEVERE, "Unknown listener " + listener);
+            logger.log(Level.SEVERE, "Unknown listener " + listener, new Exception("Unknown listener"));
     }
 
     /** Notify listeners of property change.
