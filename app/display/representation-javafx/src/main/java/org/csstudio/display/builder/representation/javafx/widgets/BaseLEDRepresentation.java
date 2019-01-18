a1734c94c13b7a7a0aaf8760bcd34b2624e83d4b
Kay Kasemir <kay.kasemir@gmail.com>
2019-01-18 12:54:30 -0500

    LED: Unmanaged

--- app/display/representation-javafx/src/main/java/org/csstudio/display/builder/representation/javafx/widgets/BaseLEDRepresentation.java
+++ app/display/representation-javafx/src/main/java/org/csstudio/display/builder/representation/javafx/widgets/BaseLEDRepresentation.java
@@ -58,6 +58,9 @@ abstract class BaseLEDRepresentation<LED extends BaseLEDWidget> extends RegionBa
         final Pane pane = new Pane();
         pane.setMinSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
         pane.setMaxSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
+
+        // Avoid expensive Node.notifyParentOfBoundsChange()
+        pane.setManaged(false);
         return pane;
     }
 
@@ -69,9 +72,13 @@ abstract class BaseLEDRepresentation<LED extends BaseLEDWidget> extends RegionBa
         else
             led = new Ellipse();
         led.getStyleClass().add("led");
+        led.setManaged(false);
+        
         label = new Label();
         label.getStyleClass().add("led_label");
         label.setAlignment(Pos.CENTER);
+        label.setManaged(false);
+        
         jfx_node.getChildren().addAll(led, label);
     }
 
@@ -200,7 +207,7 @@ abstract class BaseLEDRepresentation<LED extends BaseLEDWidget> extends RegionBa
             final int w = model_widget.propWidth().getValue();
             final int h = model_widget.propHeight().getValue();
 
-            jfx_node.setPrefSize(w, h);
+            jfx_node.resize(w, h);
             if (led instanceof Ellipse)
             {
                 final Ellipse ell = (Ellipse) led;
@@ -215,12 +222,14 @@ abstract class BaseLEDRepresentation<LED extends BaseLEDWidget> extends RegionBa
                 rect.setWidth(w);
                 rect.setHeight(h);
             }
-            label.setPrefSize(w, h);
+            label.resize(w, h);
         }
         if (dirty_content.checkAndClear())
         {
             led.setFill(value_color);
             label.setText(value_label);
+            // If text changed, is layout required?
+            // label.layout();
         }
     }
 }
