11f5ffd7e60cc91f552f24876d60b2b006d4e21f
Kunal Shroff <kunalshroff9@gmail.com>
2019-02-11 10:17:40 -0500

    Add support for adding a property to a list of channels

--- app/channel/views/src/main/java/org/phoebus/channel/views/ui/ChannelTableController.java
+++ app/channel/views/src/main/java/org/phoebus/channel/views/ui/ChannelTableController.java
@@ -1,22 +1,27 @@
 package org.phoebus.channel.views.ui;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+import org.phoebus.channel.views.ChannelTableApp;
 import org.phoebus.channelfinder.Channel;
 import org.phoebus.channelfinder.ChannelUtil;
+import org.phoebus.channelfinder.Property;
+import org.phoebus.channelfinder.utility.AddProperty2ChannelsJob;
+import org.phoebus.channelfinder.utility.ChannelSearchJob;
 import org.phoebus.framework.adapter.AdapterFactory;
 import org.phoebus.framework.adapter.AdapterService;
-import org.phoebus.framework.selection.Selection;
+import org.phoebus.framework.jobs.Job;
 import org.phoebus.framework.selection.SelectionService;
 import org.phoebus.ui.application.ContextMenuService;
+import org.phoebus.ui.dialog.ExceptionDetailsErrorDialog;
+import org.phoebus.ui.javafx.ImageCache;
 import org.phoebus.ui.spi.ContextMenuEntry;
 
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -32,6 +37,7 @@ import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.util.Callback;
 
@@ -49,10 +55,14 @@ public class ChannelTableController extends ChannelFinderController {
     Button search;
     @FXML
     TableView<Channel> tableView;
+    
+    private Collection<String> properties;
+    private Collection<String> tags;
 
     @SuppressWarnings("unchecked")
     @FXML
     public void initialize() {
+
         tableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         tableView.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
             if (newSelection != null) {
@@ -79,16 +89,40 @@ public class ChannelTableController extends ChannelFinderController {
         super.search(query.getText());
     }
 
-    @SuppressWarnings({ "unchecked", "unused", "rawtypes", "unused" })
+    private Job addPropertiesJob;
+
+
+    Image addProperties = ImageCache.getImage(ChannelTableApp.class, "/icons/add_properties.png");
+
     @FXML
     public void createContextMenu() {
 
         final ContextMenu contextMenu = new ContextMenu();
 //
 //        SelectionService.getInstance().setSelection(tableView, tableView.getSelectionModel().getSelectedItems());
+        MenuItem addProperty = new MenuItem("Add Property", new ImageView(addProperties));
+        addProperty.setOnAction(e -> {
 
+            // get the list of cf properties
+            properties = getClient().getAllProperties();
+            AddPropertyDialog dialog = new AddPropertyDialog(tableView, properties);
+            Optional<Property> result = dialog.showAndWait();
+            result.ifPresent(property -> {
+                if (addPropertiesJob != null) {
+                    addPropertiesJob.cancel();
+                }
+                List<String> channelNames = tableView.getSelectionModel().getSelectedItems().stream().map(ch -> {
+                    return ch.getName();
+                }).collect(Collectors.toList());
+                AddProperty2ChannelsJob.submit(getClient(),
+                        channelNames,
+                        property,
+                        (url, ex) -> ExceptionDetailsErrorDialog.openError("ChannelFinder Query Error", ex.getMessage(), ex));
+
+            });
+        });
         List<ContextMenuEntry> contextEntries = ContextMenuService.getInstance().listSupportedContextMenuEntries();
-
+        contextMenu.getItems().add(addProperty);
         contextMenu.getItems().add(new SeparatorMenuItem());
 
         contextEntries.forEach(entry -> {
