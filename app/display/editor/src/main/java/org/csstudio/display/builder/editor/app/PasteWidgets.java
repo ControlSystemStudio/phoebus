2815b2db1fca07aabd596a510ab3bd5c7a030a3c
Kay Kasemir <kay.kasemir@gmail.com>
2019-03-25 08:58:53 -0400

    Display Editor: Disable paste unless clipboard holds widgets
    https://github.com/kasemir/org.csstudio.display.builder/pull/514

--- app/display/editor/src/main/java/org/csstudio/display/builder/editor/app/PasteWidgets.java
+++ app/display/editor/src/main/java/org/csstudio/display/builder/editor/app/PasteWidgets.java
@@ -12,17 +12,22 @@ import org.csstudio.display.builder.editor.Messages;
 import org.phoebus.ui.javafx.ImageCache;
 
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.Clipboard;
 
 /** Menu entry to paste widgets from clipboard */
 @SuppressWarnings("nls")
 public class PasteWidgets extends MenuItem
 {
-    public PasteWidgets(EditorGUI gui)
+    public PasteWidgets(final EditorGUI gui)
     {
         super(Messages.Paste, ImageCache.getImageView(ImageCache.class, "/icons/paste.png"));
-        setOnAction(event ->
-        {
-            gui.pasteFromClipboard();
-        });
+
+        // Anything on clipboard?
+        // Does it look like widget XML?
+        final String xml = Clipboard.getSystemClipboard().getString();
+        if (xml != null  &&  xml.startsWith("<?xml")  &&   xml.contains("<display"))
+            setOnAction(event -> gui.pasteFromClipboard());
+        else
+            setDisable(true);
     }
 }
