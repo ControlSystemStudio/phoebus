b375e18186ed465bbdc7f8028e886989e9ab6a94
ky9 <ky9@cooper.ornl.gov>
2019-06-28 13:23:30 -0400

    Cursors: Avoid image cursor on Mac OS
    #721

--- app/display/representation-javafx/src/main/java/org/csstudio/display/builder/representation/javafx/Cursors.java
+++ app/display/representation-javafx/src/main/java/org/csstudio/display/builder/representation/javafx/Cursors.java
@@ -8,6 +8,7 @@
 package org.csstudio.display.builder.representation.javafx;
 
 import org.phoebus.ui.javafx.ImageCache;
+import org.phoebus.ui.javafx.PlatformInfo;
 
 import javafx.scene.Cursor;
 import javafx.scene.ImageCursor;
@@ -18,5 +19,15 @@ import javafx.scene.ImageCursor;
 @SuppressWarnings("nls")
 public class Cursors
 {
-    public static final Cursor NO_WRITE = new ImageCursor(ImageCache.getImage(JFXRepresentation.class, "/icons/blocked_cursor.png"), 16, 16);
+    public static final Cursor NO_WRITE;
+
+    static
+    {
+        // On Mac OS X, custom image cursor shows for a while, then turns into garbled image
+        // Mac 'DISAPPEAR' cursor happens to be suitable, so use that.
+        if (PlatformInfo.is_mac_os_x)
+            NO_WRITE = Cursor.DISAPPEAR;
+        else
+            NO_WRITE = new ImageCursor(ImageCache.getImage(JFXRepresentation.class, "/icons/blocked_cursor.png"), 16, 16);
+    }
 }
