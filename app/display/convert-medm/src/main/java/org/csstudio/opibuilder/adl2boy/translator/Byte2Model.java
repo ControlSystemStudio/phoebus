4ede4bcfb88a373be3b94088266e24c39318dbda
Kay Kasemir <kay.kasemir@gmail.com>
2019-08-05 12:51:16 -0400

    Byte monitor: Square; start/end/reverse

--- app/display/convert-medm/src/main/java/org/csstudio/opibuilder/adl2boy/translator/Byte2Model.java
+++ app/display/convert-medm/src/main/java/org/csstudio/opibuilder/adl2boy/translator/Byte2Model.java
@@ -20,19 +20,34 @@ public class Byte2Model extends AbstractADL2Model<ByteMonitorWidget> {
     }
 
     @Override
-    public void processWidget(ADLWidget adlWidget) throws Exception {
+    public void processWidget(ADLWidget adlWidget) throws Exception
+    {
         ByteMonitor byteWidget = new ByteMonitor(adlWidget);
-        if (byteWidget != null) {
-            setADLObjectProps(byteWidget, widgetModel);
-            setADLMonitorProps(byteWidget, widgetModel);
+        setADLObjectProps(byteWidget, widgetModel);
+        setADLMonitorProps(byteWidget, widgetModel);
 
-            widgetModel.propHorizontal().setValue(! byteWidget.getDirection().equals("down"));
-        }
+        widgetModel.propSquare().setValue(true);
+
+        final int start = Math.min(byteWidget.getStartBit(), byteWidget.getEndBit());
+        final int count = Math.abs(byteWidget.getEndBit() - byteWidget.getStartBit()) + 1;
+        widgetModel.propStartBit().setValue(start);
+        widgetModel.propNumBits().setValue(count);
+
+        // ByteMonitorWidget defaults to start bit at right resp. bottom.
+        // MEDM is reverted: Start bit left resp. top.
+        // So MEDM start=3, end=0 means: [3|2|1|0],
+        // which is the 'normal' mode for the display builder with start = 0, 4 bits, not reversed.
+        // If MEDM has start=0, end=3, this means: [0|1|2|3],
+        // which for the display builder with start = 0, 4 bits means "reversed".
+        widgetModel.propBitReverse().setValue(byteWidget.getEndBit() > byteWidget.getStartBit());
+
+        // Permitted values:  "right", "down"
+        widgetModel.propHorizontal().setValue(! byteWidget.getDirection().equals("down"));
     }
 
     @Override
-    public void makeModel(ADLWidget adlWidget,
-            Widget parentModel) {
+    public void makeModel(ADLWidget adlWidget, Widget parentModel)
+    {
         widgetModel = new ByteMonitorWidget();
         ChildrenProperty.getChildren(parentModel).addChild(widgetModel);
     }
