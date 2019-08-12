bfb7643b293c72f78fe891a25d4820a232f6da78
Kay Kasemir <kay.kasemir@gmail.com>
2019-08-12 08:40:39 -0400

    jdbc: Support mysql:replication..
    #766

--- core/framework/src/main/java/org/phoebus/framework/rdb/RDBInfo.java
+++ core/framework/src/main/java/org/phoebus/framework/rdb/RDBInfo.java
@@ -24,8 +24,8 @@ import java.util.logging.Level;
 @SuppressWarnings("nls")
 public class RDBInfo
 {
-    /** Start of MySQL URL */
-    private static final String JDBC_MYSQL = "jdbc:mysql://";
+    /** Start of MySQL URL, including " jdbc:mysql:replication://.." */
+    private static final String JDBC_MYSQL = "jdbc:mysql:";
 
     /** Start of PostgreSQL URL */
     private static final String JDBC_POSTGRESQL = "jdbc:postgresql://";
