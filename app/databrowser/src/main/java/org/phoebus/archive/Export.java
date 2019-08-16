87d9463e2b054e5d65db240e3ddac6951b1c1460
Kay Kasemir <kay.kasemir@gmail.com>
2019-08-16 11:02:17 -0400

    Export: Handle more parameters

--- app/databrowser/src/main/java/org/phoebus/archive/Export.java
+++ app/databrowser/src/main/java/org/phoebus/archive/Export.java
@@ -8,8 +8,8 @@
 package org.phoebus.archive;
 
 import java.io.FileInputStream;
-import java.time.Duration;
 import java.time.Instant;
+import java.time.temporal.TemporalAmount;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -30,6 +30,10 @@ import org.phoebus.archive.reader.ArchiveReaders;
 import org.phoebus.archive.vtype.Style;
 import org.phoebus.framework.jobs.BasicJobMonitor;
 import org.phoebus.framework.preferences.PropertyPreferenceLoader;
+import org.phoebus.util.time.SecondsParser;
+import org.phoebus.util.time.TimeInterval;
+import org.phoebus.util.time.TimeParser;
+import org.phoebus.util.time.TimeRelativeInterval;
 
 /** 'Main' for exporting archived data to files
  *  @author Kay Kasemir
@@ -49,6 +53,12 @@ public class Export
         System.out.println("-settings settings.xml                   -  Import settings from file, either exported XML or property file format");
         System.out.println("-archives                                -  Set archive URLs, separated by '*'");
         System.out.println("-list [pattern]                          -  List channel names, with optional pattern ('.', '*')");
+        System.out.println("-start '2019-01-02 08:00:00'             -  Start time");
+        System.out.println("-end '2019-02-03 16:15:20'               -  End time (defaults to 'now')");
+        System.out.println("-bin bin_count                           -  Export 'optimized' data for given bin count");
+        System.out.println("-linear HH:MM:SS                         -  Export linearly extrapolated data for time intervals");
+        System.out.println("-decimal precision                       -  Decimal format with given precision");
+        System.out.println("-exponential precision                   -  Exponential format with given precision");
         System.out.println("-export /path/to/file channel <channels> -  Export data for one or more channels into file");
     }
 
@@ -66,57 +76,145 @@ public class Export
             return;
         }
         String list = null;
+        Source source = Source.RAW_ARCHIVE;
+        int optimize_parameter = 800;
+
+        String start = "-1 week", end = "now";
+        Style style = Style.Decimal;
+        int precision = 3;
+        // TODO Command line options for all this
+        boolean with_status = true;
+
         String export = null;
         final Iterator<String> iter = args.iterator();
         while (iter.hasNext())
         {
-            while (iter.hasNext())
+            final String cmd = iter.next();
+            if (cmd.startsWith("-h"))
             {
-                final String cmd = iter.next();
-                if (cmd.startsWith("-h"))
+                help();
+                return;
+            }
+            else if (cmd.startsWith("-set"))
+            {
+                if (! iter.hasNext())
+                    throw new Exception("Missing -settings file name");
+                iter.remove();
+                final String filename = iter.next();
+                iter.remove();
+
+                System.out.println("Loading settings from " + filename);
+                if (filename.endsWith(".xml"))
+                    java.util.prefs.Preferences.importPreferences(new FileInputStream(filename));
+                else
+                    PropertyPreferenceLoader.load(new FileInputStream(filename));
+            }
+            else if (cmd.startsWith("-lis"))
+            {
+                iter.remove();
+                if (iter.hasNext())
                 {
+                    list = iter.next();
+                    iter.remove();
+                }
+                else
+                    list = "*";
+            }
+            else if (cmd.startsWith("-b"))
+            {
+                iter.remove();
+                if (! iter.hasNext())
+                {
+                    System.out.println("Missing -optimized bins");
                     help();
                     return;
                 }
-                else if (cmd.equals("-settings"))
+                source = Source.OPTIMIZED_ARCHIVE;
+                optimize_parameter = Integer.parseInt(iter.next());
+                iter.remove();
+            }
+            else if (cmd.startsWith("-lin"))
+            {
+                iter.remove();
+                if (! iter.hasNext())
                 {
-                    if (! iter.hasNext())
-                        throw new Exception("Missing -settings file name");
-                    iter.remove();
-                    final String filename = iter.next();
-                    iter.remove();
-
-                    System.out.println("Loading settings from " + filename);
-                    if (filename.endsWith(".xml"))
-                        java.util.prefs.Preferences.importPreferences(new FileInputStream(filename));
-                    else
-                        PropertyPreferenceLoader.load(new FileInputStream(filename));
+                    System.out.println("Missing -linear HH:MM:SS");
+                    help();
+                    return;
                 }
-                else if (cmd.equals("-list"))
+                source = Source.LINEAR_INTERPOLATION;
+                optimize_parameter = Math.max(1, (int)SecondsParser.parseSeconds(iter.next()));
+                iter.remove();
+            }
+            else if (cmd.startsWith("-dec"))
+            {
+                iter.remove();
+                if (! iter.hasNext())
                 {
-                    iter.remove();
-                    if (iter.hasNext())
-                    {
-                        list = iter.next();
-                        iter.remove();
-                    }
-                    else
-                        list = "*";
+                    System.out.println("Missing -decimal precision");
+                    help();
+                    return;
                 }
-                else if (cmd.equals("-export"))
+                style = Style.Decimal;
+                precision = Integer.parseInt(iter.next());
+                iter.remove();
+            }
+            else if (cmd.startsWith("-start"))
+            {
+                iter.remove();
+                if (! iter.hasNext())
                 {
-                    iter.remove();
-                    if (! iter.hasNext())
-                    {
-                        System.out.println("Missing -export filename");
-                        help();
-                        return;
-                    }
-                    export = iter.next();
-                    iter.remove();
-                    // Remaining args are channel names to export
-                    break;
+                    System.out.println("Missing -start YYYY-MM-DD HH:MM:SS");
+                    help();
+                    return;
                 }
+                start = iter.next();
+                iter.remove();
+            }
+            else if (cmd.startsWith("-end"))
+            {
+                iter.remove();
+                if (! iter.hasNext())
+                {
+                    System.out.println("Missing -end YYYY-MM-DD HH:MM:SS");
+                    help();
+                    return;
+                }
+                end = iter.next();
+                iter.remove();
+            }
+            else if (cmd.startsWith("-expon"))
+            {
+                iter.remove();
+                if (! iter.hasNext())
+                {
+                    System.out.println("Missing -exponential precision");
+                    help();
+                    return;
+                }
+                style = Style.Exponential;
+                precision = Integer.parseInt(iter.next());
+                iter.remove();
+            }
+            else if (cmd.startsWith("-expor"))
+            {
+                iter.remove();
+                if (! iter.hasNext())
+                {
+                    System.out.println("Missing -export filename");
+                    help();
+                    return;
+                }
+                export = iter.next();
+                iter.remove();
+                // Remaining args are channel names to export
+                break;
+            }
+            else
+            {
+                System.out.println("Unknown parameters " + args);
+                help();
+                return;
             }
         }
 
@@ -125,10 +223,10 @@ public class Export
         if (list != null)
         {
             System.out.println("# Channels matching pattern '" + list + "'");
-            for (ArchiveDataSource source : sources)
+            for (ArchiveDataSource arch : sources)
             {
-                System.out.println("# Archive: " + source.getUrl());
-                final ArchiveReader reader = ArchiveReaders.createReader(source.getUrl());
+                System.out.println("# Archive: " + arch.getUrl());
+                final ArchiveReader reader = ArchiveReaders.createReader(arch.getUrl());
                 for (String name : reader.getNamesByPattern(list))
                     System.out.println(name);
             }
@@ -138,19 +236,39 @@ public class Export
         {
             System.out.println("# Exporting: " + args.stream().collect(Collectors.joining(", ")));
             System.out.println("# To: " + export);
-            // TODO Command line options for all this
 
-            Instant end = Instant.now();
-            Instant start = end.minus(Duration.ofDays(365*20));
-            Source source = Source.RAW_ARCHIVE;
-            int optimize_parameter = 800;
-            int precision = 3;
-            boolean with_status = true;
+            TimeRelativeInterval range = null;
+            final Object s = TimeParser.parseInstantOrTemporalAmount(start);
+            final Object e = TimeParser.parseInstantOrTemporalAmount(end);
+            if (s instanceof Instant)
+            {
+                if (e instanceof Instant)
+                    range = TimeRelativeInterval.of((Instant)s, (Instant)e);
+                else if (e instanceof TemporalAmount)
+                    range = TimeRelativeInterval.of((Instant)s, (TemporalAmount)e);
+            }
+            else if (s instanceof TemporalAmount)
+            {
+                if (e instanceof Instant)
+                    range = TimeRelativeInterval.of((TemporalAmount) s, (Instant) e);
+                else if (e instanceof TemporalAmount)
+                    range = TimeRelativeInterval.of((TemporalAmount) s, (TemporalAmount) e);
+            }
 
+            if (range == null)
+            {
+                System.out.println("Invalid time range " + start + " .. " + end);
+                help();
+                return;
+            }
+
+            System.out.println("# Time range: " + range);
+
+            final TimeInterval abs_range = range.toAbsoluteInterval();
 
             final ValueFormatter formatter = with_status
-                                           ? new ValueWithInfoFormatter(Style.Decimal, precision)
-                                           : new ValueFormatter(Style.Decimal, precision);
+                                           ? new ValueWithInfoFormatter(style, precision)
+                                           : new ValueFormatter(style, precision);
             // Minimal model, just containing items to export
             final Model model = new Model();
             for (String name : args)
@@ -160,7 +278,7 @@ public class Export
                 model.addItem(item);
             }
             Consumer<Exception> error_handler = ex -> ex.printStackTrace();
-            final ExportJob job = new SpreadsheetExportJob(model, start, end, source, optimize_parameter, formatter, export, error_handler);
+            final ExportJob job = new SpreadsheetExportJob(model, abs_range.getStart(), abs_range.getEnd(), source, optimize_parameter, formatter, export, error_handler);
             final BasicJobMonitor monitor = new BasicJobMonitor()
             {
                 @Override
