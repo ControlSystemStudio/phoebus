26de717f538c4f4a293a7d2a0573d3437f150394
Kay Kasemir <kay.kasemir@gmail.com>
2019-09-24 14:20:48 -0400

    EDM Text: Convert into text entry or update depending on 'editing'

--- app/display/convert-edm/src/main/java/org/csstudio/display/converter/edm/widgets/Convert_activeXTextDspClass.java
+++ app/display/convert-edm/src/main/java/org/csstudio/display/converter/edm/widgets/Convert_activeXTextDspClass.java
@@ -12,7 +12,10 @@ import static org.csstudio.display.converter.edm.Converter.logger;
 import java.util.logging.Level;
 
 import org.csstudio.display.builder.model.Widget;
+import org.csstudio.display.builder.model.properties.CommonWidgetProperties;
+import org.csstudio.display.builder.model.widgets.PVWidget;
 import org.csstudio.display.builder.model.widgets.TextEntryWidget;
+import org.csstudio.display.builder.model.widgets.TextUpdateWidget;
 import org.csstudio.display.converter.edm.EdmConverter;
 import org.csstudio.opibuilder.converter.model.EdmWidget;
 import org.csstudio.opibuilder.converter.model.Edm_activeXTextDspClass;
@@ -23,17 +26,15 @@ import org.phoebus.ui.vtype.FormatOption;
  *  @author Matevz, Lei Hu, Xihui Chen et al - Original logic in Opi_.. converter
  */
 @SuppressWarnings("nls")
-public class Convert_activeXTextDspClass extends ConverterBase<TextEntryWidget>
+public class Convert_activeXTextDspClass extends ConverterBase<PVWidget>
 {
     public Convert_activeXTextDspClass(final EdmConverter converter, final Widget parent, final Edm_activeXTextDspClass r)
     {
         super(converter, parent, r);
 
-        convertColor(r.getBgColor(), widget.propBackgroundColor());
-        convertColor(r.getFgColor(), widget.propForegroundColor());
-        convertFont(r.getFont(), widget.propFont());
-
-        widget.propEnabled().setValue(r.isEditable());
+        convertColor(r.getBgColor(), widget.getProperty(CommonWidgetProperties.propBackgroundColor));
+        convertColor(r.getFgColor(), widget.getProperty(CommonWidgetProperties.propForegroundColor));
+        convertFont(r.getFont(), widget.getProperty(CommonWidgetProperties.propFont));
 
         if (r.isEditable())
         {
@@ -48,29 +49,32 @@ public class Convert_activeXTextDspClass extends ConverterBase<TextEntryWidget>
             widget.propPVName().setValue(convertPVName(r.getControlPv()));
         else
         {
-            logger.log(Level.WARNING, "Hiding activeXTextDsp (Text Entry) without PV");
+            logger.log(Level.WARNING, "Hiding activeXTextDsp without PV");
             widget.propVisible().setValue(false);
         }
 
         if (! r.isLimitsFromDb()  && r.getAttribute("precision").isExistInEDL())
-            widget.propPrecision().setValue(r.getPrecision());
-        widget.propShowUnits().setValue(r.isShowUnits());
+            widget.getProperty(CommonWidgetProperties.propPrecision).setValue(r.getPrecision());
+        widget.getProperty(CommonWidgetProperties.propShowUnits).setValue(r.isShowUnits());
         if (r.getFormat() != null)
         {
             if (r.getFormat().equals("exponential"))
-                widget.propFormat().setValue(FormatOption.EXPONENTIAL);
+                widget.getProperty(CommonWidgetProperties.propFormat).setValue(FormatOption.EXPONENTIAL);
             else if (r.getFormat().equals("decimal"))
-                widget.propFormat().setValue(FormatOption.DECIMAL);
+                widget.getProperty(CommonWidgetProperties.propFormat).setValue(FormatOption.DECIMAL);
             else if (r.getFormat().equals("hex"))
-                widget.propFormat().setValue(FormatOption.HEX);
+                widget.getProperty(CommonWidgetProperties.propFormat).setValue(FormatOption.HEX);
             else if (r.getFormat().equals("string"))
-                widget.propFormat().setValue(FormatOption.STRING);
+                widget.getProperty(CommonWidgetProperties.propFormat).setValue(FormatOption.STRING);
         }
     }
 
     @Override
-    protected TextEntryWidget createWidget(final EdmWidget edm)
+    protected PVWidget createWidget(final EdmWidget edm)
     {
-        return new TextEntryWidget();
+        if (((Edm_activeXTextDspClass) edm).isEditable())
+            return new TextEntryWidget();
+        else
+            return new TextUpdateWidget();
     }
 }
