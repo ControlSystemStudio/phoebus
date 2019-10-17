64c2e091f160c76f9869a10a0dbbd5eab677f872
Kunal Shroff <kunalshroff9@gmail.com>
2019-10-17 14:17:06 -0400

    Revert "removing the preference service since it is not used"
    This reverts commit bcfa08e7073fcf126ee54d25e2852dd1390fef60.

---
+++ core/framework/src/main/java/org/phoebus/framework/preferences/PhoebusPreferenceService.java
@@ -0,0 +1,32 @@
+package org.phoebus.framework.preferences;
+
+import java.util.prefs.Preferences;
+import java.util.prefs.PreferencesFactory;
+
+/**
+ *
+ * @author Kunal Shroff
+ *
+ */
+public class PhoebusPreferenceService implements PreferencesFactory {
+
+    /** Obtain preference node for a class
+     *  @param clazz Class
+     *  @return {@link Preferences} for that class
+     */
+    public static Preferences userNodeForClass(final Class<?> clazz)
+    {
+        return Preferences.userNodeForPackage(clazz).node(clazz.getSimpleName());
+    }
+
+    @Override
+    public Preferences userRoot() {
+        return FileSystemPreferences.getUserRoot();
+    }
+
+    @Override
+    public Preferences systemRoot() {
+        return FileSystemPreferences.getSystemRoot();
+    }
+
+}
