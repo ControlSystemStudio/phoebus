237bf7a6bee84fe98b04963c8c771c33f8b67da7
Kunal Shroff <kunalshroff9@gmail.com>
2019-10-31 12:35:32 -0400

    the value expression can handle a few basic widget property type

--- app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/ValueExpToPropertyBinding.java
+++ app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/ValueExpToPropertyBinding.java
@@ -1,7 +1,13 @@
 package org.csstudio.display.builder.runtime;
 
 import org.csstudio.display.builder.model.WidgetProperty;
+import org.csstudio.display.builder.model.properties.BooleanWidgetProperty;
+import org.csstudio.display.builder.model.properties.DoubleWidgetProperty;
+import org.csstudio.display.builder.model.properties.IntegerWidgetProperty;
+import org.csstudio.display.builder.model.properties.LongWidgetProperty;
+import org.csstudio.display.builder.model.properties.StringWidgetProperty;
 import org.csstudio.display.builder.model.rules.RuleInfo.ExpressionInfo;
+import org.csstudio.display.builder.model.util.VTypeUtil;
 import org.csstudio.display.builder.runtime.pv.RuntimePV;
 import org.csstudio.display.builder.runtime.pv.RuntimePVListener;
 import org.epics.vtype.VType;
@@ -16,7 +22,8 @@ public class ValueExpToPropertyBinding extends ExpToPropertyBinding
      * @param p
      * @param need_write_access
      */
-    public ValueExpToPropertyBinding(final WidgetRuntime<?> runtime, final ExpressionInfo<?> expression, final WidgetProperty<Object> p, final boolean need_write_access)
+    @SuppressWarnings({ "unchecked" })
+    public ValueExpToPropertyBinding(final WidgetRuntime<?> runtime, final ExpressionInfo<?> expression, final WidgetProperty p, final boolean need_write_access)
     {
         super(runtime,
                 expression,
@@ -24,12 +31,26 @@ public class ValueExpToPropertyBinding extends ExpToPropertyBinding
                 {
                     @Override
                     public void valueChanged(RuntimePV pv, VType value) {
-                        p.setValue(value);
+                        if (p instanceof BooleanWidgetProperty)
+                        {
+                            p.setValue(VTypeUtil.getValueBoolean(value));
+                        }
+                        else if (p instanceof StringWidgetProperty)
+                        {
+                            p.setValue(VTypeUtil.getValueString(value, true));
+                        }
+                        else if (p instanceof IntegerWidgetProperty
+                                || p instanceof LongWidgetProperty
+                                || p instanceof DoubleWidgetProperty)
+                        {
+                            p.setValue(VTypeUtil.getValueNumber(value));
+                        }
                     }
 
                 },
                 p,
                 need_write_access);
     }
+    
 
 }
