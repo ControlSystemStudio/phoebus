8cd29e74178b0a59eff25086b5ac0d14563c4455
Kay Kasemir <kay.kasemir@gmail.com>
2019-11-14 08:42:10 -0500

    Install ConsoleHandler only once

--- app/errlog/src/main/java/org/phoebus/applications/errlog/ErrLog.java
+++ app/errlog/src/main/java/org/phoebus/applications/errlog/ErrLog.java
@@ -29,6 +29,9 @@ public class ErrLog implements Closeable
     /** Handler for recognized lines sent to stdout resp. stderr */
     private static volatile Consumer<String> handle_stdout = ignore, handle_stderr = ignore;
 
+    /** Replacement {@link ConsoleHandler} that uses interpose */
+    private static Handler repl_console_handler = null;
+
     /** Prepare for using ErrLog
      *
      *  <p>Must be called early in the application startup process,
@@ -38,29 +41,24 @@ public class ErrLog implements Closeable
      */
     public static void prepare()
     {
-        // In principle we can take a copy of the current System.out and System.err
-        // values, later replace them with an interpose,
+        // In principle we can take a copy of the current System.out and System.err,
+        // later replace them with an interpose,
         // and finally either restore the original values,
         // or use the equivalent new PrintStream(new FileOutputStream(FileDescriptor.out));
         //
-        // But other libraries might latch on to some current value of System.out,
-        // so they would ignore our interpose,
-        // or keep writing to the interpose long after the ErrLog has been closed.
+        // But other libraries might latch on to System.out at any time,
+        // so they would ignore our interpose replacement,
+        // or keep writing to the interpose long after the ErrLog has been closed
+        // and we restored the original System.out/err.
         //
         // Therefore install an interpose early on, once and for all.
-
+        //
         // Note that these interposes will use the value of handle_stdout & handle_stderr
         // that's current whenever they're executing, i.e. those handlers
         // can change while the out and err streams remain unchanged.
         System.setOut(new PrintStream(new LineDetectingOutputInterpose(System.out, line -> handle_stdout.accept(line))));
         System.setErr(new PrintStream(new LineDetectingOutputInterpose(System.err, line -> handle_stderr.accept(line))));
     }
-
-    /** Root logger for replacing/restoring {@link ConsoleHandler} */
-    private static Logger root_logger = Logger.getLogger("");
-
-    private Handler orig_console_handler = null,
-                    repl_console_handler = null;
 
     /** Enable message line capture
      *  @param handle_stdout Called with each stdout line
@@ -73,43 +71,43 @@ public class ErrLog implements Closeable
         ErrLog.handle_stdout = handle_stdout;
         ErrLog.handle_stderr = handle_stderr;
 
-        // If there is a ConsoleHandler,
-        // it's been set to the original System.err.
-        for (Handler handler : root_logger.getHandlers())
-            if (handler instanceof ConsoleHandler)
-            {
-                orig_console_handler = handler;
-                break;
-            }
-        if (orig_console_handler != null)
+        // If prepare() is called before the Logger is ever used,
+        // as in org.phoebus.applications.errlog.Demo,
+        // then the ConsoleLogger also latches onto the prepared
+        // interpose.
+        // But when prepare() is called later, for example via
+        // ErrLogApp.start(), the ConsoleHandler latched onto
+        // the original System.err and needs to be replaced
+        if (repl_console_handler == null)
         {
-            // Remove that one...
-            root_logger.removeHandler(orig_console_handler);
-            // .. and install a new one that uses the updated System.err
-            repl_console_handler = new ConsoleHandler();
-            repl_console_handler.setLevel(orig_console_handler.getLevel());
-            repl_console_handler.setFormatter(orig_console_handler.getFormatter());
-            root_logger.addHandler(repl_console_handler);
+            Handler orig_console_handler = null;
+            final Logger root_logger = Logger.getLogger("");
+            for (Handler handler : root_logger.getHandlers())
+                if (handler instanceof ConsoleHandler)
+                {
+                    orig_console_handler = handler;
+                    break;
+                }
+            if (orig_console_handler != null)
+            {
+                // Remove that one...
+                root_logger.removeHandler(orig_console_handler);
+                // .. and install a new one that uses the updated System.err
+                repl_console_handler = new ConsoleHandler();
+                repl_console_handler.setLevel(orig_console_handler.getLevel());
+                repl_console_handler.setFormatter(orig_console_handler.getFormatter());
+                root_logger.addHandler(repl_console_handler);
+            }
         }
+        // else: ConsoleHandler already replaced
     }
 
     @Override
     public void close()
     {
-        // Restore ConsoleHandler
-        if (repl_console_handler != null)
-        {
-            root_logger.removeHandler(repl_console_handler);
-            repl_console_handler.close();
-            repl_console_handler = null;
-        }
-        if (orig_console_handler != null)
-        {
-            root_logger.addHandler(orig_console_handler);
-            orig_console_handler = null;
-        }
-
         // Restore original std, err streams
         handle_stdout = handle_stderr = ignore;
+
+        // Keep ConsoleHandler replacement
     }
 }
