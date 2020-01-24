d2f27b4da233422625d27535303b43e87c9eec61
Kay Kasemir <kay.kasemir@gmail.com>
2020-01-24 14:33:28 -0500

    Display editor: Add 'Delete' to context menu
    .. of editor pane and outline view

--- app/display/editor/src/main/java/org/csstudio/display/builder/editor/actions/ActionDescription.java
+++ app/display/editor/src/main/java/org/csstudio/display/builder/editor/actions/ActionDescription.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015-2016 Oak Ridge National Laboratory.
+ * Copyright (c) 2015-2020 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -46,6 +46,17 @@ public abstract class ActionDescription
     };
 
     /** Delete selected widgets */
+    public static final ActionDescription DELETE =
+        new ActionDescription("icons/delete.png", Messages.Delete + " [Delete]")
+    {
+        @Override
+        public void run(final DisplayEditor editor, final boolean selected)
+        {
+            editor.removeWidgets();
+        }
+    };
+
+    /** Delete selected widgets after copying to clipboard */
     public static final ActionDescription CUT =
         new ActionDescription("icons/cut_edit.png", Messages.Cut + " [" + PlatformInfo.SHORTCUT + "-X]")
     {
