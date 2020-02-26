93f498615d4b5864e7a39c0e1bda99dbf13f59b2
Ashwarya, Tanvi <ashwarya@frib.msu.edu>
2020-02-26 15:50:53 -0500

    Fix indent

--- app/display/model/src/main/java/org/csstudio/display/builder/model/widgets/TableWidget.java
+++ app/display/model/src/main/java/org/csstudio/display/builder/model/widgets/TableWidget.java
@@ -75,8 +75,8 @@ public class TableWidget extends VisibleWidget
             "Table",
             "/icons/table.png",
             "A table",
-			     Arrays.asList("org.csstudio.opibuilder.widgets.table",
-					   "org.csstudio.opibuilder.widgets.VTableDisplay"))
+	    Arrays.asList("org.csstudio.opibuilder.widgets.table",
+			  "org.csstudio.opibuilder.widgets.VTableDisplay"))
     {
         @Override
         public Widget createWidget()
