eb354dc746575d8a60a6da4f236b4f6c26cd050b
Kunal Shroff <kunalshroff9@gmail.com>
2020-03-19 13:19:58 -0400

    app-nav add unit tests for opi files

--- app/display/navigation/src/test/java/org/phoebus/applications/display/navigation/ProcessOPITest.java
+++ app/display/navigation/src/test/java/org/phoebus/applications/display/navigation/ProcessOPITest.java
@@ -27,7 +27,7 @@ public class ProcessOPITest {
     }
 
     @Test
-    public void testEmptyList()
+    public void testEmptyBOBList()
     {
         File file = new File(getClass().getClassLoader().getResource("bob/root_with_no_children.bob").getFile());
         ProcessOPI processOPI = new ProcessOPI(file);
@@ -37,7 +37,16 @@ public class ProcessOPITest {
     }
 
     @Test
-    public void testList()
+    public void testEmptyOPIList()
+    {
+        File file = new File(getClass().getClassLoader().getResource("opi/root_with_no_children.opi").getFile());
+        ProcessOPI processOPI = new ProcessOPI(file);
+        Set<File> result = processOPI.process();
+        assertTrue("Failed to parse a bob screen with no children, expected empty list but found list " + result
+                , result.isEmpty());
+    }
+    @Test
+    public void testBOBList()
     {
         File file = new File(getClass().getClassLoader().getResource("bob/root.bob").getFile());
         ProcessOPI processOPI = new ProcessOPI(file);
@@ -48,6 +57,23 @@ public class ProcessOPITest {
         expectedFiles.add(new File(getClass().getClassLoader().getResource("bob/child_1/grand_child_1_1/grand_child_1_1.bob").getFile()));
         expectedFiles.add(new File(getClass().getClassLoader().getResource("bob/child_2/child_2.bob").getFile()));
         expectedFiles.add(new File(getClass().getClassLoader().getResource("bob/child_3/child_3.bob").getFile()));
+
+        assertThat(result, is(expectedFiles));
+    }
+
+
+    @Test
+    public void testOPIList()
+    {
+        File file = new File(getClass().getClassLoader().getResource("opi/root.opi").getFile());
+        ProcessOPI processOPI = new ProcessOPI(file);
+        Set<File> result = processOPI.process();
+        // Expected results
+        Set<File> expectedFiles = new HashSet<>();
+        expectedFiles.add(new File(getClass().getClassLoader().getResource("opi/child_1/child_1.opi").getFile()));
+        expectedFiles.add(new File(getClass().getClassLoader().getResource("opi/child_1/grand_child_1_1/grand_child_1_1.opi").getFile()));
+        expectedFiles.add(new File(getClass().getClassLoader().getResource("opi/child_2/child_2.opi").getFile()));
+        expectedFiles.add(new File(getClass().getClassLoader().getResource("opi/child_3/child_3.opi").getFile()));
 
         assertThat(result, is(expectedFiles));
     }
@@ -68,4 +94,19 @@ public class ProcessOPITest {
         // assertTrue("Failed to find the linked files. expected " + " found " , result.c);
     }
 
+    @Test
+    public void testOPICyclicLinksList()
+    {
+        // The child_1 and grandchild_1_1 have a cyclic link. The test ensures that the checking for linked files
+        // does avoid entering an infinite loop
+        File file = new File(getClass().getClassLoader().getResource("opi/cyclic/cyclic_1.opi").getFile());
+        ProcessOPI processOPI = new ProcessOPI(file);
+        Set<File> result = processOPI.process();
+        // Expected results
+        Set<File> expectedFiles = new HashSet<>();
+        expectedFiles.add(new File(getClass().getClassLoader().getResource("opi/cyclic/cyclic_2.opi").getFile()));
+
+        assertThat(result, is(expectedFiles));
+        // assertTrue("Failed to find the linked files. expected " + " found " , result.c);
+    }
 }
