47301e815c41ae29992b975b2e6ebcaa8407c37b
Georg Weiss <georg.weiss@ess.eu>
2020-03-19 10:48:57 +0100

    CSSTUDIO-1120 Removed coded added by mistake

--- core/framework/src/test/java/org/phoebus/framework/macros/MacrosUnitTest.java
+++ core/framework/src/test/java/org/phoebus/framework/macros/MacrosUnitTest.java
@@ -11,7 +11,8 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
