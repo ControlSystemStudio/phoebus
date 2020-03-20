10bd5d7937ac5cc9bb53f0fdcc1b0aa71613225d
Kay Kasemir <kay.kasemir@gmail.com>
2020-03-20 12:02:35 -0400

    Display runtime start/stop debug messages

--- app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/internal/DisplayRuntime.java
+++ app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/internal/DisplayRuntime.java
@@ -1,11 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2015-2016 Oak Ridge National Laboratory.
+ * Copyright (c) 2015-2020 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 package org.csstudio.display.builder.runtime.internal;
+
+import java.util.logging.Level;
 
 import org.csstudio.display.builder.model.DisplayModel;
 import org.csstudio.display.builder.runtime.RuntimeUtil;
@@ -18,19 +20,24 @@ import org.csstudio.display.builder.runtime.WidgetRuntime;
  *
  *  @author Kay Kasemir
  */
+@SuppressWarnings("nls")
 public class DisplayRuntime extends WidgetRuntime<DisplayModel>
 {
     @Override
     public void start()
     {
+        logger.log(Level.INFO, () -> "Display Runtime startup for " + widget.getDisplayName() + " ...       ===========");
         super.start();
         RuntimeUtil.startChildRuntimes(widget.runtimeChildren());
+        logger.log(Level.INFO, () -> "Display Runtime startup for " + widget.getDisplayName() + " completed ===========");
     }
 
     @Override
     public void stop()
     {
+        logger.log(Level.INFO, () -> "Display Runtime shudown for " + widget.getDisplayName() + " ...       ===========");
         RuntimeUtil.stopChildRuntimes(widget.runtimeChildren());
         super.stop();
+        logger.log(Level.INFO, () -> "Display Runtime shudown for " + widget.getDisplayName() + " completed ===========");
     }
 }
