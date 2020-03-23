a24006f1bf5dbb11a8d4a4176583150e73863237
Kay <kay.kasemir@gmail.com>
2020-03-23 08:27:38 -0400

    Display: Add message/reason to re-parse trigger

--- app/display/model/src/main/java/org/csstudio/display/builder/model/WidgetConfigurator.java
+++ app/display/model/src/main/java/org/csstudio/display/builder/model/WidgetConfigurator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015-2016 Oak Ridge National Laboratory.
+ * Copyright (c) 2015-2020 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -55,7 +55,13 @@ public class WidgetConfigurator
     public static class ParseAgainException extends Exception
     {
         private static final long serialVersionUID = 1L;
-    };
+
+        /** @param reason Explanation for triggering a re-parse */
+        public ParseAgainException(final String reason)
+        {
+            super(reason);
+        }
+    }
 
     /** Version of the XML.
      *
