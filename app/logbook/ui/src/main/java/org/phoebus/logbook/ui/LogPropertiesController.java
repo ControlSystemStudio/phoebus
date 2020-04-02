baead0adb861ccdcb40c7eaccd423929119be8df
Kunal Shroff <kunalshroff9@gmail.com>
2020-04-02 14:31:34 -0400

    #1244 CellFactory for creating links in the property tree view

--- app/logbook/ui/src/main/java/org/phoebus/logbook/ui/LogPropertiesController.java
+++ app/logbook/ui/src/main/java/org/phoebus/logbook/ui/LogPropertiesController.java
@@ -3,14 +3,24 @@ package org.phoebus.logbook.ui;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableCell;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
 import javafx.util.Callback;
+import org.phoebus.logbook.LogEntry;
 import org.phoebus.logbook.Property;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.phoebus.util.time.TimestampFormats.SECONDS_FORMAT;
 
 public class LogPropertiesController {
 
@@ -28,7 +38,7 @@ public class LogPropertiesController {
     @FXML
     public void initialize()
     {
-        name.setMaxWidth(1f * Integer.MAX_VALUE * 50);
+        name.setMaxWidth(1f * Integer.MAX_VALUE * 40);
         name.setCellValueFactory(
                 new Callback<TreeTableColumn.CellDataFeatures<PropertyTreeNode, String>, ObservableValue<String>>() {
                     public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<PropertyTreeNode, String> p) {
@@ -36,13 +46,38 @@ public class LogPropertiesController {
                     }
                 });
 
-        value.setMaxWidth(1f * Integer.MAX_VALUE * 50);
+        value.setMaxWidth(1f * Integer.MAX_VALUE * 60);
         value.setCellValueFactory(
                 new Callback<TreeTableColumn.CellDataFeatures<PropertyTreeNode, String>, ObservableValue<String>>() {
                     public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<PropertyTreeNode, String> p) {
                         return p.getValue().getValue().valueProperty();
                     }
                 });
+        value.setCellFactory(new Callback<TreeTableColumn<PropertyTreeNode, String>,
+                                          TreeTableCell<PropertyTreeNode, String>>() {
+
+            @Override
+            public TreeTableCell<PropertyTreeNode, String> call(TreeTableColumn<PropertyTreeNode, String> param) {
+                return new TreeTableCell<PropertyTreeNode, String> () {
+                    @Override
+                    public void updateItem(String item, boolean empty){
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            try {
+                                URL url = new URL(item);
+                                final Hyperlink link = new Hyperlink(url.toString());
+                                setGraphic(link);
+                            } catch (Exception e) {
+                                setGraphic(new Label(item));
+                            }
+                        }
+                    }
+                };
+            }
+        });
+
 
         constructTree();
 
