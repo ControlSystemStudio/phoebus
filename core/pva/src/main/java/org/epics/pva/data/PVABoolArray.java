5878776b39367917fd438a979f78cce4c9a9a102
Kay <kay.kasemir@gmail.com>
2020-04-03 12:28:11 -0400

    PVA*Array types: Support setValue() from same type

--- core/pva/src/main/java/org/epics/pva/data/PVABoolArray.java
+++ core/pva/src/main/java/org/epics/pva/data/PVABoolArray.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Oak Ridge National Laboratory.
+ * Copyright (c) 2019-2020 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -48,7 +48,12 @@ public class PVABoolArray extends PVAData implements PVAArray
     @Override
     public void setValue(final Object new_value) throws Exception
     {
-        if (new_value instanceof boolean[])
+        if (new_value instanceof PVABoolArray)
+        {
+            final boolean[] other = ((PVABoolArray) new_value).value;
+            value = Arrays.copyOf(other, other.length);
+        }
+        else if (new_value instanceof boolean[])
             set(((boolean[]) new_value));
         else if (new_value instanceof List)
         {
--- core/pva/src/main/java/org/epics/pva/data/PVALongArray.java
+++ core/pva/src/main/java/org/epics/pva/data/PVALongArray.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Oak Ridge National Laboratory.
+ * Copyright (c) 2019-2020 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -56,7 +56,12 @@ public class PVALongArray extends PVAData implements PVAArray
     @Override
     public void setValue(final Object new_value) throws Exception
     {
-        if (new_value instanceof long[])
+        if (new_value instanceof PVALongArray)
+        {
+            final long[] other = ((PVALongArray) new_value).value;
+            value = Arrays.copyOf(other, other.length);
+        }
+        else if (new_value instanceof long[])
             set(((long[]) new_value));
         else if (new_value instanceof List)
         {
