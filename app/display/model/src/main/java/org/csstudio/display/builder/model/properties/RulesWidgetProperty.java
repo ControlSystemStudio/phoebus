e3ca926bf2d9771f5de0f1126b55212b48bedc66
Tanvi <ashwarya@frib.msu.edu>
2020-05-05 12:02:58 -0400

    Fix indent

--- app/display/model/src/main/java/org/csstudio/display/builder/model/properties/RulesWidgetProperty.java
+++ app/display/model/src/main/java/org/csstudio/display/builder/model/properties/RulesWidgetProperty.java
@@ -58,7 +58,7 @@ public class RulesWidgetProperty extends WidgetProperty<List<RuleInfo>>
     public static WidgetProperty<?> propIDToNewProp(final Widget widget,
             final String prop_id, final String dbg_tag)
     {
-	try
+        try
         {
 	    // Check for plain property (including re-mapped legacy properties)
             return widget.getProperty(prop_id).clone();
