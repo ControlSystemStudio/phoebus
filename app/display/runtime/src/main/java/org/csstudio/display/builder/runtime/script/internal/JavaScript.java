1ee94662f447dd1ebe2e47e0437df11f88a30e32
Kay <kay.kasemir@gmail.com>
2020-06-20 18:58:09 -0400

    Display scripts: Replace 'nashorn'

--- app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/script/internal/JavaScript.java
+++ app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/script/internal/JavaScript.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015-2016 Oak Ridge National Laboratory.
+ * Copyright (c) 2015-2020 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,8 +8,6 @@
 package org.csstudio.display.builder.runtime.script.internal;
 
 import java.util.concurrent.Future;
-
-import javax.script.CompiledScript;
 
 import org.csstudio.display.builder.model.Widget;
 import org.csstudio.display.builder.runtime.pv.RuntimePV;
@@ -22,7 +20,7 @@ class JavaScript implements Script
 {
     private final JavaScriptSupport support;
     private final String name;
-    private final CompiledScript code;
+    private final org.mozilla.javascript.Script code;
 
     /** Parse and compile script file
      *
@@ -30,7 +28,7 @@ class JavaScript implements Script
      *  @param name Name of script (file name, URL)
      *  @param code Compiled code
      */
-    public JavaScript(final JavaScriptSupport support, final String name, final CompiledScript code)
+    public JavaScript(final JavaScriptSupport support, final String name, final org.mozilla.javascript.Script code)
     {
         this.support = support;
         this.name = name;
@@ -44,7 +42,7 @@ class JavaScript implements Script
     }
 
     /** @return Compiled code */
-    public CompiledScript getCode()
+    public org.mozilla.javascript.Script getCode()
     {
         return code;
     }
