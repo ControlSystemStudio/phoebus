41c3519ed52bfaf13302e14d1c736e74cbba9953
Kay <kay.kasemir@gmail.com>
2020-08-03 16:49:47 -0400

    Move point editor demo to tests/demos

--- app/display/editor/src/test/java/org/csstudio/display/builder/editor/poly/PointsEditorDemo.java
+++ app/display/editor/src/test/java/org/csstudio/display/builder/editor/poly/PointsEditorDemo.java
@@ -1,14 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2015-2016 Oak Ridge National Laboratory.
+ * Copyright (c) 2015-2020 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.csstudio.display.builder.editor;
+package org.csstudio.display.builder.editor.poly;
 
-import org.csstudio.display.builder.editor.poly.PointsEditor;
-import org.csstudio.display.builder.editor.poly.PointsEditorListener;
+import org.csstudio.display.builder.editor.EditorUtil;
 import org.csstudio.display.builder.model.properties.Points;
 import org.phoebus.ui.javafx.ApplicationWrapper;
 
