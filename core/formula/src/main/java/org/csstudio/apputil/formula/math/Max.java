07fac9a7fc2d773ef6f5edf59848a7ca5f53bc20
Kunal Shroff <kunalshroff9@gmail.com>
2020-08-20 14:19:54 -0400

    removing redundant implementations

--- core/formula/src/main/java/org/csstudio/apputil/formula/math/Max.java
+++
@@ -1,9 +0,0 @@
-package org.csstudio.apputil.formula.math;
-
-public class Max extends TwoArgMathFunction {
-
-    protected Max(String name, String description, TwoArgFunction function)
-    {
-        super("max", "Greater of the two values", Math::max);
-    }
-}
--- core/formula/src/main/java/org/csstudio/apputil/formula/math/Min.java
+++
@@ -1,9 +0,0 @@
-package org.csstudio.apputil.formula.math;
-
-public class Min extends TwoArgMathFunction {
-
-    protected Min(String name, String description, TwoArgFunction function)
-    {
-        super("min", "Smaller of the two values", Math::min);
-    }
-}
