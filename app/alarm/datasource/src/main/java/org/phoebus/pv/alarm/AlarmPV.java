2cc392965cf8b86cdfbf9e210bc3c05c3413838a
Kunal Shroff <kunalshroff9@gmail.com>
2020-08-31 09:36:51 -0400

    #1493 A basic alarm pv where unack are mapped to equivalent epics alarms

--- app/alarm/datasource/src/main/java/org/phoebus/pv/alarm/AlarmPV.java
+++ app/alarm/datasource/src/main/java/org/phoebus/pv/alarm/AlarmPV.java
@@ -1,8 +1,12 @@
 package org.phoebus.pv.alarm;
 
 import org.epics.vtype.Alarm;
+import org.epics.vtype.AlarmSeverity;
+import org.epics.vtype.AlarmStatus;
 import org.epics.vtype.Time;
-import org.epics.vtype.VBoolean;
+import org.epics.vtype.VString;
+import org.phoebus.applications.alarm.model.AlarmTreeItem;
+import org.phoebus.applications.alarm.model.BasicState;
 import org.phoebus.pv.PV;
 
 /**
@@ -11,10 +15,47 @@ import org.phoebus.pv.PV;
  */
 public class AlarmPV extends PV
 {
+    private final AlarmPVInfo info;
+
     public AlarmPV(String name, String base_name)
     {
         super(name);
+        info = AlarmPVInfo.of(base_name);
     }
 
+    public AlarmPVInfo getInfo()
+    {
+        return info;
+    }
 
+    public void updateValue(AlarmTreeItem item)
+    {
+        // Process alarm
+        VString alarm = VString.of(item.toString(),
+                                   processState(item.getState()),
+                                   Time.now());
+        notifyListenersOfValue(alarm);
+    }
+
+    private static Alarm processState(BasicState state)
+    {
+        if (state.getSeverity().isActive())
+        {
+            switch (state.getSeverity())
+            {
+                case MINOR:
+                    return Alarm.of(AlarmSeverity.MINOR, AlarmStatus.UNDEFINED, state.toString());
+                case MAJOR:
+                    return Alarm.of(AlarmSeverity.MAJOR, AlarmStatus.UNDEFINED, state.toString());
+                case UNDEFINED:
+                    return Alarm.of(AlarmSeverity.UNDEFINED, AlarmStatus.UNDEFINED, state.toString());
+                default:
+                    return Alarm.of(AlarmSeverity.NONE, AlarmStatus.UNDEFINED, state.toString());
+            }
+        }
+        else
+        {
+            return Alarm.of(AlarmSeverity.NONE, AlarmStatus.UNDEFINED, state.toString());
+        }
+    }
 }
