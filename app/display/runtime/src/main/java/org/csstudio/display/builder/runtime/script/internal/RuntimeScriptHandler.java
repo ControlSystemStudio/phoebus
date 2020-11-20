ControlSystemStudio/phoebus

commit 634a0c7fb73cae8f3805675a9776b438c14d797d

Use PV.isDisconnected() instead of pv.read()==null
The former does more checks

app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/script/internal/RuntimeScriptHandler.java
@@ -30,6 +30,7 @@ import org.csstudio.display.builder.runtime.pv.RuntimePVListener;
 import org.epics.vtype.VType;
 import org.phoebus.framework.macros.MacroHandler;
 import org.phoebus.framework.macros.MacroValueProvider;
+import org.phoebus.pv.PV;
 
 /** Handler for one script of a widget.
  *
@@ -219,7 +220,7 @@ public class RuntimeScriptHandler implements RuntimePVListener
         // Skip script execution unless all PVs are connected?
         if (check_connections)
             for (RuntimePV p : pvs)
-                if (p.read() == null)
+                if (PV.isDisconnected(pv.read()))
                     return;
 
         // If this is a trigger PV, execute the script.

commit ff705f7f0e52e01b40438ba5e401530f24c9475d

Check all the PVs, not just the one that has changed
Fixes bug introduced in 634a0c7fb73cae8f3805675a9776b438c14d797d

app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/script/internal/RuntimeScriptHandler.java
@@ -222,7 +222,7 @@ public class RuntimeScriptHandler implements RuntimePVListener
         // Skip script execution unless all PVs are connected?
         if (check_connections)
             for (RuntimePV p : pvs)
-                if (PV.isDisconnected(pv.read()))
+                if (PV.isDisconnected(p.read()))
                     return;
 
         // If this is a trigger PV, execute the script.