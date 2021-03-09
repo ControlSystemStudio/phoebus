36fe8921ca4d7791f2482d6385ca6e6ea9453c43
Kay <ky9@ornl.gov>
2021-03-09 16:02:33 -0500

    cleanup

--- services/scan-server/src/main/java/org/csstudio/scan/server/httpd/ScanServlet.java
+++ services/scan-server/src/main/java/org/csstudio/scan/server/httpd/ScanServlet.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2020 Oak Ridge National Laboratory.
+ * Copyright (c) 2012-2021 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -105,8 +105,15 @@ public class ScanServlet extends HttpServlet
             text = request.getParameter("deadline");
             if (text != null  && !"0000-00-00 00:00:00".equals(text))
             {
-                deadline = LocalDateTime.from(TimestampFormats.SECONDS_FORMAT.parse(text));
-
+                try
+                {
+                    deadline = LocalDateTime.from(TimestampFormats.SECONDS_FORMAT.parse(text));
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Invalid deadline '" + text + "'");
+                }
+                // Allow timeout or deadline, not both
                 if (timeout_secs > 0)
                     throw new Exception("Cannot specify both timeout and deadline");
             }
