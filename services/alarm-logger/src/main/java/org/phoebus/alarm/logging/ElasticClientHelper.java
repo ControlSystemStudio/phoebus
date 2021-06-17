4f22b7d8bc762ea398671fcefad5f0e547a86151
Kunal Shroff <kunalshroff9@gmail.com>
2021-06-17 13:00:31 -0400

    #1934 using batched operations for indexing state and config messages

--- services/alarm-logger/src/main/java/org/phoebus/alarm/logging/ElasticClientHelper.java
+++ services/alarm-logger/src/main/java/org/phoebus/alarm/logging/ElasticClientHelper.java
@@ -3,17 +3,14 @@
  */
 package org.phoebus.alarm.logging;
 
-import static org.phoebus.alarm.logging.AlarmLoggingService.logger;
-
-import java.io.IOException;
-import java.util.Properties;
-import java.util.logging.Level;
-
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.DocWriteResponse.Result;
 import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -21,6 +18,20 @@ import org.elasticsearch.client.sniff.Sniffer;
 import org.phoebus.applications.alarm.messages.AlarmCommandMessage;
 import org.phoebus.applications.alarm.messages.AlarmConfigMessage;
 import org.phoebus.applications.alarm.messages.AlarmStateMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import static org.phoebus.alarm.logging.AlarmLoggingService.logger;
 
 /**
  * @author Kunal Shroff {@literal <kunalshroff9@gmail.gov>}
@@ -31,6 +42,13 @@ public class ElasticClientHelper {
     private static RestHighLevelClient client;
     private static ElasticClientHelper instance;
     private static Sniffer sniffer;
+
+    private static ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(4);
+    ScheduledFuture<?> job;
+    // State messages to be indexed
+    BlockingQueue<IndexRequest> stateMessagedQueue = new LinkedBlockingDeque<>();
+    // State messages to be indexed
+    BlockingQueue<IndexRequest> configMessagedQueue = new LinkedBlockingDeque<>();
 
     private ElasticClientHelper() {
         try {
@@ -51,8 +69,12 @@ public class ElasticClientHelper {
                 sniffer = Sniffer.builder(client.getLowLevelClient()).build();
                 logger.log(Level.INFO, "ES Sniff feature is enabled");
             }
+            // Start the executor for periodically logging into es
+            job = scheduledExecutorService.scheduleAtFixedRate(new flush2Elastic(stateMessagedQueue, configMessagedQueue),
+                    0, 250, TimeUnit.MILLISECONDS);
         } catch (Exception e) {
             try {
+                job.cancel(false);
                 sniffer.close();
                 client.close();
             } catch (IOException ex) {
@@ -91,15 +113,24 @@ public class ElasticClientHelper {
         }
     }
 
-    public boolean indexAlarmStateDocument(String indexName, AlarmStateMessage alarmStateMessage) {
+    public void indexAlarmStateDocument(String indexName, AlarmStateMessage alarmStateMessage) {
         IndexRequest indexRequest = new IndexRequest(indexName.toLowerCase(), "alarm");
         try {
             indexRequest.source(alarmStateMessage.sourceMap());
             IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-            return indexResponse.getResult().equals(Result.CREATED);
         } catch (IOException e) {
             logger.log(Level.SEVERE, "failed to log message " + alarmStateMessage + " to index " + indexName, e);
-            return false;
+        }
+    }
+
+
+    public void indexAlarmStateDocuments(String indexName, AlarmStateMessage alarmStateMessage) {
+        try {
+            IndexRequest indexRequest = new IndexRequest(indexName.toLowerCase(), "alarm");
+            indexRequest.source(alarmStateMessage.sourceMap());
+            stateMessagedQueue.put(indexRequest);
+        } catch (InterruptedException e) {
+            logger.log(Level.SEVERE, "failed to log message " + alarmStateMessage + " to index " + indexName, e);
         }
     }
 
@@ -124,6 +155,47 @@ public class ElasticClientHelper {
         } catch (IOException e) {
             logger.log(Level.SEVERE, "failed to log message " + alarmConfigMessage + " to index " + indexName, e);
             return false;
+        }
+    }
+
+    public void indexAlarmConfigDocuments(String indexName, AlarmConfigMessage alarmConfigMessage) {
+        try {
+            IndexRequest indexRequest = new IndexRequest(indexName.toLowerCase(), "alarm_config");
+            indexRequest.source(alarmConfigMessage.sourceMap());
+            configMessagedQueue.put(indexRequest);
+        } catch (InterruptedException e) {
+            logger.log(Level.SEVERE, "failed to log message " + alarmConfigMessage + " to index " + indexName, e);
+        }
+    }
+
+    private static class flush2Elastic implements Runnable {
+
+        private BlockingQueue<IndexRequest> stateMessagedQueue;
+        private BlockingQueue<IndexRequest> configMessagedQueue;
+
+        public flush2Elastic(BlockingQueue<IndexRequest> stateMessagedQueue, BlockingQueue<IndexRequest> configMessagedQueue) {
+            this.stateMessagedQueue = stateMessagedQueue;
+            this.configMessagedQueue = configMessagedQueue;
+        }
+
+        @Override
+        public void run() {
+            if(stateMessagedQueue.size() + configMessagedQueue.size() > 0){
+                System.out.println("batch execution of : " + stateMessagedQueue.size() + " state messages and " + configMessagedQueue.size() + " config messages");
+                BulkRequest bulkRequest = new BulkRequest();
+                Collection<IndexRequest> stateIndexRequests = new ArrayList<>();
+                stateMessagedQueue.drainTo(stateIndexRequests);
+                stateIndexRequests.stream().forEach(bulkRequest::add);
+                Collection<IndexRequest> configIndexRequests = new ArrayList<>();
+                configMessagedQueue.drainTo(configIndexRequests);
+                configIndexRequests.stream().forEach(bulkRequest::add);
+                bulkRequest.setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE);
+                try {
+                    BulkResponse bulkResponse = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+                } catch (IOException e) {
+                    logger.log(Level.SEVERE, "failed to log messages to index ", e);
+                }
+            }
         }
     }
 }
