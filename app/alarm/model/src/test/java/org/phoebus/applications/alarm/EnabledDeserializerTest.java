120ee77e1e4e648c5d32723761f5936604b8a0c1
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-09-22 06:50:28 -0700

    Add deserializer test

---
+++ app/alarm/model/src/test/java/org/phoebus/applications/alarm/EnabledDeserializerTest.java
@@ -0,0 +1,22 @@
+package src.test.java.org.phoebus.applications.alarm;
+
+import org.junit.Test;
+import java.io.IOException;
+import org.phoebus.applications.alarm.model.EnabledState;
+import org.phoebus.applications.alarm.messages.EnabledDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+public class EnabledDeserializerTest {
+    public static void DeserializerTest() throws IOException {
+
+        String json = "{\n" +
+                "    \"enabled\": 2021-09-22T09:30:00}\n" +
+                "}";
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(EnabledState.class, new EnabledDeserializer());
+        mapper.registerModule(module);
+        EnabledState readValue = mapper.readValue(json, EnabledState.class);
+    }
+}
