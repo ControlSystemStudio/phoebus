c1a923fd497cb3a1e8c2c58b3e2095bd312f48be
Kay <ky9@ornl.gov>
2021-10-18 16:09:57 -0400

    MCast demo cmd line args

--- core/pva/src/test/java/org/epics/pva/common/MulticastDemo.java
+++ core/pva/src/test/java/org/epics/pva/common/MulticastDemo.java
@@ -16,6 +16,7 @@ import java.net.StandardSocketOptions;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.util.Enumeration;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -29,6 +30,13 @@ import java.util.stream.Collectors;
 @SuppressWarnings("nls")
 public class MulticastDemo
 {
+    // Loopback doesn't always support MULTICAST!
+    // If there are multiple network interfaces,
+    // need to tell the program via cmd line which one to use.
+
+    // To connect as netcat 'sender', might have to add the network interface like this:
+    //   nc -6 -u 'ff02::1%eno1' 9876
+
 //    private static final String GROUP = "224.0.0.129";
     private static final String GROUP = "ff02::1";
 
@@ -52,7 +60,7 @@ public class MulticastDemo
         return null;
     }
 
-    private static void send(final String message) throws Exception
+    private static void send(final NetworkInterface iface, final String message) throws Exception
     {
         final InetSocketAddress target = new InetSocketAddress(GROUP, PORT);
         final DatagramChannel udp = target.getAddress() instanceof Inet4Address
@@ -62,7 +70,6 @@ public class MulticastDemo
         udp.socket().setReuseAddress(true);
         udp.bind(new InetSocketAddress(0));
 
-        final NetworkInterface iface = getLoopback();
         udp.setOption(StandardSocketOptions.IP_MULTICAST_IF, iface);
 
         System.out.println("Sending message to " + target.getHostString() +
@@ -74,7 +81,7 @@ public class MulticastDemo
         udp.send(buf, target);
     }
 
-    public static void receive()
+    public static void receive(final NetworkInterface iface)
     {
         try
         {
@@ -86,8 +93,6 @@ public class MulticastDemo
             udp.configureBlocking(true);
             udp.socket().setReuseAddress(true);
             udp.bind(new InetSocketAddress(PORT));
-
-            NetworkInterface iface = getLoopback();
             udp.join(group, iface);
 
             System.out.println("Listening on " + group + ", port " + PORT + " via " + iface.getDisplayName());
@@ -112,22 +117,53 @@ public class MulticastDemo
         {
             ex.printStackTrace();
         }
+        System.out.println("Exiting receiver");
     }
 
     public static void main(String[] args) throws Exception
     {
-        final ExecutorService pool = Executors.newCachedThreadPool();
-        final Future<?> receiver = pool.submit(MulticastDemo::receive);
+        NetworkInterface iface = null;
+        boolean do_send = false, do_receive = false;
+        for (String arg : args)
+            if ("-s".equals(arg))
+                do_send = true;
+            else if ("-r".equals(arg))
+                do_receive = true;
+            else if ("-h".equals(arg))
+            {
+                System.out.println("USAGE: MulticastDemo [options] [interface]");
+                System.out.println("Options:");
+                System.out.println(" -s   Run sender");
+                System.out.println(" -r   Run receiver (may run both)");
+                System.out.println("Interface will default to one marked LOOPBACK and MULTICAST");
+                return;
+            }
+            else
+                iface = NetworkInterface.getByName(arg);
 
-        while (! receiver.isDone())
+        if (iface == null)
+            iface = getLoopback();
+
+        final ExecutorService pool = Executors.newCachedThreadPool();
+        final Future<?> receiver;
+
+        if (do_receive)
         {
-            TimeUnit.SECONDS.sleep(2);
-            send("Hello");
-            TimeUnit.SECONDS.sleep(2);
-            send("bye!");
-            TimeUnit.SECONDS.sleep(2);
-            send("end");
-            TimeUnit.SECONDS.sleep(2);
+            final NetworkInterface the_iface = iface;
+            receiver = pool.submit(() -> receive(the_iface));
         }
+        else
+            receiver = new CompletableFuture<Void>();
+
+        if (do_send)
+            while (! receiver.isDone())
+                for (String text : new String[] {"Hello", "bye!", "end"})
+                {
+                    TimeUnit.SECONDS.sleep(2);
+                    send(iface, text);
+                }
+
+        pool.shutdown();
+        System.out.println("Done.");
     }
 }
