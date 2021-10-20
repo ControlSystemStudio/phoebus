f5cfdf3cdd815faacffcfe36762883d363fbdcfd
Kay <ky9@ornl.gov>
2021-10-20 12:05:01 -0400

    Mcast demo for both IPv4 and 6

--- core/pva/src/test/java/org/epics/pva/common/MulticastDemo.java
+++ core/pva/src/test/java/org/epics/pva/common/MulticastDemo.java
@@ -7,135 +7,99 @@
  ******************************************************************************/
 package org.epics.pva.common;
 
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
+import java.net.ProtocolFamily;
 import java.net.StandardProtocolFamily;
 import java.net.StandardSocketOptions;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
-import java.util.Enumeration;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
-/** Demo of basic multicasting
+/** Overall demo of using sockets for UDP with both IPv4 and IPv6
+ *
+ *  Loopback (may be called 'lo' or 'lo0') doesn't always support MULTICAST.
+ *  If there are multiple network interfaces,
+ *  need to tell the program via cmd line which one to use.
+ *
+ *  To receive, one UDP socket can join both an IPv4 and IPv6 multicast group.
+ *  It can receive from both
+ *      nc -u 224.0.0.129 9876
+ *  and nc -6 -u 'ff02::42:1%en0' 9876
+ *  (This might not work for lo/lo0, where it may only receive IPv6).
+ *  Maintaining a single receive socket for both protocol families
+ *  simplifies the receive thread.
+ *
+ *  To send, only a StandardProtocolFamily.INET port can send to the IPv4 multicast group,
+ *  and that in turn cannot send to the IPv6 multicast group,
+ *  so need one socket per protocol type for sending.
+ *
+ *  IPv6 Multicast address format: ffFS:xxxxxxxx
+ *  ff - Multicast marker
+ *  F  - Flag bits 0, Rendezvous point, Prefix, Transient
+ *       If not 'transient', address is _predefined_,
+ *       assigned/registered by IANA
+ *  S  - Scope
+ *       1 Interface-local
+ *       2 Link local
+ *       4 Admin local
+ *       5 Site local
+ *       8 Org local
+ *       E Global
+ *
+ *  Well known addresses:
+ *  ff02::1    All IPv6 devices on link, "broadcast"
  *
  *  @author Kay Kasemir
  */
 @SuppressWarnings("nls")
 public class MulticastDemo
 {
-    // Loopback doesn't always support MULTICAST!
-    // If there are multiple network interfaces,
-    // need to tell the program via cmd line which one to use.
-    //
-    // When receiving a message, that might indicate
-    // "Recevied: ... from /[fe80:....%5]..."
-    // with an interface index "%5" instead of a name,
-    // and the relationship between interface name and index
-    // isn't very obvious.
-    //
-    // To connect as netcat 'sender', might have to add the network interface like this:
-    //   nc -6 -u 'ff02::1%eno1' 9876
-    //
-    // Multicast address format: ffFS:xxxxxxxx
-    // ff - Multicast marker
-    // F  - Flag bits 0, Rendezvous point, Prefix, Transient
-    //      If not 'transient', address is _predefined_,
-    //      assigned/registered by IANA
-    // S  - Scope
-    //      1 Interface-local
-    //      2 Link local
-    //      4 Admin local
-    //      5 Site local
-    //      8 Org local
-    //      E Global
-    //
-    // Well known addresses:
-    // ff02::1    All nodes on interface
-    // ff02::1    All IPv6 devices on link, "broadcast"
-    private static final String GROUP = "ff02::42:1"; // "ff02::1";
-
-    // For IPv4 test:
-//    private static final String GROUP = "224.0.0.129";
-
+    private static final String MCAST_GROUP4 = "224.0.0.129";
+    private static final String MCAST_GROUP6 = "ff02::42:1";
     private static final int PORT = 9876;
 
-    private static NetworkInterface getLoopback() throws Exception
+    private static DatagramChannel createUDPChannel(final ProtocolFamily family, final int port) throws Exception
     {
-        final Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-        while (interfaces.hasMoreElements())
-        {
-            final NetworkInterface iface = interfaces.nextElement();
-            if (iface.isUp()  &&
-                iface.supportsMulticast()  &&
-                iface.isLoopback())
-                return iface;
-        }
-        System.err.println("Cannot determine loopback interface within " +
-                           NetworkInterface.networkInterfaces()
-                                           .map(NetworkInterface::getDisplayName)
-                                           .collect(Collectors.joining(", ")));
-        return null;
-    }
-
-    private static void send(final NetworkInterface iface, final String message) throws Exception
-    {
-        final InetSocketAddress target = new InetSocketAddress(GROUP, PORT);
-        final DatagramChannel udp = target.getAddress() instanceof Inet4Address
-                                  ? DatagramChannel.open(StandardProtocolFamily.INET)
-                                  : DatagramChannel.open(StandardProtocolFamily.INET6);
+        final DatagramChannel udp = DatagramChannel.open(family);
         udp.configureBlocking(true);
         udp.socket().setReuseAddress(true);
-        Integer ttl = 1;
-        udp.socket().setOption(StandardSocketOptions.IP_MULTICAST_TTL, ttl);
-        udp.bind(new InetSocketAddress(0));
-
-        udp.setOption(StandardSocketOptions.IP_MULTICAST_IF, iface);
-
-        System.out.println("Sending message to " + target.getHostString() +
-                           " " + target.getPort());
-
-        final ByteBuffer buf = ByteBuffer.allocate(500);
-        buf.put(message.getBytes());
-        buf.flip();
-        udp.send(buf, target);
+        udp.bind(new InetSocketAddress(port));
+        return udp;
     }
 
-    public static void receive(final NetworkInterface iface)
+    private static void receive(final NetworkInterface iface)
     {
         try
         {
-            final InetAddress group = InetAddress.getByName(GROUP);
+            final InetAddress group4 = InetAddress.getByName(MCAST_GROUP4);
+            final InetAddress group6 = InetAddress.getByName(MCAST_GROUP6);
 
-            final DatagramChannel udp = group instanceof Inet4Address
-                                      ? DatagramChannel.open(StandardProtocolFamily.INET)
-                                      : DatagramChannel.open(StandardProtocolFamily.INET6);
-            udp.configureBlocking(true);
-            udp.socket().setReuseAddress(true);
-            udp.bind(new InetSocketAddress(PORT));
-            udp.join(group, iface);
+            final DatagramChannel udp = createUDPChannel(StandardProtocolFamily.INET6, PORT);
+            udp.join(group4, iface);
+            udp.join(group6, iface);
 
-            System.out.println("Listening on " + group + ", port " + PORT + " via " + iface.getDisplayName());
+            System.out.println("Listening on " + group4 + ", port " + PORT + " via " + iface.getDisplayName());
+            System.out.println("Listening on " + group6 + ", port " + PORT + " via " + iface.getDisplayName());
+
             final ByteBuffer buf = ByteBuffer.allocate(500);
-            while (true)
+            int awaiting = 2;
+            while (awaiting > 0)
             {
                 buf.clear();
                 final InetSocketAddress client = (InetSocketAddress) udp.receive(buf);
-                System.out.println("Got UDP message from " + client.getAddress() +
-                                   ", port " + client.getPort());
                 buf.flip();
                 final byte[] data = new byte[buf.limit()];
                 buf.get(data);
                 final String received = new String(data);
                 System.out.println("Received: " + received + " from " + client);
                 if ("end".equals(received))
-                    break;
+                    --awaiting;
             }
             udp.close();
         }
@@ -146,29 +110,39 @@ public class MulticastDemo
         System.out.println("Exiting receiver");
     }
 
+    private static void send(final DatagramChannel udp, final InetAddress target, final String text) throws Exception
+    {
+        final InetSocketAddress addr = new InetSocketAddress(target, PORT);
+        System.out.println("Sending to " + addr);
+        final ByteBuffer buf = ByteBuffer.allocate(500);
+        buf.put(text.getBytes());
+        buf.flip();
+        udp.send(buf, addr);
+    }
+
     public static void main(String[] args) throws Exception
     {
         NetworkInterface iface = null;
         boolean do_send = false, do_receive = false;
+        boolean help = false;
         for (String arg : args)
             if ("-s".equals(arg))
                 do_send = true;
             else if ("-r".equals(arg))
                 do_receive = true;
             else if ("-h".equals(arg))
-            {
-                System.out.println("USAGE: MulticastDemo [options] [interface]");
-                System.out.println("Options:");
-                System.out.println(" -s   Run sender");
-                System.out.println(" -r   Run receiver (may run both)");
-                System.out.println("Interface will default to one marked LOOPBACK and MULTICAST");
-                return;
-            }
+                help = true;
             else
                 iface = NetworkInterface.getByName(arg);
 
-        if (iface == null)
-            iface = getLoopback();
+        if (help  ||  iface == null)
+        {
+            System.out.println("USAGE: MulticastDemo [options] interface");
+            System.out.println("Options:");
+            System.out.println(" -s   Run sender");
+            System.out.println(" -r   Run receiver (may run both)");
+            return;
+        }
 
         final ExecutorService pool = Executors.newCachedThreadPool();
         final Future<?> receiver;
@@ -182,12 +156,31 @@ public class MulticastDemo
             receiver = new CompletableFuture<Void>();
 
         if (do_send)
+        {
+            final DatagramChannel udp4 = createUDPChannel(StandardProtocolFamily.INET, 0);
+            udp4.setOption(StandardSocketOptions.IP_MULTICAST_IF, iface);
+            udp4.socket().setOption(StandardSocketOptions.IP_MULTICAST_TTL, 1);
+
+            final DatagramChannel udp6 = createUDPChannel(StandardProtocolFamily.INET6, 0);
+            udp6.setOption(StandardSocketOptions.IP_MULTICAST_IF, iface);
+            udp6.socket().setOption(StandardSocketOptions.IP_MULTICAST_TTL, 1);
+
+            final InetAddress group4 = InetAddress.getByName(MCAST_GROUP4);
+            final InetAddress group6 = InetAddress.getByName(MCAST_GROUP6);
+
             while (! receiver.isDone())
                 for (String text : new String[] {"Hello", "bye!", "end"})
                 {
                     TimeUnit.SECONDS.sleep(2);
-                    send(iface, text);
+                    if (receiver.isDone())
+                        break;
+                    send(udp4, group4, text);
+                    send(udp6, group6, text);
                 }
+
+            udp6.close();
+            udp4.close();
+        }
 
         pool.shutdown();
         System.out.println("Done.");
