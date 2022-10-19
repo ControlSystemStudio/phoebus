48563364204c10e277d046545a6d7ae6cc6104fe
georgweiss <georg.weiss@ess.eu>
2022-10-19 12:46:57 +0200

    Unit test fix

--- app/save-and-restore/model/src/test/java/org/phoebus/applications/saveandrestore/model/NodeTest.java
+++ app/save-and-restore/model/src/test/java/org/phoebus/applications/saveandrestore/model/NodeTest.java
@@ -1,102 +1,105 @@
 package org.phoebus.applications.saveandrestore.model;
 
-import static org.junit.Assert.*;
-import static org.junit.Assert.assertNotEquals;
+import org.junit.Test;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class NodeTest {
 
-	@Test
-	public void test1() {
+    @Test
+    public void test1() {
 
-		Node node = Node.builder().uniqueId("id1").build();
-		Node node2 = Node.builder().uniqueId("id2").build();
+        Node node = Node.builder().uniqueId("id1").build();
+        Node node2 = Node.builder().uniqueId("id2").build();
 
-		assertNotEquals(node, node2);
-		assertEquals(node, node);
+        assertNotEquals(node, node2);
+        assertEquals(node, node);
 
-		assertEquals(NodeType.FOLDER, node.getNodeType());
-		assertEquals(NodeType.CONFIGURATION, Node.builder().nodeType(NodeType.CONFIGURATION).build().getNodeType());
-		assertEquals(NodeType.SNAPSHOT, Node.builder().nodeType(NodeType.SNAPSHOT).build().getNodeType());
-	}
-	
-	
-	@Test
-	public void genericTest() {
-		
-		Date now = new Date();
-			
-		Node node = new Node();
-			
-		node.setNodeType(NodeType.CONFIGURATION);
-		node.setCreated(now);
-		node.setLastModified(now);
-		node.setUniqueId("id1");
-		node.setName("name");
-		node.setUserName("userName");
-		
-		assertEquals(now, node.getCreated());
-		assertEquals(now, node.getLastModified());
-		assertEquals("id1", node.getUniqueId());
-		assertEquals("name", node.getName());
-		assertEquals(NodeType.CONFIGURATION, node.getNodeType());
-		assertEquals("userName", node.getUserName());
-		
-	}
-	
-	
-	@Test
-	public void testCompareTo() {
-		
-		Node folder1 = Node.builder().name("a").build();
-		Node folder2 = Node.builder().name("b").build();
-		Node folder3 = Node.builder().name("a").build();
-		
-		Node config = Node.builder().nodeType(NodeType.CONFIGURATION).name("c1").build();
-		Node config2 = Node.builder().nodeType(NodeType.CONFIGURATION).name("c2").build();
-		
-		assertTrue(folder3.compareTo(folder1) == 0);
-		assertTrue(folder2.compareTo(folder1) > 0);
-		assertTrue(folder1.compareTo(folder2) < 0);
-		
-		assertTrue(folder1.compareTo(config) < 0);
-		assertTrue(config.compareTo(folder1) > 0);
-		assertTrue(config.compareTo(config2) < 0);
-	}
-	
-	@Test
-	public void testBuilder() {
-		Date now = new Date();
-		Node node = Node.builder()
-				.created(now)
-				.uniqueId("id1")
-				.lastModified(now)
-				.name("name")
-				.nodeType(NodeType.FOLDER)
-				.build();
-		
-		assertEquals("id1", node.getUniqueId());
-		assertEquals(now, node.getCreated());
-		assertEquals(now, node.getLastModified());
-		assertEquals("name", node.getName());
-		assertEquals(NodeType.FOLDER, node.getNodeType());
-	}
-	
-	
-	@Test
-	public void testToString() {
-		assertNotNull(Node.builder().build().toString());
-	}
-	
-	@Test
-	public void testHashCode() {
-		Node node1 = Node.builder().uniqueId("unique").nodeType(NodeType.FOLDER).build();
-		Node node2 = Node.builder().uniqueId("unique").nodeType(NodeType.CONFIGURATION).build();
-		assertNotEquals(node1.hashCode(), node2.hashCode());
-	}
+        assertEquals(NodeType.FOLDER, node.getNodeType());
+        assertEquals(NodeType.CONFIGURATION, Node.builder().nodeType(NodeType.CONFIGURATION).build().getNodeType());
+        assertEquals(NodeType.SNAPSHOT, Node.builder().nodeType(NodeType.SNAPSHOT).build().getNodeType());
+    }
+
+
+    @Test
+    public void genericTest() {
+
+        Date now = new Date();
+
+        Node node = new Node();
+
+        node.setNodeType(NodeType.CONFIGURATION);
+        node.setCreated(now);
+        node.setLastModified(now);
+        node.setUniqueId("id1");
+        node.setName("name");
+        node.setUserName("userName");
+
+        assertEquals(now, node.getCreated());
+        assertEquals(now, node.getLastModified());
+        assertEquals("id1", node.getUniqueId());
+        assertEquals("name", node.getName());
+        assertEquals(NodeType.CONFIGURATION, node.getNodeType());
+        assertEquals("userName", node.getUserName());
+
+    }
+
+
+    @Test
+    public void testCompareTo() {
+
+        Node folder1 = Node.builder().name("a").build();
+        Node folder2 = Node.builder().name("b").build();
+        Node folder3 = Node.builder().name("a").build();
+
+        Node config = Node.builder().nodeType(NodeType.CONFIGURATION).name("c1").build();
+        Node config2 = Node.builder().nodeType(NodeType.CONFIGURATION).name("c2").build();
+
+        assertTrue(folder3.compareTo(folder1) == 0);
+        assertTrue(folder2.compareTo(folder1) > 0);
+        assertTrue(folder1.compareTo(folder2) < 0);
+
+        assertTrue(folder1.compareTo(config) < 0);
+        assertTrue(config.compareTo(folder1) > 0);
+        assertTrue(config.compareTo(config2) < 0);
+    }
+
+    @Test
+    public void testBuilder() {
+        Date now = new Date();
+        Node node = Node.builder()
+                .created(now)
+                .uniqueId("id1")
+                .lastModified(now)
+                .name("name")
+                .nodeType(NodeType.FOLDER)
+                .build();
+
+        assertEquals("id1", node.getUniqueId());
+        assertEquals(now, node.getCreated());
+        assertEquals(now, node.getLastModified());
+        assertEquals("name", node.getName());
+        assertEquals(NodeType.FOLDER, node.getNodeType());
+    }
+
+
+    @Test
+    public void testToString() {
+        assertNotNull(Node.builder().build().toString());
+    }
+
+    @Test
+    public void testHashCode() {
+        Node node1 = Node.builder().uniqueId("unique").nodeType(NodeType.FOLDER).build();
+        Node node2 = Node.builder().uniqueId("unique").nodeType(NodeType.CONFIGURATION).build();
+        assertEquals(node1.hashCode(), node2.hashCode());
+
+        node2 = Node.builder().uniqueId("unique2").nodeType(NodeType.FOLDER).build();
+        assertNotEquals(node1.hashCode(), node2.hashCode());
+    }
 }
