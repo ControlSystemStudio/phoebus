18fbd65c7965f5de8fdda69d1a84947cadc9ce27
kasemir <kay.kasemir@gmail.com>
2022-10-27 15:15:01 -0400

    PVA write vs. asyncWrite comments

--- core/pv/src/main/java/org/phoebus/pv/pva/PVA_PV.java
+++ core/pv/src/main/java/org/phoebus/pv/pva/PVA_PV.java
@@ -164,6 +164,17 @@ public class PVA_PV extends PV
     @Override
     public void write(final Object new_value) throws Exception
     {
+        // With Channel Access, there are different protocol options
+        // for "put" vs. "put-callback".
+        // With PVA, it is currently unclear how to distinguish between
+        // these two. The PVA server might honor certain values in the
+        // "request", but that is not documented as part of the protocol.
+        // On one hand, we should 'get()' the result of the write to
+        // receive exceptions for read-only PVs.
+        // On the other hand, such a 'get()' could last a long time
+        // in case some detail in the 'request' caused the PVA server
+        // to perform a put-callback type of operation,
+        // and a GUI calling write() expect an immediate return.
         channel.write(name_helper.getWriteRequest(), new_value);
     }
 
