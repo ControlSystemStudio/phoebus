b24b37dc5c4dbd393206ee7218a3c97bc1b5e764
kasemir <ky9@ornl.gov>
2023-06-12 12:11:00 -0400

    Editor: Expand macros

--- app/display/editor/src/main/java/org/csstudio/display/builder/editor/EditorGUI.java
+++ app/display/editor/src/main/java/org/csstudio/display/builder/editor/EditorGUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015-2020 Oak Ridge National Laboratory.
+ * Copyright (c) 2015-2023 Oak Ridge National Laboratory.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -461,6 +461,7 @@ public class EditorGUI
             {
                 canon_path = file.getCanonicalPath();
                 model = ModelLoader.loadModel(new FileInputStream(file), canon_path);
+                model.expandMacros(org.csstudio.display.builder.model.Preferences.getMacros());
                 this.file = file;
             }
             catch (final Exception ex)
