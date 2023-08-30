a82a1ced30b5367f95646f5fb5888f526519de66
Abraham Wolk <abraham.wolk@ess.eu>
2023-08-30 14:10:32 +0200

    Revert "fix converters compile issues, they still need to be fixed for"
    This reverts commit 092b7a3e0d583480ddc62fd220a986528a42b0b3.

--- app/display/convert-edm/src/main/java/org/csstudio/display/converter/edm/widgets/ConverterBase.java
+++ app/display/convert-edm/src/main/java/org/csstudio/display/converter/edm/widgets/ConverterBase.java
@@ -83,14 +83,14 @@ public abstract class ConverterBase<W extends Widget>
             final String expression = "pv0>=" + t.getVisMin() + " && pv0<" + t.getVisMax();
             WidgetProperty<Boolean> val = vis_prop.clone();
             val.setValue(!t.isVisInvert());
-            exprs.add(new ExpressionInfo<>(expression, true, val));
+            exprs.add(new RuleInfo.ExprInfoValue<>(expression, val));
 
             // Else: Hide (unless inverted)
             val = vis_prop.clone();
             val.setValue(t.isVisInvert());
-            exprs.add(new ExpressionInfo<>("true", true, val));
+            exprs.add(new RuleInfo.ExprInfoValue<>("true", val));
 
-//            rules.add(new RuleInfo("EDM visibility", vis_prop.getName(), false, exprs, pvs));
+            rules.add(new RuleInfo("EDM visibility", vis_prop.getName(), false, exprs, pvs));
             widget.propRules().setValue(rules);
         }
 
@@ -147,11 +147,11 @@ public abstract class ConverterBase<W extends Widget>
                 final WidgetColor color = convertStaticColor(edm_color);
                 final WidgetProperty<WidgetColor> prop_col = prop.clone();
                 prop_col.setValue(color);
-                exprs.add(new ExpressionInfo<>(expression, true, prop_col));
+                exprs.add(new RuleInfo.ExprInfoValue<>(expression, prop_col));
             }
 
             final String name = "EDM " + (edm.getName() == null ? "color" : edm.getName());
-//            rules.add(new RuleInfo(name, prop.getName(), false, exprs, pvs));
+            rules.add(new RuleInfo(name, prop.getName(), false, exprs, pvs));
             widget.propRules().setValue(rules);
 
             return;
