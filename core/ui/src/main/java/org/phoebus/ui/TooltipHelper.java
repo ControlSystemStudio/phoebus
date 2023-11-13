5aaeab6cc26e854f01d30e76c4bde5b4273e8a2d
Abraham Wolk <abraham.wolk@ess.eu>
2023-11-13 10:26:23 +0100

    CSSTUDIO-2070 Remove reliance of JavaFX-implementation detail 'TOOLTIP_PROP_KEY = "javafx.scene.control.Tooltip"' in the class TooltipSupport.

--- core/ui/src/main/java/org/phoebus/ui/TooltipHelper.java
+++ core/ui/src/main/java/org/phoebus/ui/TooltipHelper.java
@@ -1,6 +1,7 @@
 package org.phoebus.ui;
 
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
 import javafx.scene.Node;
 import javafx.scene.control.Control;
 import javafx.scene.control.Tab;
@@ -9,25 +10,49 @@ import javafx.scene.control.Tooltip;
 import static org.phoebus.ui.JavaFXHelper.deferUntilJavaFXPropertyHasValue;
 
 public class TooltipHelper {
-    public static void installTooltip(Node node, Tooltip tooltip) {
+    public static Runnable installTooltip(Node node, Tooltip tooltip) {
+
+        /* installTooltip() adds a change-listener on the "focused" property of the window
+         * containing the Node "node" that installs and uninstalls the tooltip for the
+         * node when the window receives respectively loses focus. If the window is focused,
+         * the tooltip is installed.
+         *
+         * The return value of installTooltip() is a Runnable that uninstalls the tooltip
+         * installed as described in the previous paragraph.
+         */
+
+        ChangeListener<Boolean> windowFocusChangeListener = (property, oldValue, newValue) -> {
+            Platform.runLater(() -> {
+                if (newValue) {
+                    Tooltip.install(node, tooltip);
+                }
+                else {
+                    Tooltip.uninstall(node, tooltip);
+                }
+            });
+        };
+
         deferUntilJavaFXPropertyHasValue(node.sceneProperty(), scene -> {
             deferUntilJavaFXPropertyHasValue(scene.windowProperty(), window -> {
-                window.focusedProperty().addListener((property, oldValue, newValue) -> {
-                    Platform.runLater(() -> {
-                        if (newValue) {
-                            Tooltip.install(node, tooltip);
-                        }
-                        else {
-                            Tooltip.uninstall(node, tooltip);
-                        }
-                    });
-                });
-
+                window.focusedProperty().addListener(windowFocusChangeListener);
                 if (window.focusedProperty().get()) {
                     Tooltip.install(node, tooltip);
                 }
             });
         });
+
+        Runnable tooltipUninstaller = () -> {
+            deferUntilJavaFXPropertyHasValue(node.sceneProperty(), scene -> {
+                deferUntilJavaFXPropertyHasValue(scene.windowProperty(), window -> {
+                    window.focusedProperty().removeListener(windowFocusChangeListener);
+                    if (window.focusedProperty().get()) {
+                        Tooltip.uninstall(node, tooltip);
+                    }
+                });
+            });
+        };
+
+        return tooltipUninstaller;
     }
 
     public static void setTooltip(Control control, Tooltip tooltip) {
