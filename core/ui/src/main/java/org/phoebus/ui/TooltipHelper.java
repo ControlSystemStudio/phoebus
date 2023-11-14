cefd0591524e2582155c6c64a85a60f4ad07a567
Abraham Wolk <abraham.wolk@ess.eu>
2023-11-14 14:33:11 +0100

    Revert "CSSTUDIO-2070 Improve the class "TooltipHelper" and add the class "JavaFXHelper" with the function deferUntilJavaFXPropertyHasValue()."
    This reverts commit 66ce528f174d463279bea317723ee4738a0f7baa.

--- core/ui/src/main/java/org/phoebus/ui/TooltipHelper.java
+++ core/ui/src/main/java/org/phoebus/ui/TooltipHelper.java
@@ -1,76 +1,41 @@
 package org.phoebus.ui;
 
-import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.control.Control;
-import javafx.scene.control.Tab;
 import javafx.scene.control.Tooltip;
-
-import static org.phoebus.ui.JavaFXHelper.deferUntilJavaFXPropertyHasValue;
+import javafx.stage.Window;
+import org.phoebus.ui.docking.DockPane;
 
 public class TooltipHelper {
     public static void installTooltip(Node node, Tooltip tooltip) {
-        deferUntilJavaFXPropertyHasValue(node.sceneProperty(), scene -> {
-            deferUntilJavaFXPropertyHasValue(scene.windowProperty(), window -> {
-                window.focusedProperty().addListener((property, oldValue, newValue) -> {
-                    Platform.runLater(() -> {
-                        if (newValue) {
-                            Tooltip.install(node, tooltip);
-                        }
-                        else {
-                            Tooltip.uninstall(node, tooltip);
-                        }
-                    });
-                });
-
-                if (window.focusedProperty().get()) {
-                    Tooltip.install(node, tooltip);
-                }
-            });
+        Window window = DockPane.getActiveDockPane().getScene().getWindow();
+        window.focusedProperty().addListener((property, oldValue, newValue) -> {
+            if (newValue) {
+                Tooltip.install(node, tooltip);
+            }
+            else {
+                Tooltip.uninstall(node, tooltip);
+            }
         });
+
+        if (window.focusedProperty().get()) {
+            Tooltip.install(node, tooltip);
+        }
     }
 
-    public static void setTooltip(Control control, Tooltip tooltip) {
-        deferUntilJavaFXPropertyHasValue(control.sceneProperty(), scene -> {
-            deferUntilJavaFXPropertyHasValue(scene.windowProperty(), window -> {
-                window.focusedProperty().addListener((property, oldValue, newValue) -> {
-                    Platform.runLater(() -> {
-                        if (newValue) {
-                            control.setTooltip(tooltip);
-                        }
-                        else {
-                            control.setTooltip(null);
-                        }
-                    });
-                });
-
-                if (window.focusedProperty().get()) {
-                    control.setTooltip(tooltip);
-                }
-            });
+    public static void setTooltipOnControl(Control control, Tooltip tooltip) {
+        Window window = DockPane.getActiveDockPane().getScene().getWindow();
+        window.focusedProperty().addListener((property, oldValue, newValue) -> {
+            if (newValue) {
+                control.setTooltip(tooltip);
+            }
+            else {
+                control.setTooltip(null);
+            }
         });
-    }
 
-    public static void setTooltip(Tab tab, Tooltip tooltip) {
-        deferUntilJavaFXPropertyHasValue(tab.tabPaneProperty(), tabPane -> {
-            deferUntilJavaFXPropertyHasValue(tabPane.sceneProperty(), scene -> {
-                deferUntilJavaFXPropertyHasValue(scene.windowProperty(), window -> {
-                    window.focusedProperty().addListener((property, oldValue, newValue) -> {
-                        Platform.runLater(() -> {
-                            if (newValue) {
-                                tab.setTooltip(tooltip);
-                            }
-                            else {
-                                tab.setTooltip( null);
-                            }
-                        });
-                    });
-
-                    if (window.focusedProperty().get()) {
-                        tab.setTooltip(tooltip);
-                    }
-                });
-            });
-        });
+        if (window.focusedProperty().get()) {
+            control.setTooltip(tooltip);
+        }
     }
 }
