0e9cbd1efc2f69362fdf459b12bcbc899aaa0ca0
Patrick Hilhorst <philhorst@highvolteng.com>
2024-01-22 11:38:58 +0100

    Save parameters in ActionInfo

--- app/display/representation-javafx/src/main/java/org/csstudio/display/builder/representation/javafx/actionsdialog/CallPVActionDetailsController.java
+++ app/display/representation-javafx/src/main/java/org/csstudio/display/builder/representation/javafx/actionsdialog/CallPVActionDetailsController.java
@@ -2,11 +2,18 @@ package org.csstudio.display.builder.representation.javafx.actionsdialog;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.Pair;
 import org.csstudio.display.builder.model.properties.ActionInfo;
 import org.csstudio.display.builder.model.properties.CallPVActionInfo;
-import org.csstudio.display.builder.model.properties.WritePVActionInfo;
 import org.phoebus.ui.autocomplete.PVAutocompleteMenu;
 
 import java.util.HashMap;
@@ -21,9 +28,20 @@ public class CallPVActionDetailsController implements ActionDetailsController {
     @FXML
     private TextField pvName;
 
+    @FXML
+    private Button addParameter;
+
+    @FXML
+    private Button removeParameter;
+
+    @FXML
+    private TableView<Pair<String, String>> parameterTable;
+
     private final StringProperty descriptionProperty = new SimpleStringProperty();
     private final StringProperty pvNameProperty = new SimpleStringProperty();
     private final StringProperty returnPVProperty = new SimpleStringProperty();
+
+    private final ObservableList<Pair<String, String>> parameterList = FXCollections.observableArrayList();
 
     public CallPVActionDetailsController(ActionInfo actionInfo) {
         this.actionInfo = (CallPVActionInfo) actionInfo;
@@ -39,16 +57,48 @@ public class CallPVActionDetailsController implements ActionDetailsController {
         pvName.textProperty().bindBidirectional(pvNameProperty);
         returnPV.textProperty().bindBidirectional(returnPVProperty);
 
+        parameterTable.setItems(parameterList);
+        parameterTable.setEditable(true);
+
+        TableColumn<Pair<String, String>, String> nameColumn = new TableColumn<>("Parameter");
+        nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("key"));
+        nameColumn.setEditable(true);
+
+        TableColumn<Pair<String, String>, String> parameterColumn = new TableColumn<>("Value");
+        parameterColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        parameterColumn.setCellValueFactory(new PropertyValueFactory<>("value"));
+        parameterColumn.setEditable(true);
+
+        parameterTable.getColumns().add(nameColumn);
+        parameterTable.getColumns().add(parameterColumn);
+
+        addParameter.setOnAction(e -> {
+            parameterList.add(new Pair<>("A", "B"));
+            System.out.println(parameterList.toString());
+        });
+
+        removeParameter.setOnAction(e -> {
+            Pair<String, String> selectedItem = parameterTable.getSelectionModel().getSelectedItem();
+            parameterTable.getItems().remove(selectedItem);
+        });
+
         PVAutocompleteMenu.INSTANCE.attachField(pvName);
     }
 
 
     @Override
     public ActionInfo getActionInfo() {
+        HashMap<String, String> parameters = new HashMap<>();
+
+        for (Pair<String, String> pair: parameterList) {
+            parameters.put(pair.getKey(), pair.getValue());
+        }
+
         return new CallPVActionInfo(
                 this.descriptionProperty.get(),
                 this.pvNameProperty.get(),
-                new HashMap<>(),
+                parameters,
                 this.returnPVProperty.get()
         );
     }
