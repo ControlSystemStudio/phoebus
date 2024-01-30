e7927a966b8a48acb877a8dae5917bd791a8ae75
Abraham Wolk <abraham.wolk@ess.eu>
2024-01-30 13:57:23 +0100

    CSSTUDIO-2072 Implement ContextMenuCopySubMenu.getIcon().

--- app/probe/src/main/java/org/phoebus/applications/probe/ContextMenuCopySubMenu.java
+++ app/probe/src/main/java/org/phoebus/applications/probe/ContextMenuCopySubMenu.java
@@ -1,6 +1,8 @@
 package org.phoebus.applications.probe;
 
+import javafx.scene.image.Image;
 import org.phoebus.core.types.ProcessVariable;
+import org.phoebus.ui.javafx.ImageCache;
 import org.phoebus.ui.spi.ContextMenuEntry;
 
 public class ContextMenuCopySubMenu implements ContextMenuEntry {
@@ -9,6 +11,11 @@ public class ContextMenuCopySubMenu implements ContextMenuEntry {
         return Messages.CopySubMenu;
     }
 
+    private Image icon = ImageCache.getImage(ImageCache.class, "/icons/copy.png");
+    @Override
+    public Image getIcon() {
+        return icon;
+    }
     @Override
     public Class<?> getSupportedType() {
         return ProcessVariable.class;
