c6055afc1a9869be93ecc6314231790609c8da16
Abraham Wolk <abraham.wolk@ess.eu>
2024-01-30 13:21:18 +0100

    CSSTUDIO-2072 Add the class "ContextMenuCopySubMenu".

---
+++ app/probe/src/main/java/org/phoebus/applications/probe/ContextMenuCopySubMenu.java
@@ -0,0 +1,15 @@
+package org.phoebus.applications.probe;
+
+import org.phoebus.ui.spi.ContextMenuEntry;
+
+public class ContextMenuCopySubMenu implements ContextMenuEntry {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Class<?> getSupportedType() {
+        return null;
+    }
+}
