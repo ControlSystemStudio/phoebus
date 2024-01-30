423e11e875f2e558229335e936ffcffd178e2397
Abraham Wolk <abraham.wolk@ess.eu>
2024-01-30 13:58:19 +0100

    CSSTUDIO-2072 Implement ContextMenuCopySubMenu.getChildren().

--- app/probe/src/main/java/org/phoebus/applications/probe/ContextMenuCopySubMenu.java
+++ app/probe/src/main/java/org/phoebus/applications/probe/ContextMenuCopySubMenu.java
@@ -5,6 +5,9 @@ import org.phoebus.core.types.ProcessVariable;
 import org.phoebus.ui.javafx.ImageCache;
 import org.phoebus.ui.spi.ContextMenuEntry;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ContextMenuCopySubMenu implements ContextMenuEntry {
     @Override
     public String getName() {
@@ -16,8 +19,17 @@ public class ContextMenuCopySubMenu implements ContextMenuEntry {
     public Image getIcon() {
         return icon;
     }
+
     @Override
     public Class<?> getSupportedType() {
         return ProcessVariable.class;
+    }
+
+    @Override
+    public List<ContextMenuEntry> getChildren() {
+        ContextMenuPvToClipboard contextMenuPvToClipboard = new ContextMenuPvToClipboard();
+        ContextMenuPvAndValueToClipboard contextMenuPvAndValueToClipboard = new ContextMenuPvAndValueToClipboard();
+        return Arrays.asList(contextMenuPvToClipboard,
+                             contextMenuPvAndValueToClipboard);
     }
 }
