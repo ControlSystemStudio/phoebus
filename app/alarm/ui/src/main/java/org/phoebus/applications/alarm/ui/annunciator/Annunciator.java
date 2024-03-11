8cf2c5ec49931c2eeba9b8b9512525b376f7df9e
Kunal Shroff <kunalshroff9@gmail.com>
2024-03-11 15:55:21 -0400

    Prepare for a Annuncitor based on SPI

--- app/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/annunciator/Annunciator.java
+++ app/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/annunciator/Annunciator.java
@@ -1,51 +1,20 @@
-/*******************************************************************************
- * Copyright (c) 2018 Oak Ridge National Laboratory.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
 package org.phoebus.applications.alarm.ui.annunciator;
 
-import com.sun.speech.freetts.Voice;
-import com.sun.speech.freetts.VoiceManager;
-
-/**
- * Annunciator class. Uses freeTTS to annunciate passed messages.
- * @author Evan Smith
- */
-@SuppressWarnings("nls")
-public class Annunciator
+public interface Annunciator
 {
-    private final VoiceManager voiceManager;
-    private final Voice        voice;
-    private static final String voice_name = "kevin16";
-
-    /** Constructor */
-    public Annunciator()
-    {
-        // Define the voices directory.
-        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
-        voiceManager = VoiceManager.getInstance();
-        voice = voiceManager.getVoice(voice_name);
-        voice.allocate();
-    }
+    /**
+     * Initialize the Annunciator resources.
+     */
+    default void initialize(){}
 
     /**
      * Annunciate the message. Only returns once speaking finishes.
      * @param message Message text
      */
-    public void speak(final String message)
-    {
-        if (null != message)
-            voice.speak(message);
-    }
+    void speak(final String message);
 
     /**
-     * Deallocates the voice.
+     * Release resources that need to be cleaned on shutdown.
      */
-    public void shutdown()
-    {
-        voice.deallocate();
-    }
+    default void shutdown(){}
 }
