228d483fa007c04d9019fa77fdf1ef482c120f39
Abraham Wolk <abraham.wolk@ess.eu>
2024-05-08 13:15:16 +0200

    CSSTUDIO-2072 Add labels to messages.properties.

--- app/databrowser/src/main/java/org/csstudio/trends/databrowser3/ui/properties/AddPVsFromTheClipboardMenuItem.java
+++ app/databrowser/src/main/java/org/csstudio/trends/databrowser3/ui/properties/AddPVsFromTheClipboardMenuItem.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.Clipboard;
 import org.csstudio.trends.databrowser3.Activator;
+import org.csstudio.trends.databrowser3.Messages;
 import org.csstudio.trends.databrowser3.model.Model;
 import org.csstudio.trends.databrowser3.ui.DroppedPVNameParser;
 import org.phoebus.ui.dialog.DialogHelper;
@@ -15,7 +16,7 @@ public class AddPVsFromTheClipboardMenuItem extends MenuItem {
     public AddPVsFromTheClipboardMenuItem(UndoableActionManager undoableActionManager,
                                           Model model,
                                           Node nodeToPositionDialogOver) {
-        super("Add PV(s) from the Clipboard",
+        super(Messages.AddPVsFromTheClipboard,
                 Activator.getIcon("paste"));
 
         this.nodeToPositionDialogOver = nodeToPositionDialogOver;
@@ -48,8 +49,8 @@ public class AddPVsFromTheClipboardMenuItem extends MenuItem {
 
     private void showNoPVsFoundInClipboardWarning() {
         Alert warningAlert = new Alert(Alert.AlertType.INFORMATION,
-                                       "The clipboard doesn't contain a list of PV names separated by newline-characters:\n\n<PV Name1>\n<PV Name2>\n    ⋮\n\nOptionally, display names can be specified after a comma following the PV name:\n\n<PV Name 1>,<Display Name 1>\n<PV Name 2>,<Display Name 2>\n    ⋮");
-        warningAlert.setHeaderText("No PVs found in the clipboard!");
+                                       Messages.TheClipboardDoesNotContainPVs);
+        warningAlert.setHeaderText(Messages.NoPVsFoundInTheClipboard);
         DialogHelper.positionDialog(warningAlert, nodeToPositionDialogOver, 0, 0);
         warningAlert.show();
     }
