dee5a41528f704d5a722201b720e855956cc9e92
Kunal Shroff <kunalshroff9@gmail.com>
2024-06-17 11:42:41 -0400

    Refactor the use of google Strings with the Phoebus core-util version

--- app/logbook/ui/src/main/java/org/phoebus/logbook/ui/LogbookQueryUtil.java
+++ app/logbook/ui/src/main/java/org/phoebus/logbook/ui/LogbookQueryUtil.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.phoebus.util.text.Strings;
 import org.phoebus.util.time.TimeParser;
 
 public class LogbookQueryUtil {
