ac7414eac2fe93f3cfba5be34af0b9647fe305de
Abraham Wolk <abraham.wolk@ess.eu>
2025-02-21 15:40:10 +0100

    Revert "CSSTUDIO-3017 Only display the configuration window when a leaf representing a PV was clicked on."
    This reverts commit 965b4867b124994742ea492e2ca2e3c69df7e819.

--- app/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/tree/AlarmTreeView.java
+++ app/alarm/ui/src/main/java/org/phoebus/applications/alarm/ui/tree/AlarmTreeView.java
@@ -649,12 +649,10 @@ public class AlarmTreeView extends BorderPane implements AlarmClientListener
                 return;
 
             final AlarmTreeItem<?> item = tree_view.getSelectionModel().getSelectedItems().get(0).getValue();
-            if (item instanceof AlarmClientLeaf alarmClientLeaf) {
-                final ItemConfigDialog dialog = new ItemConfigDialog(model, alarmClientLeaf);
-                DialogHelper.positionDialog(dialog, tree_view, -150, -300);
-                // Show dialog, not waiting for it to close with OK or Cancel
-                dialog.show();
-            }
+            final ItemConfigDialog dialog = new ItemConfigDialog(model, item);
+            DialogHelper.positionDialog(dialog, tree_view, -150, -300);
+            // Show dialog, not waiting for it to close with OK or Cancel
+            dialog.show();
         });
     }
 
