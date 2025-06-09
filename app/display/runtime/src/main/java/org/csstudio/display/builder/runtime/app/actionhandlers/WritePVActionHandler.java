fe9bae10ee54d861c600cc9abfc7406cfb758f3f
Florian, LEFEVRE <florian.lefevre@thalesgroup.com>
2025-06-09 15:14:48 +0200

    feature: add formula evaluation on button PV Write Actions

--- app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/app/actionhandlers/WritePVActionHandler.java
+++ app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/app/actionhandlers/WritePVActionHandler.java
@@ -1,8 +1,11 @@
 /*
  * Copyright (C) 2024 European Spallation Source ERIC.
+ * Copyright (C) 2025 Thales.
  */
 
 package org.csstudio.display.builder.runtime.app.actionhandlers;
+
+import static org.csstudio.display.builder.model.ModelPlugin.logger;
 
 import org.csstudio.display.builder.model.Widget;
 import org.csstudio.display.builder.model.spi.ActionInfo;
@@ -11,18 +14,21 @@ import org.csstudio.display.builder.runtime.WidgetRuntime;
 import org.csstudio.display.builder.runtime.script.ScriptUtil;
 import org.csstudio.display.builder.model.spi.ActionHandler;
 import org.csstudio.display.actions.WritePVAction;
+import org.epics.vtype.VType;
+import org.phoebus.core.vtypes.VTypeHelper;
 import org.phoebus.framework.macros.MacroHandler;
 import org.phoebus.framework.macros.MacroValueProvider;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.phoebus.pv.PV;
+import org.phoebus.pv.PVPool;
 
 
 public class WritePVActionHandler implements ActionHandler {
     @Override
     public void handleAction(Widget sourceWidget, ActionInfo pluggableActionInfo) {
         final WidgetRuntime<Widget> runtime = RuntimeUtil.getRuntime(sourceWidget);
-        // System.out.println(action.getDescription() + ": Set " + action.getPV() + " = " + action.getValue());
         final MacroValueProvider macros = sourceWidget.getMacrosOrProperties();
         WritePVAction writePVAction = (WritePVAction)pluggableActionInfo;
         String pvName = writePVAction.getPV(), value = writePVAction.getValue();
@@ -44,7 +50,14 @@ public class WritePVActionHandler implements ActionHandler {
         }
         try
         {
-            runtime.writePV(pvName,value);
+            if (value.startsWith("="))
+            {
+                runtime.writePV(pvName, evaluateFormulaPV(value));
+            }
+            else
+            {
+                runtime.writePV(pvName,value);
+            }
         }
         catch (final Exception ex)
         {
@@ -54,8 +67,39 @@ public class WritePVActionHandler implements ActionHandler {
         }
     }
 
+    /**
+     * Evaluate a Formula PV expression
+     * @param formulaExpression The complete formula expression including =` and `
+     * @return The evaluated result as a string
+     */
+    private String evaluateFormulaPV(final String formulaExpression)
+    {
+        try
+        {
+            final PV formulaPV = PVPool.getPV(formulaExpression);
+
+            final VType value = formulaPV.read();
+
+            if (value != null)
+            {
+                return VTypeHelper.toString(value);
+            }
+            else
+            {
+                logger.log(Level.WARNING, "Formula PV '" + formulaExpression + "' returned null value");
+                return "<null>";
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.log(Level.WARNING, "Error evaluating Formula PV: " + formulaExpression, ex);
+            return "<null>";
+        }
+    }
+
     @Override
-    public boolean matches(ActionInfo pluggableActionInfo) {
+    public boolean matches(ActionInfo pluggableActionInfo)
+    {
         return pluggableActionInfo.getType().equalsIgnoreCase(WritePVAction.WRITE_PV);
     }
 }
