a1c9bbd4ecc800651e09a67eff868ec72b9e6706
jack soesman <jsoesman1@yahoo.com>
2025-07-31 13:15:28 -0400

    💪 Implement PVWS_Client into PVWS_Context as singleton class. Trigger lazy initalization of WebSocket client in PVWS_PV

--- core/pv-pvws/src/main/java/org/phoebus/pv/pvws/PVWS_PV.java
+++ core/pv-pvws/src/main/java/org/phoebus/pv/pvws/PVWS_PV.java
@@ -1,6 +1,9 @@
 package org.phoebus.pv.pvws;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.phoebus.pv.PV;
+
+import java.net.URISyntaxException;
 
 public class PVWS_PV extends PV {
     /**
@@ -8,7 +11,8 @@ public class PVWS_PV extends PV {
      *
      * @param name PV name
      */
-    protected PVWS_PV(String name) {
+    protected PVWS_PV(String name) throws URISyntaxException, InterruptedException, JsonProcessingException {
         super(name);
+        PVWS_Context context = PVWS_Context.getInstance();
     }
 }
