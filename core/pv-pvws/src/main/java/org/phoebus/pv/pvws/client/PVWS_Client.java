3e0984632319a784fbfe244cc1c8415ad0f4b912
jack soesman <jsoesman1@yahoo.com>
2025-07-31 11:46:52 -0400

    🚗 Migrate SessionHandler into pvws/client as PVWS_Client

---
+++ core/pv-pvws/src/main/java/org/phoebus/pv/pvws/client/PVWS_Client.java
@@ -0,0 +1,196 @@
+package org.phoebus.pv.pvws.client;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.java_websocket.WebSocket;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.framing.Framedata;
+import org.java_websocket.handshake.ServerHandshake;
+import org.phoebus.pv.pvws.PVWS_PV;
+import org.phoebus.pv.pvws.models.pv.PvwsData;
+import org.phoebus.pv.pvws.models.pv.PvwsMetadata;
+
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+
+public class PVWS_Client extends WebSocketClient {
+
+        private final ObjectMapper mapper;
+        private final CountDownLatch latch;
+        /*
+        private SubscriptionHandler subHandler;
+        private HeartbeatHandler heartbeatHandler;
+        private ReconnectHandler reconnectHandler;
+        private MetadataHandler metadataHandler;
+
+         */
+
+        public PVWS_Client(URI serverUri, CountDownLatch latch, ObjectMapper mapper) {
+            super(serverUri);
+            this.latch = latch;
+            this.mapper = mapper;
+        }
+
+        @Override
+        public void onOpen(ServerHandshake handshakedata) {
+            try {
+                System.out.println("Connected to server");
+                latch.countDown();
+                //reconnectHandler.resetStatus();
+                //heartbeatHandler.start();
+            } catch (Exception e) {
+                System.err.println("Exception in onOpen: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onMessage(String message) {
+
+            System.out.println("📨👍👍 Received: " + message);
+
+            try {
+                JsonNode node = mapper.readTree(message);
+                PvwsMetadata pvMeta = mapper.treeToValue(node, PvwsMetadata.class);
+
+                /* NEEDS METADATA MAP CLASS TODO
+                if (pvMeta.getVtype() != null)
+                    metadataHandler.setData(pvMeta); // comment this line out to test missing
+
+                 */
+
+
+                String type = node.get("type").asText();
+                switch (type) {
+                    case "update":
+                        PvwsData pvObj = mapper.treeToValue(node, PvwsData.class);
+
+
+                        /* NEEDS METADATA MAP CLASS TODO
+                        if (!metadataHandler.pvMetaMap.containsKey(pvObj.getPv())) {
+
+                            final int MAX_SUBSCRIBE_ATTEMPTS = 5;
+                            metadataHandler.refetch(MAX_SUBSCRIBE_ATTEMPTS, pvObj, this);
+                            return;
+
+                        }
+
+                         */
+
+                        // TODO: REPLACE WITH VArrDecoder
+                        //Base64BufferDeserializer.decodeArrValue(node, pvObj);
+
+
+                        //subscribeAttempts.remove(pvObj.getPv()); // reset retry count if we got the meta data
+
+                        /* TODO: NEEDS separate class to handle this specific severity data and probably status too
+                        if (node.has("severity"))// if severity changes set it in cached value
+                        {
+                            String currPV = pvObj.getPv();
+                            String currSeverity = pvObj.getSeverity();
+                            metadataHandler.pvMetaMap.get(currPV).setSeverity(currSeverity);
+                        }*/
+
+                        //merges class PV and json node of metadata together
+                        /* TODO: NEEDS METADATA MAP CLASS AND existing toVType class in utils/pv
+                        JsonNode nodeMerge = mapper.valueToTree(metadataHandler.pvMetaMap.get(pvObj.getPv()));
+                        mapper.readerForUpdating(pvObj).readValue(nodeMerge);
+
+                        VtypeHandler.processUpdate(pvObj);
+
+                         */
+                        System.out.println("🧊⛸️🥶: " + pvObj);
+
+                        break;
+                    default:
+                        System.out.println("⚠️ 😤Unknown message type: " + type);
+
+                }
+            } catch (Exception e) {
+                System.err.println("Error parsing or processing message: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onClose(int code, String reason, boolean remote) {
+            System.out.println("❌ Disconnected. Reason: " + reason);
+            /* TODO: HEARTBEAT AND RECONN HANDLER
+             heartbeatHandler.stop();
+
+            attemptReconnect();
+
+             */
+        }
+
+        @Override
+        public void onError(Exception ex) {
+            System.err.println("🚨 WebSocket Error: " + ex.getMessage());
+            /* TODO: HEARTBEAT AND RECONN HANDLER
+            heartbeatHandler.stop();
+            attemptReconnect();
+
+             */
+        }
+
+
+        public void closeClient() {
+            this.close();
+        }
+
+
+
+
+        /* TODO: ADD HANDLERS
+        public void setSubscriptionHandler(SubscriptionHandler subHandler) {
+            this.subHandler = subHandler;
+        }
+
+        public void setHeartbeatHandler(HeartbeatHandler heartbeatHandler) {
+            this.heartbeatHandler = heartbeatHandler;
+        }
+
+        public void setReconnectHandler(ReconnectHandler reconnectHandler) {
+            this.reconnectHandler = reconnectHandler;
+        }
+
+        public void setMetadataHandler(MetadataHandler metadataHandler) {
+            this.metadataHandler = metadataHandler;
+        }
+
+        public void subscribeClient(String[] pvs) throws JsonProcessingException {
+            subHandler.subscribe(pvs);
+        }
+
+        public void unSubscribeClient(String[] pvs) throws JsonProcessingException {
+            subHandler.unSubscribe(pvs);
+        }
+
+        public void attemptReconnect() {
+            this.reconnectHandler.attemptReconnect();
+        }
+        */
+
+
+        /* TODO: NEEDS HEARTBEAT HANDLER AND IDEALLY REFACTOR THESE 2 INTO THE HEARTBEAT CLASS
+        @Override
+        public void onWebsocketPing(WebSocket conn, Framedata f) {
+            System.out.println("Received Ping frame");
+            super.onWebsocketPing(conn, f);
+        }
+
+        @Override
+        public void onWebsocketPong(WebSocket conn, Framedata f) {
+            System.out.println("Received Pong frame"); // you could also comment this out to test the heartbeat timeout just for visual clarity
+            super.onWebsocketPong(conn, f);
+
+           heartbeatHandler.setLastPongTime(System.currentTimeMillis());
+        }
+
+         */
+
+
+    }
+
+
