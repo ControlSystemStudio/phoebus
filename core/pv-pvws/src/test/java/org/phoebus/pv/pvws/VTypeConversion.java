9df1d53a8d8c603e7d9c679209b7893fb9a276d5
jack soesman <jsoesman1@yahoo.com>
2025-08-08 14:23:10 -0400

    🐛 Fix vtype test for Arrays by adding mock arr attribute

--- core/pv-pvws/src/test/java/org/phoebus/pv/pvws/VTypeConversion.java
+++ core/pv-pvws/src/test/java/org/phoebus/pv/pvws/VTypeConversion.java
@@ -3,6 +3,7 @@ package org.phoebus.pv.pvws;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.epics.vtype.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -75,8 +76,9 @@ public class VTypeConversion {
                 new TestCase("VByte", "7", VByte.class),
                 new TestCase("VString", "\"hello\"", VString.class),
                 new TestCase("VBoolean", "true", VBoolean.class),
-                new TestCase("VDoubleArray", "[1.1,2.2,3.3]", VDoubleArray.class),
+                new TestCase("VDoubleArray", "\"xGDmkvzZC0BAq5qM2wzsv5RVPJoOJhDAclqZc/QfE0DQVwyThmfqvweT7jrRbwnA5P6Doq5UEcBofbejMXPwv5w59JqMuRJAovbbUNkkAUAmEwBR4RgSwJgwya4RYPQ/5IISWSJ8DcDY+1SCdHoJQDatWWpVRATAIk2TqBJsAMDEEvWipZPwv1AwRuPQ1eQ/qki27bRhA0CIxVSuHFwMQDCRJaSBdfE/nBEX3yUSEkAuwKcwWigDQPTBzhJDEQ7AxNftYwxNCEC6b0f/9wEAwOjHU4aZ3ANAofqkJgQeDMD4eqSywV4HwJAkDcPwgQRAiHpDeE3tDkDgTa/LTXwIQNxnDeE55vI//vJvb80zB0B21HvDS6YDwCDl9RGvFBDAcl//omAUAkDYMAhtH3f0vzAYj2AP6e8/lvQeGqvr878EdurHqeXxv7j+5F5KFPE/IDRdtid//7/408VyW2cMQFLSBm/EagHAMPQDAlOgEUBbZjRnDUESwLimeh96j/c/yN2+eH9gDUAQUjiyNJwBQCLwiL2pj/O/F2lHjsxfCcC1pmRoLwkRwMQ1hLZ4DwdAbGeQycYADUAu3U7BR+X2v5Tse39lcv8/NdAOTh5EE8Dsri7i/EABwLQMt77PUxFA1gbfxPRJE8CIgDiVqMgKwMbFa9wnWhPAcDlo7pcABEDgx2Clw08JQID1K7qoZgnAYP7UWDA5yz/NNbUzSeARwKC0QP9HONA/isv1QQnRB8DgCz7jCEnbP0wmb1THZQtAzHziz/VeE0Csyd6LkikAQJBI7hauiOg/kLbiMnoo1b9o8TCiodsQQIxYhKOmxBNA/IIZxBQSCUBU5jum1O4JQM0RxDym1hLAtxCuBRhaEcAE5SuXNKULQDgRO7RIluW/ekd+7F2mA8C6vgadaM0SQIEeybf7FBDAxsSe+mlR+79ot0hbyZUBQECSv8T/x+q/gFqLLi5XD8A/o7rSNoQGwADhyppXAqo/yM2DdvpzEkCAWn7L9o+jPzDCP3IW7dw/uLdy/dZr879kRiqSOXYIwHwgit3H4QRAsLtRCo3vB8A=\"", VDoubleArray.class),
                 new TestCase("VStringArray", "[\"a\",\"b\",\"c\"]", VStringArray.class)
+
         );
     }
 
@@ -106,8 +108,17 @@ public class VTypeConversion {
                 """.formatted(testCase.vtypeName, testCase.valueJson);
 
 
+        if(testCase.vtypeName.equals("VDoubleArray"))
+        {
+            message = message.replaceFirst("\"value\":", "\"b64dbl\":");
+        }
+
         JsonNode node = mapper.readTree(message);
+
+
+
         PvwsData pvObj = mapper.treeToValue(node, PvwsData.class);
+
         VArrDecoder.decodeArrValue(node, pvObj);
 
 
