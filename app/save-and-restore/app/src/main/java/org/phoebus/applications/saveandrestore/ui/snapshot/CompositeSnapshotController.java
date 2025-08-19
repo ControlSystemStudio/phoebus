49cc009065edf5fa06e847be4678aa8c7e5119c6
georgweiss <georg.weiss@ess.eu>
2025-08-19 10:22:52 +0200

    Fix sorting in composite snapshot editor

--- app/save-and-restore/app/src/main/java/org/phoebus/applications/saveandrestore/ui/snapshot/CompositeSnapshotController.java
+++ app/save-and-restore/app/src/main/java/org/phoebus/applications/saveandrestore/ui/snapshot/CompositeSnapshotController.java
@@ -105,6 +105,10 @@ public class CompositeSnapshotController extends SaveAndRestoreBaseController im
 
     @SuppressWarnings("unused")
     @FXML
+    private TableColumn<Node, String> snapshotDescriptionColumn;
+
+    @SuppressWarnings("unused")
+    @FXML
     private TableView<Node> snapshotTable;
 
     @SuppressWarnings("unused")
@@ -175,8 +179,6 @@ public class CompositeSnapshotController extends SaveAndRestoreBaseController im
         snapshotTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         snapshotTable.getSelectionModel().selectedItemProperty().addListener((obs, ov, nv) -> selectionEmpty.set(nv == null));
 
-        snapshotNameColumn.setComparator(Comparator.comparing(Node::getName));
-
         MenuItem deleteMenuItem = new MenuItem(Messages.menuItemDeleteSelectedPVs,
                 new ImageView(ImageCache.getImage(SaveAndRestoreController.class, "/icons/delete.png")));
         deleteMenuItem.setOnAction(ae -> {
@@ -223,6 +225,7 @@ public class CompositeSnapshotController extends SaveAndRestoreBaseController im
                 };
             }
         });
+        snapshotPathColumn.setComparator(Comparator.comparing(n -> DirectoryUtilities.CreateLocationString(n, true).toLowerCase()));
 
         ContextMenu contextMenu = new ContextMenu();
         MenuItem removeMenuItem = new MenuItem("Remove Selected");
@@ -290,6 +293,9 @@ public class CompositeSnapshotController extends SaveAndRestoreBaseController im
                 };
             }
         });
+        snapshotNameColumn.setComparator(Comparator.comparing(n -> n.getName().toLowerCase()));
+
+        snapshotDescriptionColumn.setComparator(Comparator.comparing(String::toLowerCase));
 
         compositeSnapshotNameField.textProperty().bindBidirectional(compositeSnapshotNameProperty);
         compositeSnapshotNameField.disableProperty().bind(userIdentity.isNull());
