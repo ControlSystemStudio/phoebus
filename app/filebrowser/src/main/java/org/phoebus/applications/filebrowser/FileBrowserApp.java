62c8166a0cb60bd5597b822ce332827f4105b7eb
Gareth Lee <60355612+Gir4ffe@users.noreply.github.com>
2025-10-06 12:01:28 +0100

    Update FileBrowserApp to work with Top Resources
    Updated to allow the FileBrowserApp to be called from Top Resources with a path. This allows navigation to repositories from Top Resources.

--- app/filebrowser/src/main/java/org/phoebus/applications/filebrowser/FileBrowserApp.java
+++ app/filebrowser/src/main/java/org/phoebus/applications/filebrowser/FileBrowserApp.java
@@ -2,6 +2,7 @@ package org.phoebus.applications.filebrowser;
 
 import java.io.File;
 import java.net.URI;
+import java.net.URL;
 
 import org.phoebus.framework.preferences.AnnotatedPreferences;
 import org.phoebus.framework.preferences.Preference;
@@ -42,14 +43,37 @@ public class FileBrowserApp implements AppResourceDescriptor {
         return createWithRoot(default_root);
     }
 
+    // @Override
+    // public AppInstance create(final URI resource)
+    // {
+    //     return createWithRoot(new File(resource));
+    // }
+
     @Override
     public AppInstance create(final URI resource)
     {
-        return createWithRoot(new File(resource));
+        try
+        {
+            // Remove query component since File(URI) does not accept it
+            URI clean = new URI(resource.getScheme(), resource.getSchemeSpecificPart(), null);
+            return createWithRoot(new File(clean));
+        }
+        catch (Exception ex)
+        {
+            // Fallback: try using path string
+            return createWithRoot(new File(resource.getPath()));
+        }
     }
+
 
     public AppInstance createWithRoot(final File directory)
     {
         return new FileBrowser(this, directory);
+    }
+
+    @Override
+    public URL getIconURL()
+    {
+        return FileBrowserApp.class.getResource("/icons/filebrowser.png");
     }
 }
