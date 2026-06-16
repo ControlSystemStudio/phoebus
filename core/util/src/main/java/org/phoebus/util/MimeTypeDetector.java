7e97eab29e0e93a8b0ae5a2d8f947bbb930bc34e
Sky Brewer <sky.brewer@ess.eu>
2026-06-16 15:42:22 +0200

    Swap determineMimeType to File input
    This means no need to handle the inputstream
    
    But this means MemoryStreams etc can't be handled.

--- core/util/src/main/java/org/phoebus/util/MimeTypeDetector.java
+++ core/util/src/main/java/org/phoebus/util/MimeTypeDetector.java
@@ -5,41 +5,32 @@
 package org.phoebus.util;
 
 import org.apache.tika.Tika;
-import org.apache.tika.metadata.Metadata;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MimeTypeDetector {
 
     /**
-     * Attempts to determine the file type based on its content. Cleverness delegated to Apache Tika.
-     * <p>
-     * Note that the {@link InputStream} will be closed by this method.
-     * </p>
+     * Attempts to determine the file type based on its content and name. Cleverness delegated to Apache Tika.
      *
-     * @param inputStream A non-null {@link InputStream} that will be closed by this method.
+     * @param file A non-null {@link File} to inspect.
      * @return A MIME type string, e.g. image/jpeg
-     * @throws IOException If there is a problem reading the stream or if the provided {@link InputStream} is
+     * @throws IOException If there is a problem reading the file or if the provided {@link File} is
      *                     <code>null</code>.
      */
-    public static String determineMimeType(InputStream inputStream) throws IOException {
-        if (inputStream == null) {
-            throw new IOException("InputStream must not be null");
+    public static String determineMimeType(File file) throws IOException {
+        if (file == null) {
+            throw new IOException("File must not be null");
         }
-        try (inputStream) {
-            try {
-                return new Tika().detect(inputStream, new Metadata());
-            } catch (IOException e) {
-                Logger.getLogger(MimeTypeDetector.class.getName())
-                    .log(Level.WARNING, "Unable to read input stream", e);
-                throw e;
-            }
+        try {
+            return new Tika().detect(file);
         } catch (IOException e) {
             Logger.getLogger(MimeTypeDetector.class.getName())
-                .log(Level.WARNING, "Failed to close input stream", e);
+                .log(Level.WARNING, "Unable to determine MIME type of " + file, e);
+            throw e;
         }
     }
 }
