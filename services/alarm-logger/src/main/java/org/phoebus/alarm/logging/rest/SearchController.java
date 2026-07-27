5f360d29dc2b71c40a24ac50986d1c86ea9a74d7
shroffk <kunalshroff9@gmail.com>
2026-07-27 15:17:38 -0400

    fix the javax import for the alarm-logger service

--- services/alarm-logger/src/main/java/org/phoebus/alarm/logging/rest/SearchController.java
+++ services/alarm-logger/src/main/java/org/phoebus/alarm/logging/rest/SearchController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
